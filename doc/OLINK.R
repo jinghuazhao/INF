b5cfb749e1d2b656ccd8885f26558c2b9c2630f0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-08-23 21:18:57 +0100

    Documents

--- doc/OLINK.R
+++ doc/OLINK.R
@@ -2,11 +2,11 @@
 
 options(width=160)
 
-olink <- function(xlsx, tabs, order=FALSE, nlines=5)
+olink <- function(xlsx, tabs, order, nlines, verbose)
 {
   for (x in tabs) 
   {
-    cat("\n\n", x, ":\n", rep("-", nchar(x)+1), "\n\n", sep="")
+    if (verbose) cat("\n\n", x, ":\n", rep("-", nchar(x)+1), "\n\n", sep="")
     t <- openxlsx::read.xlsx(xlsx, sheet=x, colNames=TRUE, skipEmptyRows=FALSE, cols=c(1:16), rows=3:95)
     s <- sub(" ", "_", x)
     if (!order) assign(s, t, envir=.GlobalEnv) else
@@ -14,17 +14,22 @@ olink <- function(xlsx, tabs, order=FALSE, nlines=5)
       o <- order(t[,2])
       assign(s, t[o,], envir=.GlobalEnv)
     }
-    s <- get(noquote(s))
+    s <- get(s)
     t <- "Target"
     n <- names(s)
-    print(head(s["Target"],nlines),right=FALSE)
-    cat("\n")
-    print(head(s[setdiff(n,t)],nlines))
+    if (verbose)
+    {
+      print(head(s["Target"],nlines),right=FALSE)
+      cat("\n")
+      print(head(s[setdiff(n,t)],nlines))
+    }
   }
 }
 xlsx <- "Olink validation data all panels.xlsx"
+tabs <- "Inflammation"
+olink(xlsx,tabs,FALSE,92,TRUE)
+inf.orig <- Inflammation
 tabs <-c("Cardiometabolic","Cell Regulation","CVD II","CVD III","Development","Immune Response","Immuno-Oncology",
          "Inflammation","Metabolism","Neurology","Oncology II","Organ Damage")
-olink(xlsx,tabs)
-tabs <- "Inflammation"
-olink(xlsx,tabs,nlines=92)
+olink(xlsx,tabs,TRUE,92,FALSE)
+ls()
