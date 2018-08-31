d39ec7fbe8aae937d87d9760d169c3c50454573f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-08-31 14:42:43 +0100

    st.sh

---
+++ st.sh
@@ -0,0 +1,11 @@
+# 31-8-2018 JHZ
+
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add doc
+git commit -m "Documents"
+git add st.sh
+git commit -m "st.sh"
+git push
