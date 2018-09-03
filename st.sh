67dd4305e547b5a0f82435793134755012f7076d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-03 15:28:32 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -5,7 +5,7 @@ git commit -m ".gitignore"
 git add README.md
 git commit -m "README"
 git add doc
-git commit -m "Documents"
+git commit -m "Documents and auxiliary files"
 git add st.sh
 git commit -m "st.sh"
 git push
