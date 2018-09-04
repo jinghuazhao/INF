cd85181f30e72c3b1341e9f6020413d04c7abbd7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-04 16:46:51 +0100

    Documents and auxiliary files

---
+++ doc/circlize.R
@@ -0,0 +1,13 @@
+# http://jokergoo.github.io/circlize/example/genomic_link.html
+
+library(circlize)
+
+bed1 = generateRandomBed(nr = 100)
+bed1 = bed1[sample(nrow(bed1), 20), ]
+bed2 = generateRandomBed(nr = 100)
+bed2 = bed2[sample(nrow(bed2), 20), ]
+circos.par("track.height" = 0.1, cell.padding = c(0, 0, 0, 0))
+circos.initializeWithIdeogram()
+
+circos.genomicLink(bed1, bed2, col = sample(1:5, 20, replace = TRUE), border = NA)
+circos.clear()
