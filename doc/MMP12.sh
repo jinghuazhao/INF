fc6094825df05db2ed744b7bf958efe761800200
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-05 16:34:16 +0100

    Documents and auxiliary files

--- doc/MMP12.sh
+++ doc/MMP12.sh
@@ -1,9 +1,9 @@
-# 3/9/2018 JHZ
+# 5/9/2018 JHZ
 
 # MMP12 (UCSC uc001phk.3) at chr11:102733464-102745764
 # MMP12 (RefSeq) at chr11:102733460-102745764
 
-# One should use whole-genome counterpart to replicate analysis in the paper
+# One could use whole-genome counterpart to replicate analysis in the paper
 gunzip -c MMP12.4496.60.2/MMP12.4496.60.2_chrom_11_meta_final_v1.tsv.gz | awk 'NR==1||($3>=102733464 && $3<=102745764)' > MMP12.txt
 awk 'NR==1||($2==11 && $3>=102733464 && $3<=102745764)' cad.add.160614.website.txt > CAD.txt
 
