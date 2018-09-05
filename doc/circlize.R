d695d5580a5b5aa2feee0051170775ff1a6c8a76
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-05 11:22:54 +0100

    Documents and auxiliary files

--- doc/circlize.R
+++ doc/circlize.R
@@ -3,11 +3,12 @@
 library(circlize)
 
 bed1 = generateRandomBed(nr = 100)
-bed1 = bed1[sample(nrow(bed1), 20), ]
+bed1 = bed1[sample(nrow(bed1), 1), ]
+bed1 <- rbind(bed1,bed1,bed1,bed1,bed1,bed1)
 bed2 = generateRandomBed(nr = 100)
-bed2 = bed2[sample(nrow(bed2), 20), ]
+bed2 = bed2[sample(nrow(bed2), 6), ]
 circos.par("track.height" = 0.1, cell.padding = c(0, 0, 0, 0))
 circos.initializeWithIdeogram()
 
-circos.genomicLink(bed1, bed2, col = sample(1:5, 20, replace = TRUE), border = NA)
+circos.genomicLink(bed1, bed2, col = sample(1:6, 6, replace = TRUE), border = NA)
 circos.clear()
