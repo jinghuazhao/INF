b8eb2ee93fda546f71335c91148b26c89729fb84
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-06 14:40:59 +0100

    Documents and auxiliary files

--- doc/circlize.R
+++ doc/circlize.R
@@ -11,9 +11,7 @@ b1 <- with(b, data.frame(chr=paste0("chr",Chr),start=94844947-1,end=94844947,val
 b2 <- with(b, data.frame(chr=X.chrom,start=chromStart,end=chromEnd,value1=t[["Meta-analysis"]]/X14,Target=Target,UniProt=UniProt))
 b1
 b2
-pdf("circlize.pdf")
 circos.par("track.height" = 0.1, cell.padding = c(0, 0, 0, 0))
 circos.initializeWithIdeogram()
 circos.genomicLink(b1, b2, col = 10, border = 10, lwd = 2)
 circos.clear()
-dev.off()
