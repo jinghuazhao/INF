d274ae702ff555f5595cdfcd4809d64a15bf38f1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-12 09:22:06 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,4 @@
-# 4-9-2018 JHZ
+# 12-9-2018 JHZ
 
 git add .gitignore
 git commit -m ".gitignore"
@@ -6,6 +6,10 @@ git add README.md
 git commit -m "README"
 git add doc
 git commit -m "Documents and auxiliary files"
+git add list.sh format.sh INTERVAL.awk PLINK.sh
+git commit -m "Programs"
+git add METAL.qsub METAL.sh
+git commit -m "METAL program"
 git add st.sh
 git commit -m "st.sh"
 git push
