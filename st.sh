f0ae3eaa2c76e4dcab7ff5d0c8cd294e3c776ab6
Jing Hua Zhao <jhz22@cardio-login1.xcat.cluster>
2018-09-17 06:23:40 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,4 @@
-# 12-9-2018 JHZ
+# 17-9-2018 JHZ
 
 git add .gitignore
 git commit -m ".gitignore"
@@ -6,6 +6,10 @@ git add README.md
 git commit -m "README"
 git add doc
 git commit -m "Documents and auxiliary files"
+git add sumstats
+git commit -m 'Reformatted summary statistics by cohort'
+git add METAL
+git commit -m "METAL results"
 git add list.sh format.sh INTERVAL.awk PLINK.sh
 git commit -m "Programs"
 git add METAL.qsub
