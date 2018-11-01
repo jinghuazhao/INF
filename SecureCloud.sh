d1f193ee6ccae13b49df29f972bad51f010e6954
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-11-01 10:17:44 +0000

    Programs

---
+++ SecureCloud.sh
@@ -0,0 +1,15 @@
+# 29-10-2018 JHZ
+
+git add .gitignore
+git commit -m ".gitignore"
+git add SecureCloud.md
+git commit -m "SecreCloud"
+git add files list.sh format.sh metal.sh analysis.sh analysis.ini
+git commit -m "Programs"
+git add TORQUE.md TORQUE.qsub
+git commit -m "TORQUE notes and program"
+git add doc
+git commit -m "Documents"
+git add SecureCloud.sh
+git commit -m "SecureCloud.sh"
+git push
