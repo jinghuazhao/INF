586a4e8589024381406de8c3e1611eba20f8852f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-11-01 15:37:14 +0000

    Documents and auxiliary files

--- doc/cardio.sh
+++ doc/cardio.sh
@@ -26,6 +26,58 @@ export BGEN=$BGEN_DIR/interval_olink_subset_unrelated_4994_pihat_0.1875_autosoma
 
 qctool -g $BGEN.bgen -s $BGEN.sample -snp-stats -osnp INTERVAL.snpstats
 
+awk 'NR==10' INTERVAL.snpstats | \
+awk '{gsub(/\t/, "\n",$0)};1'| \
+awk '{print "#" NR, $1}'
+
+#1 alternate_ids
+#2 rsid
+#3 chromosome
+#4 position
+#5 alleleA
+#6 alleleB
+#7 comment
+#8 HW_exact_p_value
+#9 HW_lrt_p_value
+#10 alleleA_count
+#11 alleleB_count
+#12 alleleA_frequency
+#13 alleleB_frequency
+#14 minor_allele_frequency
+#15 minor_allele
+#16 major_allele
+#17 info
+#18 impute_info
+#19 missing_proportion
+#20 A
+#21 B
+#22 AA
+#23 AB
+#24 BB
+#25 NULL
+#26 total
+
+head -1 INTERVAL.snpstats
+awk '
+{
+   OFS="\t"
+   if (NR>1)
+   {
+     CHR=$3
+     POS=$4
+     a1=$5
+     a2=$6
+     if (a1>a2) snpid="chr" CHR ":" POS "_" a2 "_" a1;
+     else snpid="chr" CHR ":" POS "_" a1 "_" a2
+     $1=snpid
+   }
+   print snpid, $2
+}' INTERVAL.snpstats | \
+sort | \
+join - INTERVAL.clumped | \
+cut -d' ' -f2 > INTERVAL.rsid
+
+
 export SCRIPT=/scratch/jp549/analyses/interval_subset_olink/inf1/r2/outlier_in/pcs1_3
 export BS=/scratch/jp549/apps/bram-scripts
 export INF=/scratch/curated_genetic_data/phenotypes/interval/high_dimensional_data/Olink_proteomics_inf/gwasqc/olink_qcgwas_inf.csv
