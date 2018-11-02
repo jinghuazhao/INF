a7d48c2ed4bc7ceda162d83ae7082a58f8b6b86d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-11-02 23:44:53 +0000

    Documents and auxiliary files

--- doc/cardio.sh
+++ doc/cardio.sh
@@ -3,27 +3,6 @@
 
 # 2-11-2018 JHZ
 
-export INTERVAL=/scratch/jp549/olink-merged-output
-function CD6()
-# SUMSTATS for depict
-{
-  gunzip -c $INTERVAL/INTERVAL_inf1_CD6___Q8WWJ7_chr_merged.gz | \
-  awk -vOFS="\t" '(NR>1){
-       SNP=$2;
-       chr=$3; sub(/^0/,"",chr);
-       pos=$4;
-       a1=$5;
-       a2=$6;
-       N=$18;
-       EAF=(0.5*$15+$16)/N;
-       beta=$24;
-       se=$25;
-       p=$22;
-       if(p<=1e-5) print SNP,a2,a1,EAF,beta,se,p,N,chr,pos;
-  }' | \
-  sort -k9,9n -k10,10n > CD6
-}
-
 export BGEN_DIR=/scratch/bp406/data_sets/interval_subset_olink/genotype_files/unrelated_4994_pihat_0.1875_autosomal_typed_only
 export BGEN=$BGEN_DIR/interval_olink_subset_unrelated_4994_pihat_0.1875_autosomal_typed_only
 function snpstats_typed()
@@ -74,66 +53,70 @@ function snpstats_typed()
 #25 NULL
 #26 total
 
-cd work
-sed 's|work/INTERVAL.||g;s/.clumped://g' INTERVAL.clumped | \
-awk '{$1=$1;if(NR==1) $1="prot";if(NF>1) print}' > INTERVAL.clumped.dat
-
-sed 's|work/INTERVAL.||g;s/.jma.cojo:/ /g' INTERVAL.jma | \
-awk '{$1=$1;if(NR==1) $1="prot";if(NF>1) print}' > INTERVAL.jma.dat
-awk 'NR>1' INTERVAL.jma.dat | \
-sort -k2,2n -k4,4n | \
-awk '{print "chr" $2 ":" $4}' | \
-uniq > INTERVAL.ps
-
-sed 's|work/INTERVAL.||g;s/.ldr.cojo:/ /g' INTERVAL.ldr| \
-awk '{$1=$1; if(NR>1 && NF>1) print}' > INTERVAL.ldr.dat
-cd -
-
 export REF=/scratch/curated_genetic_data/reference_files/interval/
 export TMPDIR=/scratch/jhz22/INF/work
-seq 22 | \
-parallel -j5 --env REF -C' ' '
-  cut -f2-6 $REF/impute_{}_interval.snpstats | \
-  awk "NR>1" | \
-  awk "{\
-    rsid=\$1;chr=\$2;pos=\$3;a1=\$4;a2=\$5; \
-    gsub(/^0/,\"\",chr); \
-    if (a1>a2) snpid=\"chr\" chr \":\" pos \"_\" a2 \"_\" a1; \
-    else snpid=\"chr\" chr \":\" pos \"_\" a1 \"_\" a2; \
-    print snpid, rsid \
-  }" | \
-  gzip -f > INTERVAL.snpid-{}.gz'
-cat INTERVAL.snpid-1.gz \
-    INTERVAL.snpid-2.gz \
-    INTERVAL.snpid-3.gz \
-    INTERVAL.snpid-4.gz \
-    INTERVAL.snpid-5.gz \
-    INTERVAL.snpid-6.gz \
-    INTERVAL.snpid-7.gz \
-    INTERVAL.snpid-8.gz \
-    INTERVAL.snpid-9.gz \
-    INTERVAL.snpid-10.gz \
-    INTERVAL.snpid-11.gz \
-    INTERVAL.snpid-12.gz \
-    INTERVAL.snpid-13.gz \
-    INTERVAL.snpid-14.gz \
-    INTERVAL.snpid-15.gz \
-    INTERVAL.snpid-16.gz \
-    INTERVAL.snpid-17.gz \
-    INTERVAL.snpid-18.gz \
-    INTERVAL.snpid-19.gz \
-    INTERVAL.snpid-20.gz \
-    INTERVAL.snpid-21.gz \
-    INTERVAL.snpid-22.gz > INTERVAL.snpid.gz
-rm INTERVAL.snpid-*.gz
+function snpstats_imputed()
+{
+  cd work
+  seq 22 | \
+  parallel -j5 --env REF -C' ' '
+    cut -f2-6 $REF/impute_{}_interval.snpstats | \
+    awk "NR>1" | \
+    awk "{\
+      rsid=\$1;chr=\$2;pos=\$3;a1=\$4;a2=\$5; \
+      gsub(/^0/,\"\",chr); \
+      if (a1>a2) snpid=\"chr\" chr \":\" pos \"_\" a2 \"_\" a1; \
+      else snpid=\"chr\" chr \":\" pos \"_\" a1 \"_\" a2; \
+      print snpid, rsid \
+    }" | \
+    gzip -f > INTERVAL.snpid-{}.gz'
+  cat INTERVAL.snpid-1.gz \
+      INTERVAL.snpid-2.gz \
+      INTERVAL.snpid-3.gz \
+      INTERVAL.snpid-4.gz \
+      INTERVAL.snpid-5.gz \
+      INTERVAL.snpid-6.gz \
+      INTERVAL.snpid-7.gz \
+      INTERVAL.snpid-8.gz \
+      INTERVAL.snpid-9.gz \
+      INTERVAL.snpid-10.gz \
+      INTERVAL.snpid-11.gz \
+      INTERVAL.snpid-12.gz \
+      INTERVAL.snpid-13.gz \
+      INTERVAL.snpid-14.gz \
+      INTERVAL.snpid-15.gz \
+      INTERVAL.snpid-16.gz \
+      INTERVAL.snpid-17.gz \
+      INTERVAL.snpid-18.gz \
+      INTERVAL.snpid-19.gz \
+      INTERVAL.snpid-20.gz \
+      INTERVAL.snpid-21.gz \
+      INTERVAL.snpid-22.gz > INTERVAL.snpid.gz
+  rm INTERVAL.snpid-*.gz
+  cd -
+}
 
-# see also http://www.phenoscanner.medschl.cam.ac.uk
+function process_clump_cojo()
+{
+  cd work
+  sed 's|work/INTERVAL.||g;s/.clumped://g' INTERVAL.clumped | \
+  awk '{$1=$1;if(NR==1) $1="prot";if(NF>1) print}' > INTERVAL.clumped.dat
 
-module load phenoscanner/phenoscanner_v1.1
-phenoscanner -c All -l No -p 0.00001 -i INTERVAL.ps -o INTERVAL
+  sed 's|work/INTERVAL.||g;s/.jma.cojo:/ /g' INTERVAL.jma | \
+  awk '{$1=$1;if(NR==1) $1="prot";if(NF>1) print}' > INTERVAL.jma.dat
+  awk 'NR>1' INTERVAL.jma.dat | \
+  sort -k2,2n -k4,4n | \
+  awk '{print "chr" $2 ":" $4}' | \
+  uniq > INTERVAL.ps
+
+  sed 's|work/INTERVAL.||g;s/.ldr.cojo:/ /g' INTERVAL.ldr| \
+  awk '{$1=$1; if(NR>1 && NF>1) print}' > INTERVAL.ldr.dat
+  cd -
+}
 
 function signals()
 {
+  cd work
   awk 'NR>1' INTERVAL.jma.dat | \
   sort -k3,3 | \
   cut -d' ' -f1,3 > INTERVAL.jma.snpid
@@ -141,21 +124,51 @@ function signals()
   sort -k1,1 | \
   join -11 -22 - INTERVAL.jma.snpid | \
   cut -d' ' -f2 > INTERVAL.rsid
+  cd -
+}
+
+function ps()
+# see also http://www.phenoscanner.medschl.cam.ac.uk
+{
+  module load phenoscanner/phenoscanner_v1.1
+  cd work
+  phenoscanner -c All -l No -p 0.00001 -i INTERVAL.ps -o INTERVAL
+  cd -
+}
+
+export INTERVAL=/scratch/jp549/olink-merged-output
+function CD6()
+# SUMSTATS for depict
+{
+  gunzip -c $INTERVAL/INTERVAL_inf1_CD6___Q8WWJ7_chr_merged.gz | \
+  awk -vOFS="\t" '(NR>1){
+       SNP=$2;
+       chr=$3; sub(/^0/,"",chr);
+       pos=$4;
+       a1=$5;
+       a2=$6;
+       N=$18;
+       EAF=(0.5*$15+$16)/N;
+       beta=$24;
+       se=$25;
+       p=$22;
+       if(p<=1e-5) print SNP,a2,a1,EAF,beta,se,p,N,chr,pos;
+  }' | \
+  sort -k9,9n -k10,10n > CD6
 }
 
 export PHEN=/scratch/curated_genetic_data/phenotypes/interval/high_dimensional_data/Olink_proteomics_inf/gwasqc/olink_qcgwas_inf.csv
 export SCRIPT=/scratch/jp549/analyses/interval_subset_olink/inf1/r2/outlier_in/pcs1_3
 export BS=/scratch/jp549/apps/bram-scripts
-
 function NOTE_for_MASS_Processing()
 {
-ls $INTERVAL/*gz | \
-grep inf1 | \
-xargs -l basename | \
-sed 's/INTERVAL_inf1_//g;s/_chr_merged.gz\*//g' | \
-cut -d'_' --output-delimiter=' ' -f1,4 | \
-grep -w CD6 | \
-parallel -j2 --env INTERVAL -C' ' '
-  gunzip -c $INTERVAL/INTERVAL_inf1_{1}___{2}_chr_merged.gz
-'
+  ls $INTERVAL/*gz | \
+  grep inf1 | \
+  xargs -l basename | \
+  sed 's/INTERVAL_inf1_//g;s/_chr_merged.gz\*//g' | \
+  cut -d'_' --output-delimiter=' ' -f1,4 | \
+  grep -w CD6 | \
+  parallel -j2 --env INTERVAL -C' ' '
+    gunzip -c $INTERVAL/INTERVAL_inf1_{1}___{2}_chr_merged.gz
+  '
 }
