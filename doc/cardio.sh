c7911fad2135aa7ab2e10ddb5761e1a44df3fe90
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-11-05 12:45:57 +0000

    Documents and auxiliary files

--- doc/cardio.sh
+++ doc/cardio.sh
@@ -5,7 +5,7 @@
 # 1. The overall design was influenced by the fact that snpid (chr:pos_a1_a2) instead of rsid is used in the metal-analysis.
 # 2. PLINK clumping (clumped) provides corroborative result to GCTA -cojo (jma) used for PhenoScanner|cis/trans expliotation.
 # 3. The snpid-rsid correspondence is obtained from snpstats_typed() and snpstats_imputed(), respectively.
-# 4. A SNP-gene match is established by snp_gene(), the experimental cis_trans() and olink_cis_trans().
+# 4. A SNP-gene match is established by snp_gene() and olink_cis_trans().
 #    Additional notes for this step are as follows,
 #    This follows https://github.com/jinghuazhao/PW-pipeline/blob/master/vegas2v2.sh
 #    bedtools 2.4.26 on cardio is called with
@@ -138,47 +138,9 @@ function clumped_jma()
   cd -
 }
 
-export INF=/scratch/jhz22/INF
-function olink_cis_trans()
-{
-  head -1 $INF/doc/olink.inf.panel.annot.tsv | \
-  awk '{gsub(/\t/, "\n",$0)};1'| \
-  awk '{print "#" NR, $1}'
-  awk '{
-    FS=OFS="\t"
-    gsub(/\"/,"",$0)
-    if(NR==1) print "#chrom","start","end","gene";
-    else print "chr" $8,$9,$10,$7
-  }' $INF/doc/olink.inf.panel.annot.tsv > olink.bed
-  module load gcc/4.8.1
-  bedtools intersect -a INTERVAL.bed -b olink.bed -loj > INTERVAL.tmp
-  cut -f8 INTERVAL.tmp | \
-  awk '!/\.|NA/' | \
-  wc -l
-  awk '{
-    FS=OFS="\t"
-    chr=$1;sub(/chr/,"",chr)
-    pos=$3
-    print $0,chr,pos
-  }' INTERVAL.tmp | \
-  sort -k9,9n -k10,10n | \
-  cut -f1-8 > INTERVAL.olink.cis_trans
-}
-#1 "target"
-#2 "target.short"
-#3 "uniprot"
-#4 "panel"
-#5 "prot.on.multiple.panel"
-#6 "panels.with.prot"
-#7 "hgnc_symbol"
-#8 "chromosome_name"
-#9 "start_position"
-#10 "end_position"
-#11 "olink.id"
-#12 "alternate.uniprot"
-
 export M=1000000
 function snp_gene()
+# broad SNP-gene match
 {
   cd work
   mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -D hg19 -e 'select * from refGene' > refGene.txt
@@ -205,10 +167,31 @@ function snp_gene()
   cd -
 }
 
-function cis_trans()
-# This somewhat causes confusion since 1,000,000 flankings covers more genes.
+export INF=/scratch/jhz22/INF
+function olink_cis_trans()
 {
+  head -1 $INF/doc/olink.inf.panel.annot.tsv | \
+  awk '{gsub(/\t/, "\n",$0)};1'| \
+  awk '{print "#" NR, $1}'
+  awk '{
+    FS=OFS="\t"
+    gsub(/\"/,"",$0)
+    if(NR==1) print "#chrom","start","end","gene";
+    else print "chr" $8,$9,$10,$7
+  }' $INF/doc/olink.inf.panel.annot.tsv > olink.bed
   module load gcc/4.8.1
+  bedtools intersect -a INTERVAL.bed -b olink.bed -loj > INTERVAL.tmp
+  cut -f8 INTERVAL.tmp | \
+  awk '!/\.|NA/' | \
+  wc -l
+  awk '{
+    FS=OFS="\t"
+    chr=$1;sub(/chr/,"",chr)
+    pos=$3
+    print $0,chr,pos
+  }' INTERVAL.tmp | \
+  sort -k9,9n -k10,10n | \
+  cut -f1-8 > INTERVAL.olink.cis_trans
   awk -vOFS="\t" -vM=$M '{
     chrom=$1
     cdsStart=$2
@@ -234,6 +217,18 @@ function cis_trans()
   }' glist-hg19.bed > glist-hg19.cis_trans
   bedtools intersect -a INTERVAL.bed -b glist-hg19.cis_trans -loj > INTERVAL.glist-hg19.cis_trans
 }
+#1 "target"
+#2 "target.short"
+#3 "uniprot"
+#4 "panel"
+#5 "prot.on.multiple.panel"
+#6 "panels.with.prot"
+#7 "hgnc_symbol"
+#8 "chromosome_name"
+#9 "start_position"
+#10 "end_position"
+#11 "olink.id"
+#12 "alternate.uniprot"
 
 export INTERVAL=/scratch/jp549/olink-merged-output
 function format_for_METAL()
