638d18748a10d9663d78e283e5081fff101fb4c4
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-11-10 15:27:20 +0000

    TRYGGVE notes

--- tryggve.sh
+++ tryggve.sh
@@ -1,15 +1,15 @@
-# 29-10-2018 JHZ
+# 10-11-2018 JHZ
 
 git add .gitignore
 git commit -m ".gitignore"
-git add SecureCloud.md
-git commit -m "SecreCloud"
+git add tryggve.md
+git commit -m "tryggve notes"
 git add files list.sh format.sh metal.sh analysis.sh analysis.ini
 git commit -m "Programs"
-git add TORQUE.md TORQUE.qsub
+git add metal.md metal.qsub
 git commit -m "TORQUE notes and program"
 git add doc
 git commit -m "Documents"
-git add SecureCloud.sh
-git commit -m "SecureCloud.sh"
+git add tryggve.sh
+git commit -m "tryggve.sh"
 git push
