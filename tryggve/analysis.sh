46c8ae6fd509bd9de5ffdacfb1004ab88abe9825
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-11-24 09:45:40 +0000

    Notes and programs on TRYGGVE

--- tryggve/analysis.sh
+++ tryggve/analysis.sh
@@ -51,44 +51,6 @@ parallel -j1 -C' ' '
 # convert OPG-000001.png -resize 130% OPG-000003.png
 # convert \( OPG-qqman-000001.png -append OPG-qqman-000002.png -append OPG-000003.png -append \) +append OPG-qml.png
 
-echo "--> 1000Genomes reference data"
-
-# individual genotypes
-export EUR=/services/tools/locuszoom/1.4/data/1000G/genotypes/2014-10-14/EUR
-
-seq 22 | \
-parallel 'plink --bfile $EUR/chr{} --recode vcf bgz --out chr{}'
-seq 22 | \
-parallel 'tabix -f -p vcf chr{}.vcf.gz'
-seq 22 | \
-awk -vp=$PWD '{print p "/chr" $1 ".vcf.gz"}' > EUR.list
-vcf-concat --files EUR.list | \
-bcftools norm -d both - | \
-bcftools convert - -O z -o EUR.vcf.gz
-plink --vcf EUR.vcf.gz --make-bed --out EUR
-awk -vFS="\t" '
-{
-  CHR=$1
-  POS=$4
-  a1=$5
-  a2=$6
-  if (a1>a2) snpid="chr" CHR ":" POS "_" a2 "_" a1;
-  else snpid="chr" CHR ":" POS "_" a1 "_" a2
-  print snpid, $2
-
-# 1 CHR
-# 2 SNPID
-# 3 0
-# 4 POS
-# 5 A1
-# 6 A2
-
-}' EUR.bim > EUR.snpid
-
-plink --bfile EUR --update-name EUR.snpid 1 2 --make-bed --out EUR1KG
-plink --bfile EUR1KG --chr 6 --from-mb 25 --to-mb 35 --make-bed --out MHC
-cut -f2 MHC.bim > MHC.snpid
-
 echo "--> clumping"
 
 export rt=$HOME/INF/METAL
@@ -105,7 +67,7 @@ plink --bfile EUR1KG \
       --clump-kb 500 \
       --clump-p1 5e-10 \
       --clump-p2 0.01 \
-      --clump-r2 0.1 \
+      --clump-r2 0.0001 \
       --out $rt/{}
 '
 
@@ -265,28 +227,3 @@ echo "--> remove duplicates"
 #  sort -t $'\t' -k1,1 -k2,2n -k4,4 | \
 #  awk -F '\t' 'BEGIN {prev="";} {key=sprintf("%s\t%s\t%s",$1,$2,$4);if(key==prev) next;print;prev=key;}' \
 #) 
-
-echo "--> SNP/gene databases and indices"
-
-export hg19=/services/tools/locuszoom/1.4/data/database/locuszoom_hg19.db
-sqlite3 $hg19 <<END
--- locuszoom 1.4 tables and indices
-.tables
-.indices
-.separator "\t"
-.header on
-.output gencode.tsv
-select * from gencode;
-.output recomb_rate.tsv
-select * from recomb_rate;
-.output refFlat.tsv
-select * from refFlat;
-.output refsnp_trans.tsv
-select * from refsnp_trans;
-.output snp_pos.tsv
-select * from snp_pos;
-.output snp_set.tsv
-select * from snp_set;
-END
-
-gzip -f snp_pos.tsv
