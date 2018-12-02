ab11363968480cb81c00e3ac63e5cb01b6d7b2f3
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-02 21:17:49 +0000

    Documents and auxiliary files

--- doc/1KG.sh
+++ doc/1KG.sh
@@ -11,9 +11,9 @@
 gzip -f > 1KGp3v5.txt.gz
 
 R --no-save -q <<END
-z <- gzfile("1KGp3v5.txt.gz")
-allele_ref_std <- read.table(z,header=TRUE,as.is=TRUE)
-save(allele_ref_std,file="1KGp3v3.RData")
+  z <- gzfile("1KGp3v5.txt.gz")
+  allele_ref_std <- read.table(z,header=TRUE,as.is=TRUE)
+  save(allele_ref_std,file="1KGp3v3.RData")
 END
 
 # to work on data from LocusZoom 1.4
