fa2a4c8ded9428c3c3b8210dce06e84b6bd4c8c1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-02 21:14:14 +0000

    Notes and programs on TRYGGVE

--- tryggve/qcgwas.R
+++ tryggve/qcgwas.R
@@ -1,22 +1,20 @@
 # 2-12-2018 JHZ
 
-z <- gzfile("/data/jinhua/1KGp3/1KGp3v5.txt.gz")
-allfreq_ref_std <- read.table(z,header=TRUE,as.is=TRUE)
-save(allfreq_ref_std,file="1KGp3v3.RData")
+header_translations <- read.delim("tryggve/header_translations.txt",as.is=TRUE)
 
 library(QCGWAS)
 QC_series(data_files=c("INTERVAL.ARTN.gz","INTERVAL.IFN.gamma.gz","INTERVAL.IL.13.gz"),
 	dir_data="sumstats/INTERVAL",
 	dir_output="work",
 	dir_references=".",
-	header_translations = "tryggve/header_translations.txt",
+	header_translations = header_translations,
 	save_final_dataset = TRUE,
 	HQfilter_FRQ = 0.01,
 	HQfilter_imp = 0.3,
 	QQfilter_FRQ = c(NA, 0.01, 0.03, 0.05, 3),
 	QQfilter_imp = c(NA, 0.3, 0.5, 0.7, 0.9),
 	NAfilter = TRUE,
-	allele_ref_std = "1KGp3v5.RData",
+	allele_ref_std = allele_ref_std,
 	allele_name_std = "OneKG",
 	remove_mismatches = TRUE,
 	allele_ref_alt = "ref_alternative.RData",
