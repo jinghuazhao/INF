c0e0ab4dc29a61726793c45c8e642731df7c2fff
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-03 14:44:33 +0000

    Notes and programs on TRYGGVE

--- tryggve/QCGWAS.R
+++ tryggve/QCGWAS.R
@@ -5,32 +5,35 @@ library(QCGWAS)
 prot <- Sys.getenv("protein")
 src <- "INTERVAL"
 src_file <- paste(src, prot, "gz", sep=".")
-EGCUT <- paste0("EGCUT_INF",c("_autosomal","_male","_female"))
-studies <- c(EGCUT, "INTERVAL", "NSPHS_INF", "ORCADES", "STABILITY", "STANLEY", "VIS")
-QC_files <- paste(studies, prot, "gz", sep=".")
+EGCUT <- paste0("EGCUT_",c("autosomal","X_male","X_female"))
+STANLEY <- paste0("STANLEY_",c("lah1-","swe6-"),prot,".gz")
+studies <- c(EGCUT, "INTERVAL", "NSPHS", "ORCADES", "STABILITY", "VIS")
+QC_files <- c(paste(studies, prot, "gz", sep="."), STANLEY)
 header_translations <- read.delim("tryggve/header_translations.tsv",as.is=TRUE)
 
-QC_GWAS(src_file,
-        dir_data = "sumstats/work",
-        dir_output = "work",
-        dir_references = "/data/jinhua/data/EasyQC",
-        header_translations = header_translations,
-        save_final_dataset = FALSE,
-        HQfilter_FRQ = 0.01,
-        HQfilter_imp = 0.3,
-        QQfilter_FRQ = c(NA, 0.01, 0.03, 0.05, 3),
-        QQfilter_imp = c(NA, 0.3, 0.5, 0.7, 0.9),
-        NAfilter = TRUE,
-        allele_ref_std = "1KGp3v5.RData",
-        allele_name_std = "KG",
-        remove_mismatches = TRUE,
-        allele_ref_alt = NULL,
-        allele_name_alt = "alternative",
-        update_alt = TRUE,    
-        update_savename = "ref_alternative",
-        update_as_rdata = TRUE)
+src_qc <- QC_GWAS(src_file,
+	        dir_data = "sumstats/work",
+	        dir_output = "work",
+	        dir_references = "/data/jinhua/data/EasyQC",
+	        header_translations = header_translations,
+	        save_final_dataset = FALSE,
+	        HQfilter_FRQ = 0.01,
+	        HQfilter_imp = 0.3,
+	        QQfilter_FRQ = c(NA, 0.01, 0.03, 0.05, 3),
+	        QQfilter_imp = c(NA, 0.3, 0.5, 0.7, 0.9),
+	        NAfilter = TRUE,
+	        allele_ref_std = "1KGp3v5.RData",
+	        allele_name_std = "KG",
+	        remove_mismatches = TRUE,
+	        allele_ref_alt = NULL,
+	        allele_name_alt = "alternative",
+	        update_alt = TRUE,    
+	        update_savename = "ref_alternative",
+	        update_as_rdata = TRUE)
+src_qc
 
 QC_series(data_files = QC_files,
+	filename_output = QC_files,
 	dir_data = "sumstats/work",
 	dir_output = "work",
 	dir_references = "/data/jinhua/data/EasyQC",
