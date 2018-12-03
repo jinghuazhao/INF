bac772e7d411ac141486115278b04453e01726fc
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-03 10:58:57 +0000

    Notes and programs on TRYGGVE

--- tryggve/QCGWAS.R
+++ tryggve/QCGWAS.R
@@ -1,10 +1,32 @@
 # 3-12-2018 JHZ
 
 library(QCGWAS)
+
+QC_GWAS(data_files = "INTERVAL.ARTN.gz",
+        dir_data = "sumstats/work",
+        dir_output = "work",
+        dir_references = "/data/jinhua/data/1KG",
+        output_filenames = "INTERVAL.ARTN.gz"",
+        header_translations = "tryggve/header_translations.txt",
+        save_final_dataset = TRUE,
+        HQfilter_FRQ = 0.01,
+        HQfilter_imp = 0.3,
+        QQfilter_FRQ = c(NA, 0.01, 0.03, 0.05, 3),
+        QQfilter_imp = c(NA, 0.3, 0.5, 0.7, 0.9),
+        NAfilter = TRUE,
+        allele_ref_std = "1KGp3v5.RData",
+        allele_name_std = "KG",
+        remove_mismatches = TRUE,
+        allele_ref_alt = NULL,
+        allele_name_alt = "alternative",
+        update_alt = TRUE,    
+        update_savename = "ref_alternative",
+        update_as_rdata = TRUE)
+
 QC_series(data_files=c("INTERVAL.ARTN.gz","ORCADES.ARTN.gz","STABILITY.ARTN.gz"),
-	dir_data="sumstats/work",
-	dir_output="work",
-	dir_references="/data/jinhua/data/1KG",
+	dir_data = "sumstats/work",
+	dir_output = "work",
+	dir_references = "/data/jinhua/data/1KG",
 	output_filenames = c("INTERVAL.ARTN.gz","ORCADES.ARTN.gz","STABILITY.ARTN.gz"),
 	header_translations = "tryggve/header_translations.txt",
 	save_final_dataset = TRUE,
@@ -14,5 +36,10 @@ QC_series(data_files=c("INTERVAL.ARTN.gz","ORCADES.ARTN.gz","STABILITY.ARTN.gz")
 	QQfilter_imp = c(NA, 0.3, 0.5, 0.7, 0.9),
 	NAfilter = TRUE,
 	allele_ref_std = "1KGp3v5.RData",
-	allele_name_std = "OneKG",
-	remove_mismatches = TRUE)
+	allele_name_std = "KG",
+	remove_mismatches = TRUE,
+	allele_ref_alt = "ref_alternative.RData",
+	allele_name_alt = "alternative",
+	update_alt = TRUE,
+	update_savename = "ref_alternative",
+	update_as_rdata = TRUE)
