33d196928cabd9970c464c19cfde6533c99caebf
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-04 15:02:29 +0000

    Notes and programs on TRYGGVE

--- tryggve/QCGWAS.sh
+++ tryggve/QCGWAS.sh
@@ -2,10 +2,13 @@
 
 source tryggve/analysis.ini
 
-declare -a proteins=(ARTN CCL25 CD6 CST5 FGF.5 IFN.gamma IL.13 IL.18R1 IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 LIF MCP.2 NRTN TSLP IL.10RA IL.5 TNF)
-for s in $(seq 1 ${#cohorts[@]}); do
-    export protein=${cohorts[$(( $s-1 ))]}
-    echo $protein
-    if [ ! -d $protein ]; then mkdir $protein; fi
-    R --no-save -q < tryggve/QCGWAS.R > $protein.log
+# mkdir
+declare -a prot=(ARTN CCL25 CD6 CST5 FGF.5 IFN.gamma IL.13 IL.18R1 IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 LIF MCP.2 NRTN IL.10RA IL.5 TNF TSLP)
+for s in $(seq 1 ${#prot[@]}); do
+    export protein=${prot[$(( $s-1 ))]}
+    if [ ! -d work/$protein ]; then mkdir work/$protein; fi
 done
+
+# QCGWAS
+export protein=$1
+R --no-save -q < tryggve/QCGWAS.R > $protein.log
