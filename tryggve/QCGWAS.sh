31c1ed201f67ef748784a7cec59f1dc2b07f1d43
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-04 19:45:00 +0000

    Notes and programs on TRYGGVE

--- tryggve/QCGWAS.sh
+++ tryggve/QCGWAS.sh
@@ -2,13 +2,15 @@
 
 source tryggve/analysis.ini
 
-# mkdir
-declare -a prot=(ARTN CCL25 CD6 CST5 FGF.5 IFN.gamma IL.13 IL.18R1 IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 LIF MCP.2 NRTN IL.10RA IL.5 TNF TSLP)
-for s in $(seq 1 ${#prot[@]}); do
-    export protein=${prot[$(( $s-1 ))]}
-    if [ ! -d work/$protein ]; then mkdir work/$protein; fi
-done
+export rt=$HOME/INF
 
-# QCGWAS
-export protein=$1
-R --no-save -q < tryggve/QCGWAS.R > work/$protein.log
+# work on tryggve and work subdirectories
+
+cd $rt/work
+parallel --env rt -j1 -C' ' '
+echo {}; \
+export protein={}; \
+if [ ! -d {} ]; then mkdir {}; fi; \
+R --no-save -q < $rt/tryggve/QCGWAS.R > {}.log' ::: CCL25 CD6 CST5 FGF.5 IL.13 IL.18R1 IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 LIF MCP.2 NRTN IL.10RA IL.5 TNF
+
+## ARTN IFN.gamma TSLP
