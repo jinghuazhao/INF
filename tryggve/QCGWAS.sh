1c46b73621f12f4ce9724dbecbe0706a5f3fc31a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-05 06:45:16 +0000

    Notes and programs on TRYGGVE

--- tryggve/QCGWAS.sh
+++ tryggve/QCGWAS.sh
@@ -10,8 +10,10 @@ cd $rt
 parallel --env rt -j1 -C' ' '
 echo {}; \
 export protein={}; \
-cd $rt/work; if [ ! -d {} ]; then mkdir {}; fi; cd -; \
-R --no-save -q < $rt/tryggve/QCGWAS.R > {}.log' ::: CCL25 CD6 CST5 FGF.5 IL.13 IL.18R1 IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 LIF MCP.2 NRTN IL.10RA IL.5 TNF
+cd $rt/work; \
+if [ ! -d {} ]; then mkdir {}; fi; \
+R --no-save -q < $rt/tryggve/QCGWAS.R > {}.log; \
+cd -' ::: CCL25 CD6 CST5 FGF.5 IL.13 IL.18R1 IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 LIF MCP.2 NRTN IL.10RA IL.5 TNF
 
 ## ARTN IFN.gamma TSLP
 
