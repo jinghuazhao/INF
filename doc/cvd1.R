ca371e5417c6aa4d5a894d4bb05872ed409af790
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-10 20:11:51 +0000

    removed

--- doc/cvd1.R
+++
@@ -1,6 +0,0 @@
-# 3-11-2018 JHZ
-
-d <- read.delim("cvd1.tsv", as.is=TRUE)
-proteins <- d[c("Olink_name", "gene", "Uniprot")]
-dim(proteins)
-head(proteins, 40)
--- doc/cvd1_annotation.R
+++
@@ -1,5 +0,0 @@
-# 3-11-2018 JHZ
-
-cvd1_annotation <- read.delim("cvd1_annotation.tsv", as.is=TRUE)
-dim(cvd1_annotation)
-head(cvd1_annotation, 40)
