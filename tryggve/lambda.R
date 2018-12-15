18fcc4e85b7aefd39fadca9ca215a328fb60baa4
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-15 08:55:50 +0000

    gc.lambda is now with R/gap

--- tryggve/lambda.R
+++
@@ -1,8 +0,0 @@
-gc.lambda <- function(p) {
-  p <- p[!is.na(p)]
-  n <- length(p)
-  obs <- qchisq(p,1,lower.tail=FALSE)
-  exp <- qchisq(1:n/n,1,lower.tail=FALSE)
-  lambda <- median(obs)/median(exp)
-  return(lambda)
-}
