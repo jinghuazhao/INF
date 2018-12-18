8773a02fa00f5f44b3af0482dc92318a8bce37fe
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-18 15:25:58 +0000

    Notes and programs on TRYGGVE

--- tryggve/QCGWAS.sh
+++ tryggve/QCGWAS.sh
@@ -1,10 +1,7 @@
-# 11-12-2018 JHZ
+# 18-12-2018 JHZ
 
 source tryggve/analysis.ini
-
 export rt=$HOME/INF
-
-# work on tryggve and work subdirectories
 
 cd $rt
 parallel --env rt -j1 -C' ' '
@@ -13,19 +10,17 @@ export protein={}; \
 cd $rt/work; \
 if [ ! -d {} ]; then mkdir {}; fi; \
 cd -; \
-R --no-save -q < $rt/tryggve/QCGWAS.R > $rt/work/{}.log; \
-' ::: ARTN IFN.gamma IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 NRTN TSLP
+R --no-save -q < $rt/tryggve/QCGWAS.R > $rt/work/{}.log' ::: ARTN IFN.gamma IL.1.alpha IL.20 IL.20RA IL.22.RA1 NRTN TSLP
 
-function single_protein_as_argument()
+declare -a prot=(ARTN CCL25 CD6 CST5 FGF.5 IFN.gamma IL.13 IL.18R1 IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 LIF MCP.2 NRTN IL.10RA IL.5 TNF TSLP)
+function protein_array ()
 {
 # mkdir
-declare -a prot=(ARTN CCL25 CD6 CST5 FGF.5 IFN.gamma IL.13 IL.18R1 IL.1.alpha IL.20 IL.20RA IL.22.RA1 IL.24 IL.2RB IL.33 LIF MCP.2 NRTN IL.10RA IL.5 TNF TSLP)
-for s in $(seq 1 ${#prot[@]}); do
-    export protein=${prot[$(( $s-1 ))]}
-    if [ ! -d work/$protein ]; then mkdir work/$protein; fi
-done
-
+  for s in $(seq 1 ${#prot[@]}); do
+      export protein=${prot[$(( $s-1 ))]}
+      if [ ! -d work/$protein ]; then mkdir work/$protein; fi
+  done
 # QCGWAS
-export protein=$1
-R --no-save -q < tryggve/QCGWAS.R > work/$protein.log
+  export protein=$1
+  R --no-save -q < tryggve/QCGWAS.R > work/$protein.log
 }
