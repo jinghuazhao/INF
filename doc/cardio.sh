5021aa99758d09064dfe10ebdec1cdb74a8b46fe
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-28 20:35:18 +0000

    Documents and auxiliary files

--- doc/cardio.sh
+++ doc/cardio.sh
@@ -234,6 +234,33 @@ function format_for_METAL()
   '
 }
 
+echo "--> clumping"
+
+export rt=$HOME/INF/METAL
+sbatch --wait doc/clump.sb
+(
+  grep CHR $rt/*.clumped | \
+  head -1
+  grep -v CHR $rt/*.clumped
+) | \
+sed 's|'"$rt"'/||g;s/.clumped://g' | \
+awk '(NF>1){$3="";print}' | \
+awk '{$1=$1;if(NR==1)$1="prot";print}' > INF1.clumped
+R --no-save -q <<END
+  require(gap)
+  clumped <- read.table("INF1.clumped",as.is=TRUE,header=TRUE)
+  hits <- merge(clumped[c("CHR","BP","SNP","prot")],inf1[c("prot","uniprot")],by="prot")
+  names(hits) <- c("prot","Chr","bp","SNP","uniprot")
+  cistrans <- cis.vs.trans.classification(hits)
+  sink("INF1.clumped.out")
+  with(cistrans,table)
+  sink()
+  sum(with(cistrans,table))
+  pdf("INF1.circlize.pdf")
+  circos.cis.vs.trans.plot(hits="INF1.clumped")
+  dev.off()
+END
+
 function CD6()
 # SUMSTATS for depict
 {
