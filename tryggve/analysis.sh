302222af3b7f811af14b60daa76b6d227bed1949
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-01-03 10:36:57 +0000

    Notes and programs on TRYGGVE

--- tryggve/analysis.sh
+++ tryggve/analysis.sh
@@ -226,8 +226,7 @@ awk '(NR>1){
 export rt=$HOME/INF/METAL
 for p in $(ls METAL/*tbl.gz | sed 's/-1.tbl.gz//g' | xargs -l basename)
 do
-  awk 'NR>1' tryggve/EURLD.bed | \
-  sed 's/chr//g' | \
+  awk 'NR>1{gsub(/chr/,"",$1);print}' tryggve/EURLD.bed | \
   parallel --env p --env rt -C' ' '
   (
     plink --bfile EUR \
