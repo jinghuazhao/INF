3cb0f4b2a5207878b76c7b0fe287aa2d4136a955
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-02-25 15:55:50 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,4 @@
-# 15-12-2018 JHZ
+# 25-2-2019 JHZ
 
 git add .gitignore
 git commit -m ".gitignore"
@@ -8,6 +8,8 @@ git add doc
 git commit -m "Documents and auxiliary files"
 git add SCALLOP_INF1_analysis_plan.*
 git commit -m "Analysis plan"
+git add cardio
+git commit -m "Work on CEU's cardio"
 git add tryggve tryggve.*
 git commit -m "Notes and programs on TRYGGVE"
 git add st.sh
