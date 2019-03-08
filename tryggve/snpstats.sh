0933e59a2e5e8c3b733320d35e3222bddf7dd698
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-03-08 15:08:04 +0000

    Notes and programs on TRYGGVE

--- tryggve/snpstats.sh
+++ tryggve/snpstats.sh
@@ -315,7 +315,7 @@ do
   awk -vOFS="\t" '{ 
      if (NR==1) print "SNPID", "CHR", "POS", "STRAND", "N", "EFFECT_ALLELE", "REFERENCE_ALLELE", "CODE_ALL_FQ",
                       "BETA", "SE", "PVAL", "RSQ", "RSQ_IMP", "IMP", "M", "MAF", "HWE"
-     if print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $18, $14, $15, $16, $17
+     print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $18, $14, $15, $16, $17
   }' | \
   gzip -f > ${s}.${p}.gz
   R --no-save -q <<\ \ END
