2d82ca29702ee0376a6e322571319ccbf1354738
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-03-08 14:59:22 +0000

    Notes and programs on TRYGGVE

--- tryggve/snpstats.sh
+++ tryggve/snpstats.sh
@@ -309,9 +309,9 @@ do
 # $s
   if [ $s == "EGCUT" ]; then export d=EGCUT_INF;export ss=EGCUT_autosomal; else export d=$s;export ss=s;fi
   gunzip -c snpstats/${s}.snpstats.gz > snpstats/${s}.snpstats
-  gunzip -c ${d}/${ss}.${p}.gz | \
+  gunzip -c sumstats/${d}/${ss}.${p}.gz | \
   awk 'NR>1' | \
-  join - snpstats/${s}.snpstats | \
+  join - snpstats/${d}/${s}.snpstats | \
   awk -vOFS="\t" '{ 
      if (NR==1) print "SNPID", "CHR", "POS", "STRAND", "N", "EFFECT_ALLELE", "REFERENCE_ALLELE", "CODE_ALL_FQ",
                       "BETA", "SE", "PVAL", "RSQ", "RSQ_IMP", "IMP", "M", "MAF", "HWE"
