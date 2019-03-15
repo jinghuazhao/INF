dac3665ca9f9d5409c12f71dce6487a2768cf5f5
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-03-15 16:38:26 +0000

    Notes and programs on TRYGGVE

--- tryggve/sw34.sh
+++ tryggve/sw34.sh
@@ -17,19 +17,23 @@ function plink20()
   export m=map
 }
 
-module load bgen/20180807
-module load $module
-export rt=/data/andmala/STANLEY
-
 plink20
+module load $module
 
-ls $rt/*gz | \
-sed 's/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr//g;s/.out.dosage.fam//g;s/.out.dosage.gz//g;s/.out.dosage.map//g' | \
+export rt=/data/andmala/STANLEY
+ls $rt/ | \
+grep .gz | \
+grep -v test | \
+sed 's/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr//g;s/.out.dosage.fam//g;s/.out.dosage.gz//g;s/.out.dosage.map//g' > sw34.list
+cat sw34.list | \
 head -2 | \
 parallel -j5 --env rt --env c --env d --env p --env m -C' ' '
   $c --${d} $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.gz \
      --${p} $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.fam \
      --${m} $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.map \
-     --bgen --log {} --out {}
+     --export bgen-1.1 --out {}
 '
+
 module unload $module
+module load bgen/20180807
+cat-bgen -g $(awk '{$1=$1 ".bgen";printf $1 " "}' sw34.list) -og sw34
