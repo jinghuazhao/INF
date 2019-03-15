500a95acb046e7943f8e2225cc8ec09ded02fc5f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-03-15 15:55:47 +0000

    Notes and programs on TRYGGVE

--- tryggve/sw34.sh
+++ tryggve/sw34.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/bash
 
-module load plink2/1.90beta5.4
+function plink1()
+{
+  export module=plink2/1.90beta5.4
+  export c=plink
+  export d=import-dosage
+  export p=fam
+  export m=map
+}
+function plink2()
+{
+  export module=plink2/2.00alpha20181028
+  export c=plink2
+  export d=import-dosage
+  export p=psam
+  export m=map
+}
 
-export rt=/data/andmala/STANLEY 
-ls $rt | \
+plink2
+module load $module
+
+export rt=/data/andmala/STANLEY
+ls $rt/*gz | \
+head -2 | \
 sed 's/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr//g;s/.out.dosage.fam//g;s/.out.dosage.gz//g;s/.out.dosage.map//g' | \
-parallel -j5 --env rt -C' ' '
-  plink --dosage $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.gz \
-        --fam $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.fam \
-        --map $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.map
-        --freq --out work/sw34.{}.freq
+parallel -j5 --env rt --env c --env d --env p --env m -C' ' '
+  $c --${d} $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.gz \
+     --${p} $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.fam \
+     --${m} $rt/dos_bip_sw34_eur_rk-qc.hg19.ch.fl.chr{}.out.dosage.map \
+     --out {}
 '
+module unload $module
