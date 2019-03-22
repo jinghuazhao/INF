66fa4c34662a67ec30a8b5180865fcb2535e5a9c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-03-22 11:51:10 +0000

    Notes and programs on TRYGGVE

--- tryggve/metal.sh
+++ tryggve/metal.sh
@@ -1,79 +1,93 @@
-# 19-3-2019 JHZ
+# 22-3-2019 JHZ
 
-## build the lists
-if [ ! -d METAL ]; then mkdir METAL; fi
-(
-for dir in INTERVAL BioFinder EGCUT MadCam KORA NSPHS ORCADES STABILITY STANLEY VIS
-do
-   ls sumstats/$dir | \
-   awk -vdir=$dir '{
-      s=$1
-      gsub(dir,"",s)
-      gsub(/EGCUT|NSPHS/,"",s)
-      gsub(/\_autosomal|\_X_female|\_X_male|\_lah1-|\_swe6-|.gz|@/,"",s)
-      gsub(/^\./,"",s)
-      gsub(/@/,"",$1)
-      print s " /data/" ENVIRON["USER"] "/INF/sumstats/" dir "/" $1
-   }'
-done
-) | \
-awk '{print $0, NR}' > METAL/METAL.tmp
+function METAL_list()
+{
+# build the lists
+  if [ ! -d METAL ]; then mkdir METAL; fi
+  (
+  for dir in INTERVAL BioFinder EGCUT MadCam KORA NSPHS ORCADES RECOMBINE STABILITY STANLEY VIS
+  do
+     ls sumstats/$dir | \
+     awk -vdir=$dir '{
+        s=$1
+        gsub(dir,"",s)
+        gsub(/EGCUT|NSPHS/,"",s)
+        gsub(/\_autosomal|\_X_female|\_X_male|\_lah1.|\_swe6.|.gz|@/,"",s)
+        gsub(/^\./,"",s)
+        gsub(/@/,"",$1)
+        print s " /data/" ENVIRON["USER"] "/INF/sumstats/" dir "/" $1
+     }'
+  done
+  ) | \
+  awk '{print $0, NR}' | \
+  sort -k1,1 > METAL/METAL.list
+}
 
-sort -k1,1 METAL/METAL.tmp > METAL/METAL.list
+function METAL_file()
+{
 # generate METAL command files
-for p in $(cut -f1 inf1.list)
-do
-(
-   echo SEPARATOR TAB
-   echo COLUMNCOUNTING STRICT
-   echo CHROMOSOMELABEL CHR
-   echo POSITIONLABEL POS
-   echo CUSTOMVARIABLE N
-   echo LABEL N as N
-   echo TRACKPOSITIONS ON
-   echo AVERAGEFREQ ON
-   echo MINMAXFREQ ON
-   echo ADDFILTER N ">=" 10
-   echo ADDFILTER PVAL ">" 0
-   echo MARKERLABEL SNPID
-   echo ALLELELABELS EFFECT_ALLELE REFERENCE_ALLELE
-   echo EFFECTLABEL BETA
-   echo PVALUELABEL PVAL
-   echo WEIGHTLABEL N
-   echo FREQLABEL CODE_ALL_FQ
-   echo STDERRLABEL SE
-   echo SCHEME STDERR
-   echo GENOMICCONTROL OFF
-   echo OUTFILE $HOME/INF/METAL/$p- .tbl
-   echo $p | join METAL/METAL.list - | sort -k3,3n | awk '{print "PROCESS", $2}'
-   echo ANALYZE
-   echo CLEAR
-) > METAL/$p.metal
-done
+  for p in $(cut -f1 inf1.list)
+  do
+  (
+     echo SEPARATOR TAB
+     echo COLUMNCOUNTING STRICT
+     echo CHROMOSOMELABEL CHR
+     echo POSITIONLABEL POS
+     echo CUSTOMVARIABLE N
+     echo LABEL N as N
+     echo TRACKPOSITIONS ON
+     echo AVERAGEFREQ ON
+     echo MINMAXFREQ ON
+     echo ADDFILTER N ">=" 10
+     echo ADDFILTER PVAL ">" 0
+     echo MARKERLABEL SNPID
+     echo ALLELELABELS EFFECT_ALLELE REFERENCE_ALLELE
+     echo EFFECTLABEL BETA
+     echo PVALUELABEL PVAL
+     echo WEIGHTLABEL N
+     echo FREQLABEL CODE_ALL_FQ
+     echo STDERRLABEL SE
+     echo SCHEME STDERR
+     echo GENOMICCONTROL OFF
+     echo OUTFILE $HOME/INF/METAL/$p- .tbl
+     echo $p | \
+     join METAL/METAL.list - | \
+     sort -k3,3n | \
+     awk '{print "PROCESS", $2}'
+     echo ANALYZE
+     echo CLEAR
+  ) > METAL/$p.metal
+  done
+}
 
+function METAL_QCGWAS()
+{
 # all in one directory to get ready for QCGWAS
-cd $HOME/INF/sumstats/work
-cat $HOME/INF/METAL/METAL.tmp | \
-parallel -j10 -C' ' '
-  echo {2}; \
-  ln -sf {2}
-'
-cd -
+  cd $HOME/INF/sumstats/work
+  cat $HOME/INF/METAL/METAL.list | \
+  parallel -j10 -C' ' '
+    echo {2}; \
+    ln -sf {2}
+  '
+  cd -
+}
 
+function METAL_analysis()
+{
 # conduct the analysis 
 # module load metal/20110325 parallel/20170822
-export rt=$HOME/INF
-ls METAL/*.metal | \
-sed 's/.metal//g' | \
-parallel -j5 --env rt -C' ' '
-  metal $rt/{}.metal 2>&1 | \
-  tee $rt/{}-1.tbl.log; \
-  gzip -f $rt/{}-1.tbl
-'
-
-# obtain largest M -- the union of SNP lists as initially requested by NSPHS
+  export rt=$HOME/INF
+  ls METAL/*.metal | \
+  sed 's/.metal//g' | \
+  parallel -j5 --env rt -C' ' '
+    metal $rt/{}.metal 2>&1 | \
+    tee $rt/{}-1.tbl.log; \
+    gzip -f $rt/{}-1.tbl
+  '
+}
 
 function largest_M()
+# the union of SNP lists as initially requested by NSPHS
 {
   for dir in EGCUT INTERVAL LifeLinesDeep ORCADES PIVUS STABILITY STANLEY ULSAM VIS
   do
@@ -93,3 +107,5 @@ function largest_M()
   uniq > /data/jinhua/M/M.union
   cd -
 }
+
+$1
