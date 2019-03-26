4b63f6bf5c100353bc82e1a388a3a6f55fcd02b2
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-03-26 12:20:30 +0000

    Notes and programs on TRYGGVE

--- tryggve/metal.sh
+++ tryggve/metal.sh
@@ -64,7 +64,6 @@ function METAL_QCGWAS()
 # all in one directory to get ready for QCGWAS
   cd $HOME/INF/sumstats/work
   cat $HOME/INF/METAL/METAL.list | \
-  grep -v BDNF | \
   parallel -j10 -C' ' '
     echo {2}; \
     ln -sf {2}
@@ -79,7 +78,6 @@ function METAL_analysis()
   export rt=$HOME/INF
   ls METAL/*.metal | \
   sed 's/.metal//g' | \
-  grep -v BDNF | \
   parallel -j5 --env rt -C' ' '
     metal $rt/{}.metal 2>&1 | \
     tee $rt/{}-1.tbl.log; \
