eddb5bf1498d9a01cb80b01a3e15fdf842b7d33a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-04-09 19:21:49 +0100

    Notes and programs on TRYGGVE

--- tryggve/analysis.sh
+++ tryggve/analysis.sh
@@ -133,6 +133,29 @@ function fp()
   ) | \
   sed 's|/data/jinhua/INF/sumstats||g;s/.gz//g' > INF1.clumped.all
   R -q --no-save <<\ \ END
+    test_forest <- function()
+    {
+      tabletext <- cbind(c("Study",study,"Summary"),
+                           c("Effect",format(BETA,digits=3),format(tbl[i,"Effect"],digits=3)),
+                           c("SE",format(SE,digits=3),format(tbl[i,"StdErr"],digits=3)),
+                           c("N",N,tbl[i,"N"]))
+      print(tabletext)
+      forestplot(tabletext,
+                 c(NA,BETA,tbl[i,"Effect"]),
+                 c(NA,BETA-1.96*SE,tbl[i,"Effect"]-1.96*tbl[i,"StdErr"]),
+                 c(NA,BETA+1.96*SE,tbl[i,"Effect"]+1.96*tbl[i,"StdErr"]),
+                 zero=0,
+                 is.summary=c(TRUE,rep(FALSE,length(BETA)),TRUE),
+                 boxsize=0.75,
+                 col=meta.colors(box="royalblue",line="darkblue", summary="royalblue"))
+      title(title)
+      metaplot(BETA,SE,N,
+               labels=sprintf("%s (%.3f %.3f %.0f)",study,BETA,SE,N),
+               xlab="Effect distribution",ylab="",xlim=c(-1.5,1.5),
+               summn=tbl[i,"Effect"],sumse=tbl[i,"StdErr"],sumnn=tbl[i,"N"],
+               colors=meta.colors(box="red",lines="blue", zero="green", summary="red", text="black"))
+      title(title)
+    }
     tbl <- read.delim("INF1.clumped.tbl",as.is=TRUE)
     tbl <- within(tbl, {
       prot <- sapply(strsplit(Chromosome,":"),"[",1)
@@ -153,7 +176,7 @@ function fp()
       prot <- substring(study.prot,pos+1)
     })
     require(rmeta)
-    pdf("INF1.fp.pdf")
+    pdf("INF1.fp.pdf",width=8.75,height=5)
     for(i in 1:nrow(tbl))
     {
        p <- tbl[i,"prot"]
@@ -178,29 +201,12 @@ function fp()
          print(cbind(A1,A2,EFFECT_ALLELE,REFERENCE_ALLELE,a1,a2,format(BETA,digits=3),format(BETA*c,digits=3)))
          BETA <- BETA * c
          title <- sprintf("%s [%s (%s/%s) N=%.0f]",p,m,A1,A2,tbl[i,"N"])
-         tabletext <- cbind(c("Study",study,"Summary"),
-                              c("Effect",format(BETA,digits=3),format(tbl[i,"Effect"],digits=3)),
-                              c("SE",format(SE,digits=3),format(tbl[i,"StdErr"],digits=3)),
-                              c("N",N,tbl[i,"N"]))
-         print(tabletext)
-         forestplot(tabletext,
-                    c(NA,BETA,tbl[i,"Effect"]),
-                    c(NA,BETA-1.96*SE,tbl[i,"Effect"]-1.96*tbl[i,"StdErr"]),
-                    c(NA,BETA+1.96*SE,tbl[i,"Effect"]+1.96*tbl[i,"StdErr"]),
-                    zero=0,
-                    is.summary=c(TRUE,rep(FALSE,length(BETA)),TRUE),
-                    boxsize=0.75,
-                    col=meta.colors(box="royalblue",line="darkblue", summary="royalblue"))
-         title(title)
          require(meta)
          mg <- metagen(BETA,SE,sprintf("%s (%.0f)",study,N),title=title)
-         forest(mg,layout="JAMA")
-         metaplot(BETA,SE,N,
-                  labels=sprintf("%s (%.3f %.3f %.0f)",study,BETA,SE,N),
-                  xlab="Effect distribution",ylab="",xlim=c(-1.5,1.5),
-                  summn=tbl[i,"Effect"],sumse=tbl[i,"StdErr"],sumnn=tbl[i,"N"],
-                  colors=meta.colors(box="red",lines="blue", zero="green", summary="red", text="black"))
-         title(title)
+         forest(mg,colgap.forest.left = "1cm")
+         require(grid)
+         grid.text(title,0.5,0.9)
+#        test_forest()
        })
     }
     dev.off()
