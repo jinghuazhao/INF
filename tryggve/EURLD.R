51eb6b2436b9c93f3df3f562210a0a70e2002c64
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-04-23 11:02:22 +0100

    Notes and programs on TRYGGVE

--- tryggve/EURLD.R
+++ tryggve/EURLD.R
@@ -1,15 +1,15 @@
-# 21-4-2019 JHZ
+# 23-4-2019 JHZ
 
 EURLD <- read.delim("tryggve/EURLD.bed")
 EURLD <- within(EURLD, {
            dist <- round((End-Start)/2/1000)
-           in250k <- ifelse(dist<250,0,1)
-           in500k <- ifelse(dist<500,0,1)
-           in10m <- ifelse(dist<10000,0,1)
+           in250k <- ifelse(dist<250,1,0)
+           in500k <- ifelse(dist<500,1,0)
+           in10m <- ifelse(dist<10000,1,0)
          })
 ord <- with(EURLD, order(dist))
 with(EURLD,summary(dist))
 with(EURLD,table(in250k))
 with(EURLD,table(in500k))
 with(EURLD,table(in10m))
-EURLD[ord,]
+write.csv(cbind(EURLD[ord,],line=1:nrow(EURLD)),file="tryggve/EURLD.csv",quote=FALSE,row.names=FALSE)
