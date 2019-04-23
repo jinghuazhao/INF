bfc267c0de835cdb2022cd9fe79bf5d12228038d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-04-23 11:09:52 +0100

    Notes and programs on TRYGGVE

--- tryggve/EURLD.R
+++ tryggve/EURLD.R
@@ -2,12 +2,13 @@
 
 EURLD <- read.delim("tryggve/EURLD.bed")
 EURLD <- within(EURLD, {
-           dist <- round((End-Start)/2/1000)
-           in250k <- ifelse(dist<250,1,0)
-           in500k <- ifelse(dist<500,1,0)
-           in10m <- ifelse(dist<10000,1,0)
+           dist_kb <- round((End-Start)/1000)
+           flanking <- round((End-Start)/2/1000)
+           in250k <- ifelse(flanking<250,1,0)
+           in500k <- ifelse(flanking<500,1,0)
+           in10m <- ifelse(flanking<10000,1,0)
          })
-ord <- with(EURLD, order(dist))
+ord <- with(EURLD, order(dist_kb))
 with(EURLD,summary(dist))
 with(EURLD,table(in250k))
 with(EURLD,table(in500k))
