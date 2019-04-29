3e001241d05d877df5029d79fc5f2dc930eb5654
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-04-29 16:19:48 +0100

    Notes and programs on TRYGGVE

--- cardio/aild.sh
+++ cardio/aild.sh
@@ -4,7 +4,7 @@
 (
   echo -e "prot\tregion\t$(cat *jma.cojo | head -1)"
   cat INF1.ailist | \
-  parallel -C' ' 'awk "NR>1 {print p, r, \$0}" OFS="\t" p={1} r={2} {1}-{2}.jma.cojo'
+  parallel -j1 -C' ' 'awk "NR>1 {print p, r, \$0}" OFS="\t" p={1} r={2} {1}-{2}.jma.cojo'
 ) > INF1.jma
 
 R --no-save -q <<END
