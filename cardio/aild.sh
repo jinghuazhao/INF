4ba3942e6a2370beeada2feccf87cf2741413e1e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-04-30 12:05:26 +0100

    Work on CEU's cardio

--- cardio/aild.sh
+++ cardio/aild.sh
@@ -1,16 +1,20 @@
-# 29-4-2019 JHZ
+# 30-4-2019 JHZ
 
 # code to summarise protein-region results
 (
   echo -e "prot\tregion\t$(cat *jma.cojo | head -1)"
   cat INF1.ailist | \
   parallel -j1 -C' ' 'awk "NR>1 {print p, r, \$0}" OFS="\t" p={1} r={2} {1}-{2}.jma.cojo'
-) > INF1.jma
+) > INF1.txt
 
 R --no-save -q <<END
-  jma <- read.delim("INF1.jma")
+  jma <- read.delim("INF1.txt")
   jma <- within(jma, {r2=LD_r^2})
   dim(jma)
   subset(jma,r2>0.3)
   subset(jma,r2>0.1)
 END
+
+# for tryggve/analysis.sh fp
+
+cut -f1,3-16 INF1.txt > INF1.jma
