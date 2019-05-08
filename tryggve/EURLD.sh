06857b5a0e5098b8ef5738dcb75c650bf2677010
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-08 21:35:22 +0100

    Notes and programs on TRYGGVE

--- tryggve/EURLD.sh
+++ tryggve/EURLD.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+module load gcc/5.2.0
+
 cd tryggve
 
 awk -vOFS="\t" '{
   if(NR==1) print "#chrom","Start","End", "r"
   print "chr" $1, $2, $3, $4
 }' high-LD-regions-hg19.txt > high-LD-regions-hg19.bed
-
-module load gcc/5.2.0
 
 (
   head -1 EURLD.bed
