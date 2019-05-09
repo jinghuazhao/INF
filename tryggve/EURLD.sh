2c923b01b5c9de9cc543a0bcaa036c7985120385
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-09 22:23:48 +0100

    Notes and programs on TRYGGVE

--- tryggve/EURLD.sh
+++ tryggve/EURLD.sh
@@ -9,9 +9,6 @@ awk -vOFS="\t" '{
   print "chr" $1, $2, $3, $4
 }' high-LD-regions-hg19.txt > high-LD-regions-hg19.bed
 
-(
-  head -1 EURLD.bed
-  bedtools intersect -v -a EURLD.bed -b high-LD-regions-hg19.bed
-) > EURLD-no-high-LD-regions-hg19.bed
+bedtools intersect -v -header -a EURLD.bed -b high-LD-regions-hg19.bed > EURLD-no-high-LD-regions-hg19.bed
 
 cd -
