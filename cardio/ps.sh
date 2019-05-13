0c7313b7fd4e1029c8b2287b0724202e18c349f9
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-13 11:37:16 +0100

    Work on CEU's cardio

--- cardio/ps.sh
+++ cardio/ps.sh
@@ -1,20 +1,60 @@
 #!/bin/bash
 
+export TMPDIR=/scratch/jhz22/tmp
+export INF=$HOME/INF
+
+#### snpstats
+
+function rsid()
+{
+  cd $INF/work
+  gunzip -c INTERVAL.rsid.gz | \
+  awk '$2!="."' > INTERVAL.rsid
+  cut -f4 $INF1.jma | \
+  awk 'NR>1' | \
+  uniq | \
+  sort -k1,1 | \
+  join - INTERVAL.rsid > $HOME/ps/INF1.rsid
+  cd -
+}
+
+#### PhenoScanner
+
 . /etc/profile.d/modules.sh
 module load default-cardio
 module load use.own
 
+function ps_v1.1 ()
+# This version works with chr:pos
+{
 # PhenoScanner uses old R
-module load R/3.4.2 phenoscanner/phenoscanner_v1.1
+  module load R/3.4.2 phenoscanner/phenoscanner_v1.1
+  export R_LIBS=
+
+  cd ps
+  for i in $(cut -f1 INF1.jma | awk 'NR>1' | uniq )
+  do
+    echo $i
+    awk -vprot=$i '$1==prot{print "chr" $2 ":" $4}' INF1.jma > $i.ps
+  # default -r 0.6
+    phenoscanner -c All -l 1000G -p 0.0000001 -i $i.ps -o $i
+  done
+  cd -
+}
+
+module load R/3.4.2 phenoscanner/phenoscanner_v2
 export R_LIBS=
 
-cd ps
+cd $INF/ps
+ln -sf $INF/cojo/aild-indel/INF1.jma
 for i in $(cut -f1 INF1.jma | awk 'NR>1' | uniq )
 do
-  echo $i
-  awk -vprot=$i '$1==prot{print "chr" $2 ":" $4}' INF1.jma > $i.ps
-# default -r 0.6
-  phenoscanner -c All -l 1000G -p 0.0000001 -i $i.ps -o $i
+  echo $i;
+  awk -vprot=$i '$1==prot{print $4}' INF1.jma | \
+  sort -k1,1 | \
+  join INF1.rsid - | \
+  awk '{print $2}'> $i.ps;
+  phenoscanner -s T -c All -x EUR -p 0.0000001 -i $i.ps -o $i
 done
 cd -
 
