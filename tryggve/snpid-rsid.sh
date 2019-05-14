3f155782ba8f4af1277349446f611b6f67306656
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-14 16:55:34 +0100

    Notes and programs on TRYGGVE

--- tryggve/snpid-rsid.sh
+++ tryggve/snpid-rsid.sh
@@ -1,19 +1,24 @@
-function INTERVAL()
-{
+# 14-5-2019 JHZ
+
+# ORCADES, STABILITY, VIS and MadCam actually contains INFO
+# INTERVAL and KORA has SNPTEST outputs as well as qctool -snp-stats
+# ORCADES, STABILITY and VIS have chromosome as NA
+# BioFinder, NSPHS, RECOMBINE and STANLEY are pending on qctool -snp-stats
+
 (
   seq 22 | \
   parallel -j1 -C' ' '
     gunzip -c /data/jinhua/data/INTERVAL/impute_{}_interval.snpstats.gz | \
     awk -vOFS="\t" "{
-      if(NR==1) print \"SNPID\",\"SNP\";
+      if(NR==1) print \"SNPID\",\"SNP\",\"a1\",\"a2\";
       else {
         CHR=\$3
         sub(/^0/,\"\",CHR)
         POS=\$4
         a1=\$5
         a2=\$6
-        if (a1>a2) print \"chr\" CHR \":\" POS \"_\" a2 \"_\" a1, \$1;
-        else print \"chr\" CHR \":\" POS \"_\" a1 \"_\" a2, \$1
+        if (a1>a2) print \"chr\" CHR \":\" POS \"_\" a2 \"_\" a1, \$1, a2, a1;
+        print \"chr\" CHR \":\" POS \"_\" a1 \"_\" a2, \$1, a1, a2
       }
     }"
   '
@@ -21,7 +26,6 @@ function INTERVAL()
 awk '(NR==1||$1!="SNPID")' | \
 sort -k1,1 | \
 gzip -f > work/INTERVAL.rsid.gz
-}
 
 # gunzip -c impute_1_interval.snpstats.gz | \
 # head -1 | \
@@ -46,5 +50,3 @@ gzip -f > work/INTERVAL.rsid.gz
 #17 missing
 #18 missing_calls
 #19 information
-
-INTERVAL
