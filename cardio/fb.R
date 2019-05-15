d0720b7686096e60d544cdcbc12a658fc4f010e0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-15 16:46:02 +0100

    Work on CEU's cardio

--- cardio/fb.R
+++ cardio/fb.R
@@ -1,10 +1,15 @@
 # 15-5-2019 JHZ
 
 jma <- read.delim("snps/cojo/INF1.jma",as.is=TRUE)
+jma <- within(jma, {
+       F <- ifelse(b<0, 1-freq, freq)
+       B <- abs(b)
+       MAF <- ifelse(freq < 0.5, freq, 1-freq)
+})
 pdf("work/fb.pdf")
 with(jma, {
   print(cor(b,bJ))
-  plot(freq,b,cex=0.6)
+  plot(MAF,B,cex=0.6)
   plot(b,bJ,cex=0.6)
 })
 dev.off()
