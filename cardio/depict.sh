0d72149dc5bec7284904b42cebbe6ee5fce1ac3f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-05-21 12:06:42 +0100

    Work on CEU's cardio

--- cardio/depict.sh
+++ cardio/depict.sh
@@ -21,8 +21,8 @@ R --no-save <<END
     m <- within(nest_join(tbl,rsid),{rsid <- unlist(lapply(lapply(y,"[[",1),"[",1))})
     isna <- with(m, is.na(rsid))
     t <- within(m, {rsid[isna] <- MarkerName[isna]})
-    h <- c("Allele1","Allele2","Freq1","Effect","StdErr","P.value","N","Chromosome","Position","MarkerName")
-    s <- subset(aggregate(x = t[h], by = list(t$rsid), FUN = "min"), substr(Group.1,1,2)=="rs")
+    h <- c("Allele1","Allele2","Freq1","Effect","StdErr","P.value","N","Chromosome","Position")
+    s <- subset(aggregate(x = t[h], by = list(with(t,rsid)), FUN = "min"), substr(Group.1,1,2)=="rs")
     names(s)[1] <- "SNP"
     write.table(s,file="INF1.sumstats",col.names=FALSE,row.names=FALSE,quote=FALSE)
 END
