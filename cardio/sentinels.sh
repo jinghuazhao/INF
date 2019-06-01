e00c473a28eec2c876c2068e9b88b5d0cd98c3b7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-01 15:56:37 +0100

    Work on CEU's cardio

--- cardio/sentinels.sh
+++ cardio/sentinels.sh
@@ -1,35 +1,32 @@
-#!/bin/bash
-# 31-5-2019 JHZ
+# 1-6-2019 JHZ
 
-export rt=/scratch/jhz22/INF/snps/clump0
-cut -d' ' -f3 $rt/INF1.clumped | \
-awk 'NR>1' | \
-sort -k1,1 | \
-uniq | \
-join work/INTERVAL.rsid - > INF1.rsid
-
-R --no-save <<END
-  prot <- MarkerName <- NA
-  tbl <- read.table("/scratch/jhz22/INF/snps/clump0/INF1.clumped",as.is=TRUE,header=TRUE)
-  tbl <- tbl[setdiff(names(tbl), c("NSIG", "S05", "S01", "S001", "S0001", "SP2"))]
-  rsid <- read.table("INF1.rsid",as.is=TRUE,col.names=c("SNP","rsid"))
-  requireNamespace("dplyr")
-  d <- dplyr::nest_join(tbl,rsid)
-  dy <- d["y"]
-  rsid <- unlist(dy)
-  d <- d[setdiff(names(d),"y")]
-  t <- cbind(d,rsid)
-  ord <- with(t,order(prot,CHR))
-  tbl <- t[ord,]
-  write.table(tbl,file="INF1.clumped",col.names=FALSE,row.names=FALSE,quote=FALSE)
-END
-
-export rt=/scratch/jhz22/INF/snps/cojo
+ls METAL/*-1.tbl.gz | \
+sed 's|METAL/||g;s/-1.tbl.gz//g' | \
+parallel -j3 -C' ' '
 (
-  sort -k1,1 -k2,2n ${rt}/INF1.lz | \
-  parallel --env rt -j1 -C' ' '
-    awk "NR>1 && \$2 <=5e-10" $rt/lz/{1}-{2}.lz | \
-    sort -k2,2g | \
-    awk -vOFS="\t" -vprot={1} -vregion={2} "NR==1{print "prot", "region", \$0}" 
-  '
-) > sentinels.txt
+# zcat METAL/{}-1.tbl.gz | head -1
+  zcat METAL/{}-1.tbl.gz | awk "NR>1 && length(\$4)==1 && length(\$5)==1 && \$12<5e-10" | sort -k1,1n -k2,2n
+) | gzip -f > {}.p.gz 
+'
+module load bedtools/2.27.1
+
+for p in $(ls METAL/*-1.tbl.gz | sed 's|METAL/||g;s/-1.tbl.gz//g')
+do
+  (
+    zcat ${p}.tbl.gz | head -1 | awk -vOFS="\t" '{$1="Chrom";$2="Start" "\t" "End";print}'
+    zcat ${p}.p.gz | \
+    awk -vOFS="\t" '{$1="chr" $1; start=$2-1;$2=start "\t" $2;print}'
+  ) | bedtools subtract -header -a - -b tryggve/high-LD-regions-hg19.bed > ${p}.p
+# echo $(zcat ${p}.p.gz | wc -l) $(wc -l ${p}.p)
+  export lines=$(wc -l ${p}.p|cut -d' ' -f1)
+  if [ $lines -eq 1 ]; then
+    echo removing ${p} with $lines lines
+    rm ${p}.p
+  fi
+done
+
+for prot in $(ls *.p | sed 's|\.p||g')
+do 
+  export prot=${prot}
+  R --no-save -q < sentinels.R > ${prot}.o
+done
