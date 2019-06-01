dc06feed9ee4472c1aaf932b41bdc12614d01195
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-01 16:16:32 +0100

    Notes and programs on TRYGGVE

--- tryggve/sentinels.sh
+++ tryggve/sentinels.sh
@@ -1,5 +1,8 @@
 # 1-6-2019 JHZ
 
+module load bedtools/2.27.1
+
+# extract all significant SNPs
 ls METAL/*-1.tbl.gz | \
 sed 's|METAL/||g;s/-1.tbl.gz//g' | \
 parallel -j3 -C' ' '
@@ -7,13 +10,12 @@ parallel -j3 -C' ' '
 # zcat METAL/{}-1.tbl.gz | head -1
   zcat METAL/{}-1.tbl.gz | awk "NR>1 && length(\$4)==1 && length(\$5)==1 && \$12<5e-10" | sort -k1,1n -k2,2n
 ) | gzip -f > {}.p.gz 
-'
-module load bedtools/2.27.1
 
+# removing those in high LD regions'
 for p in $(ls METAL/*-1.tbl.gz | sed 's|METAL/||g;s/-1.tbl.gz//g')
 do
   (
-    zcat ${p}.tbl.gz | head -1 | awk -vOFS="\t" '{$1="Chrom";$2="Start" "\t" "End";print}'
+    zcat METAL/${p}-1.tbl.gz | head -1 | awk -vOFS="\t" '{$1="Chrom";$2="Start" "\t" "End";print}'
     zcat ${p}.p.gz | \
     awk -vOFS="\t" '{$1="chr" $1; start=$2-1;$2=start "\t" $2;print}'
   ) | bedtools subtract -header -a - -b tryggve/high-LD-regions-hg19.bed > ${p}.p
@@ -25,6 +27,7 @@ do
   fi
 done
 
+# find sentinels
 for prot in $(ls *.p | sed 's|\.p||g')
 do 
   export prot=${prot}
