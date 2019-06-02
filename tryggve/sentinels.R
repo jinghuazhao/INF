af1b7131b43b88fd889d39f5862e1aeb8581cb6e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-02 08:15:34 +0100

    Notes and programs on TRYGGVE

--- tryggve/sentinels.R
+++ tryggve/sentinels.R
@@ -12,23 +12,23 @@ pp <- function(p,st,debug=FALSE,flanking=1e6)
   u <- p[nrow(p),"End"]
   len <- with(p,max(s))
   if (len < flanking) {
-    pmin <- with(p,min(P.value))
-    x <- subset(p, P.value==pmin)
+    p1 <- with(p,min(P.value))
+    x <- subset(p, P.value==p1)
     m <- x[1,"End"]
-    cat(prot, l, "-", u, "d =", u-l, "m =", m, "p =", pmin, "row =", row.names(x), "(case 1)\n")
+    cat(prot, l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", row.names(x), "(case 1)\n")
   }
   else
   {
     s <- subset(p,s < flanking)
-    pmin <- with(s,min(P.value))
-    x <- subset(s, P.value==pmin)
+    p1 <- with(s,min(P.value))
+    x <- subset(s, P.value==p1)
     m <- x[1,"End"]
     t <- subset(p, m+flanking < End)
-    qmin <- with(t,min(P.value))
-    y <- subset(t, P.value==pmin)
+    p2 <- with(t,min(P.value))
+    y <- subset(t, P.value==p2)
     u <- p[nrow(t), "End"]
-    if (qmin>pmin) {
-      cat(prot, l, "-", u, "d =", u-l, "m =", m, "p =", pmin, "row =", row.names(x), "(case 2)\n")
+    if (p2 > p1) {
+      cat(prot, l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", row.names(x), "(case 2)\n")
       pp(p,row.names(t[nrow(t),]))
     }
     else pp(p,row.names(y[nrow(y),]))
