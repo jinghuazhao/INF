ef6b2685d1f7759f3122940f74975d64b1542189
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-02 21:56:05 +0100

    Notes and programs on TRYGGVE

--- tryggve/sentinels.R
+++ tryggve/sentinels.R
@@ -1,6 +1,6 @@
 # 2-6-2019 JHZ
 
-pp <- function(p,st,debug=FALSE,flanking=1e6)
+pp <- function(p,st,debug=FALSE,flanking=1e+6)
 {
   nr <- nrow(p)
   z <- within(p[st:nr,],{
@@ -10,16 +10,13 @@ pp <- function(p,st,debug=FALSE,flanking=1e6)
   if (debug) print(z[c("Chrom","End","d","s","MarkerName","P.value")])
   l <- z[1, "End"]
   u <- z[nrow(p), "End"]
-  len <- with(z, max(s))
-  if (len < flanking) {
+  if (z[nrow(z),s) < flanking) {
     p1 <- with(z, min(P.value))
     x <- subset(z, P.value==p1)
     r1 <- row.names(x)[1]
     m <- x[1,"End"]
-    cat("prottein =",prot, l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", r1, "(case 1)\n")
-  }
-  else
-  {
+    cat("Prottein =", prot, chr, ":", l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", r1, "(case 1)\n")
+  } else {
     s <- subset(z, s < flanking)
     p1 <- with(s, min(P.value))
     x <- subset(s, P.value==p1)
@@ -27,7 +24,6 @@ pp <- function(p,st,debug=FALSE,flanking=1e6)
     m <- x[1, "End"]
     t <- subset(z, End > m & End < m + flanking)
     if (nrow(t)==0) {
-print(r1)
       r2 <- as.numeric(r1) + 1
       pp(p, r2)
     } else {
@@ -36,7 +32,7 @@ print(r1)
       u <- p[nrow(t), "End"]
       r2 <- row.names(t)[nrow(t)]
       if (p2 > p1) {
-        cat("protein =", prot, l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", r1, "(case 2)\n")
+        cat("Protein =", prot, chr, ":", l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", r1, "(case 2)\n")
         if (r2 < nr) pp(p, r2)
       } else {
         r2 <- row.names(y)[nrow(y)]
@@ -47,10 +43,9 @@ print(r1)
 }
 prot <- Sys.getenv("prot")
 p <- read.delim(paste0(prot,".p"),as.is=TRUE)
-chr <- with(p,unique(Chrom))
-for(s in chr)
+chrs <- with(p,unique(Chrom))
+for(chr in chrs)
 {
-  cat("prot =",prot,"Chromosome =",s,"\n")
-  ps <- subset(p,Chrom==s)
+  ps <- subset(p,Chrom==chr)
   pp(ps, 1)
 }
