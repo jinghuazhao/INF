d199a554e057576c9d41e22b3ce49d5679a408dc
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-02 08:40:00 +0100

    Notes and programs on TRYGGVE

--- tryggve/sentinels.R
+++ tryggve/sentinels.R
@@ -9,29 +9,34 @@ pp <- function(p,st,debug=FALSE,flanking=1e6)
   })
   if (debug) print(p[c("Chrom","End","d","s","MarkerName","P.value")])
   l <- p[1,"End"]
-  u <- p[nrow(p),"End"]
+  u <- p[nrow(p), "End"]
   len <- with(p,max(s))
   if (len < flanking) {
     p1 <- with(p,min(P.value))
     x <- subset(p, P.value==p1)
+    r1 <- row.names(x)[1]
     m <- x[1,"End"]
-    cat(prot, l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", row.names(x), "(case 1)\n")
+    cat(prot, l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", r1, "(case 1)\n")
   }
   else
   {
-    s <- subset(p,s < flanking)
-    p1 <- with(s,min(P.value))
+    s <- subset(p, s < flanking)
+    p1 <- with(s, min(P.value))
     x <- subset(s, P.value==p1)
-    m <- x[1,"End"]
+    r1 <- row.names(x)[1]
+    m <- x[1, "End"]
     t <- subset(p, End > m & End < m + flanking)
-    p2 <- with(t,min(P.value))
+    p2 <- with(t, min(P.value))
     y <- subset(t, P.value==p2)
     u <- p[nrow(t), "End"]
     if (p2 > p1) {
-      cat(prot, l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", row.names(x), "(case 2)\n")
-      pp(p,row.names(t[nrow(t),]))
+      r2 <- row.names(t)[nrow(t)]
+      cat(prot, l, "-", u, "d =", u-l, "m =", m, "p =", p1, "row =", r1, "(case 2)\n")
+      pp(p, r2)
+    } else {
+      r2 <- row.names(y)[nrow(y)]
+      pp(p, r2)
     }
-    else pp(p,row.names(y[nrow(y),]))
   }
 }
 prot <- Sys.getenv("prot")
