4ebac8f0710b22cab1b9b0285acbcc5bde4045a0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-06-03 14:17:09 +0100

    Notes and programs on TRYGGVE

--- tryggve/sentinels.R
+++ tryggve/sentinels.R
@@ -15,7 +15,7 @@ pp <- function(p,st,debug=FALSE,flanking=1e+6)
     r1 <- row.names(x)[1]
     m <- x[1,"End"]
     n <- x[1, "MarkerName"]
-    cat(prot, chr, n, l, u, u-l, p1, r1, "I\n", sep=",")
+    cat(prot, n, l, u, u-l, p1, r1, "I\n", sep=",")
   } else {
     s <- subset(z, s <= flanking)
     l <- s[1, "End"]
@@ -27,7 +27,7 @@ pp <- function(p,st,debug=FALSE,flanking=1e+6)
     n <- x[1, "MarkerName"]
     t <- subset(z, End > m & End < m + flanking)
     if (nrow(t)==0) {
-      if (u-l > 0) cat(prot, chr, n, l, u, u-l, p1, r1, "II\n", sep=",")
+      if (u-l > 0) cat(prot, n, l, u, u-l, p1, r1, "II\n", sep=",")
       message(paste0("No variants +1 MB downstream so move to next block (",prot,")"))
       r2 <- as.numeric(r1) + 1
       pp(p, r2)
@@ -37,7 +37,7 @@ pp <- function(p,st,debug=FALSE,flanking=1e+6)
       u <- p[nrow(t), "End"]
       r2 <- row.names(t)[nrow(t)]
       if (p2 > p1) {
-        cat(prot, chr, n, l, u, u-l, p1, r1, "III\n", sep=",")
+        cat(prot, n, l, u, u-l, p1, r1, "III\n", sep=",")
         if (r2 < nr) pp(p, r2)
       } else {
         r2 <- as.numeric(row.names(y)[nrow(y)])
