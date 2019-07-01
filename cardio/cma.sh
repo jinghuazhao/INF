0bac5a6424f6b03c3f2083e4725a01375fdd3dfb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-07-01 15:29:23 +0100

    Work on CEU's cardio

--- cardio/cma.sh
+++ cardio/cma.sh
@@ -7,6 +7,6 @@
   cut -d' ' -f1,4 dist/INF1_nold.sentinels | \
   parallel -j1 -C' ' '
     if [ -f dist/cojo/{1}-{2}.cma.cojo ]; then
-       awk -vprot={1} -v snpid={2} "\$NF <= 5e-10 {print prot, snpid, \$0}" dist/cojo/{1}-{2}.cma.cojo
+       awk -vprot={1} -v snpid={2} -vOFS="\t" "\$NF <= 5e-10 {print prot, snpid, \$0}" dist/cojo/{1}-{2}.cma.cojo
     fi'
 ) > INF1.cma
