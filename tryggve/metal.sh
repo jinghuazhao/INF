7b4a470989ff9f33d3ed0eb366aef82f9facda33
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-07-04 15:44:34 +0100

    Notes and programs on TRYGGVE

--- tryggve/metal.sh
+++ tryggve/metal.sh
@@ -1,4 +1,4 @@
-# 3-7-2019 JHZ
+# 4-7-2019 JHZ
 
 function METAL_list()
 {
@@ -83,9 +83,42 @@ function METAL_analysis()
   parallel -j4 --env rt -C' ' '
     metal $rt/{}.metal 2>&1 | \
     tee $rt/{}-1.tbl.log; \
-    gzip -f $rt/{}-1.tbl
+    awk "{
+       d3=\$13;
+       gsub(/?/,\"\",d3)
+       if (length(d3 >= 3) && \$18 >= 3500)
+          if (\$12 > -9.30103) print;
+          else {
+             if (\$14 < 30) print;
+             else if (d3 == \"nnn\" || d3 == \"ppp\") print
+          }
+    }" $rt/{}-1.tbl | \
+    gzip -f > $rt/{}-1.tbl.gz
+    rm $rt/{}-1.tbl
   '
 }
+# R
+# > -log10(5e-10)
+# [1] 9.30103
+# head -1 METAL/4E.BP1-1.tbl | sed 's|\t|\n|g' | awk '{print "#" NR,$1}'
+#1 Chromosome
+#2 Position
+#3 MarkerName
+#4 Allele1
+#5 Allele2
+#6 Freq1
+#7 FreqSE
+#8 MinFreq
+#9 MaxFreq
+#10 Effect
+#11 StdErr
+#12 log(P)
+#13 Direction
+#14 HetISq
+#15 HetChiSq
+#16 HetDf
+#17 logHetP
+#18 N
 
 function largest_M()
 # the union of SNP lists as initially requested by NSPHS
