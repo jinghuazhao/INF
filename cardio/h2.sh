c5fe8ea458025855ff43c9c8cd55db13182f9cba
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-07-29 11:16:13 +0100

    Work on CEU's cardio

--- cardio/h2.sh
+++ cardio/h2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 export TMPDIR=/scratch/jhz22/tmp
-export rt=/scratch/jhz22/data/INTERVAL/INTERVAL
-export s=/home/jp549/post-doc/genetics/r2-test/sample_files/o5000-inf1-outlier_out-r2.sample
+export rt=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/INF/INTERVAL
+export s=${rt}/o5000-inf1-outlier_out-r2.sample
 
-cut -d' ' -f1-2,5-7 ${s} | awk 'NR>3' > ${rt}.covar
-cut -d' ' -f1-2,4,8-28 ${s} | awk 'NR>3' > ${rt}.qcovar
+cut -d' ' -f1-2,5-7 ${s} | awk 'NR>3' > ${rt}/INTERVAL.covar
+cut -d' ' -f1-2,4,8-28 ${s} | awk 'NR>3' > ${rt}/INTERVAL.qcovar
 cut -d' ' -f1-2,29- ${s} | awk 'NR>3' > ${rt}.pheno
 
-plink --bfile ${rt} --indep-pairwise 500kb 1 0.80 --maf 0.01 --out $rt
-plink --bfile ${rt} --extract ${rt}.prune.in --make-bed --out ${rt}.prune
-plink --bfile ${rt}.prune --make-grm-bin --threads 2 --out ${rt}
+plink --bfile ${rt} --indep-pairwise 500kb 1 0.80 --maf 0.01 --out $rt/INTERVAL
+plink --bfile ${rt} --extract ${rt}.prune.in --make-bed --out ${rt}/INTERVAL.prune
+plink --bfile ${rt}.prune --make-grm-bin --threads 2 --out ${rt}/INTERVAL
 
-sbatch --wait cardio/h2.sb
+sbatch --wait csd3/h2.sb
 
 export wd=$(dirname $rt)
 cd $wd
