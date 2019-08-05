40638dfaa12df0abbf56a5134f58ce0d3f297017
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-08-05 14:58:46 +0100

    Work on CSD3

--- csd3/jma.sh
+++ csd3/jma.sh
@@ -1,4 +1,4 @@
-# 26-7-2019 JHZ
+# 5-8-2019 JHZ
 (
   cat work/*jma.cojo | \
   head -1 | \
@@ -12,13 +12,13 @@
 sed 's/Chr/CHR/g;s/bp/BP/g' INF1.jma > jma
 R --no-save -q <<END
   require(gap);require(gap.examples)
-  jma <- read.delim("jma")
-  ni <- dim(jma)[1]
-  primary <- dim(subset(jma,p <= 5e-10 & pJ <= 5e-10))[1]
-  secondary <- dim(subset(jma,p > 5e-10 & pJ <= 5e-10))[1]
-  print(cbind(ni,primary,secondary))
+  jma <- read.delim("jma", as.is=TRUE)
+  primary <- subset(jma,p <= 5e-10 & pJ <= 5e-10)
+  secondary <- subset(jma,p > 5e-10 & pJ <= 5e-10)
+  print(cbind(nrow(jma),nrow(primary),nrow(secondary)))
   hits <- merge(jma[c("prot","CHR","BP","SNP")],inf1[c("prot","uniprot")],by="prot")
   names(hits) <- c("prot","Chr","bp","SNP","uniprot")
+# total
   cistrans <- cis.vs.trans.classification(hits,inf1,"uniprot")
   cis.vs.trans <- with(cistrans,data)
   write.table(cis.vs.trans,file="INF1.jma.cis.vs.trans",row.names=FALSE,quote=TRUE)
@@ -31,5 +31,17 @@ R --no-save -q <<END
   pdf("INF1.jma.pdf")
   circos.cis.vs.trans.plot(hits="jma",inf1,"uniprot")
   dev.off()
+# primary
+  cistrans1 <- cis.vs.trans.classification(subset(hits,SNP%in%with(primary,SNP)),inf1,"uniprot")
+  cis.vs.trans1 <- with(cistrans1,data)
+  sink("INF1.jma.1.out")
+  with(cistrans1,table)
+  sink()
+# secondary
+  cistrans2 <- cis.vs.trans.classification(subset(hits,SNP%in%with(secondary,SNP)),inf1,"uniprot")
+  cis.vs.trans2 <- with(cistrans2,data)
+  sink("INF1.jma.2.out")
+  with(cistrans2,table)
+  sink()
 END
 rm jma
