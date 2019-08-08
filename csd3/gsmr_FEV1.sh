4bcda2fa989f425f72b89e4bcdd99b3cb52e86d2
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-08-08 09:05:20 +0100

    Work on CSD3

---
+++ csd3/gsmr_FEV1.sh
@@ -0,0 +1,14 @@
+# 7-8-2019 JHZ
+
+if [ -f work/INF1_FEV1.gsmr ]; then rm work/INF1_FEV1.gsmr; fi
+(
+  cat work/gsmr_FEV1*.gsmr | \
+  head -1
+  ls work/gsmr_FEV1*.gsmr | \
+  parallel -j1 -C' ' '
+    if [ -f {} ]; then
+       awk "NR>1" {}
+    fi
+  '
+) | \
+grep -v nan > INF1_FEV1.gsmr
