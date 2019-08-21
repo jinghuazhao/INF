29101c29e5f33d2b25680dd3991c677c7a92cbeb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-08-21 13:58:40 +0100

    Work on CSD3

--- csd3/ld-map.sh
+++ csd3/ld-map.sh
@@ -3,4 +3,4 @@
 export f=INTERVAL/per_chr/interval.imputed.olink.chr_
 seq 22 | parallel --env f -j1 -C' ' 'awk -f csd3/ld-map.awk ${f}{}.bim > work/INTERVAL-{}.map'
 seq 22 | parallel --env f -j1 -C' ' 'qctool -g ${f}{}.bgen -map-id-data work/INTERVAL-{}.map -og work/INTERVAL-{}.bgen'
-seq 22 | parallel --env f -j1 -C' ' 'bgenix -g ${f}{}.bgen -index'
+seq 22 | parallel --env f -j1 -C' ' 'bgenix -g work/INTERVAL-{}.bgen -index'
