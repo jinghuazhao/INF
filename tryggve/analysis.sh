3e2330fbd72eaeb10dd382e947d8addeb755d078
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-08-23 20:16:20 +0100

    Notes and programs on TRYGGVE

--- tryggve/analysis.sh
+++ tryggve/analysis.sh
@@ -132,20 +132,25 @@ function ma()
   ) > $rt/{}.ma
   '
 }
-#1 Chromosome
-#2 Position
-#3 MarkerName
-#4 Allele1
-#5 Allele2
-#6 Freq1
-#7 FreqSE
-#8 MinFreq
-#9 MaxFreq
-#10 Effect
-#11 StdErr
-#12 P-value
-#13 Direction
-#14 N
+sed 's/\t/\n/g' work/METAL.hdr| awk '{print "# " NR " " $1}'
+# 1 Chromosome
+# 2 Position
+# 3 MarkerName
+# 4 Allele1
+# 5 Allele2
+# 6 Freq1
+# 7 FreqSE
+# 8 MinFreq
+# 9 MaxFreq
+# 10 Effect
+# 11 StdErr
+# 12 log(P)
+# 13 Direction
+# 14 HetISq
+# 15 HetChiSq
+# 16 HetDf
+# 17 logHetP
+# 18 N
 
 function cojo()
 {
