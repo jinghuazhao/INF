1f68d9d2bcaacbacdae0ac06a1028f371ae1e1a3
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-08-25 10:44:53 +0100

    Work on CSD3

--- csd3/ld-map.sh
+++ csd3/ld-map.sh
@@ -1,4 +1,4 @@
-# 24-8-2019 JHZ
+# 25-8-2019 JHZ
 
 export f=INTERVAL/per_chr/interval.imputed.olink.chr_
 export TMPDIR=/rds/user/jhz22/hpc-work/work
@@ -8,6 +8,6 @@ seq 22 | parallel --env f -j1 -C' ' 'qctool -g ${f}{}.bgen -map-id-data work/INT
 seq 22 | parallel --env f -j1 -C' ' 'bgenix -g work/INTERVAL-{}.bgen -index'
 
 cat-bgen -g $(seq 22|awk '{printf "work/INTERVAL-" $1 ".bgen "}') -og work/INTERVAL.bgen -clobber
-qctool -g work/INTERVAL.bgen -s ${f}10.sample -excl-range 19:54296855-54311176 -ofiletype binary_ped -og work/INTERVAL
+qctool -g work/INTERVAL.bgen -s ${f}10.sample -excl-range 19:54296855-54500000 -ofiletype binary_ped -og work/INTERVAL
 
 # NLRP12 - chr19:54,296,855-54,311,176
