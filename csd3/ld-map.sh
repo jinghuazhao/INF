a106c6c1a2f02d785674062afe2c6e6e8ee71c65
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-08-27 15:17:34 +0100

    Work on CSD3

--- csd3/ld-map.sh
+++ csd3/ld-map.sh
@@ -1,23 +1,39 @@
-# 26-8-2019 JHZ
+# 27-8-2019 JHZ
 
 export f=INTERVAL/per_chr/interval.imputed.olink.chr_
 export TMPDIR=/rds/user/jhz22/hpc-work/work
 
-# snpid - rsid mapping
+# bgen, rsid, bed+bim+fam in INTERVAL/ directory
 
-seq 22 | parallel --env f -j1 -C' ' '(cat csd3/ld-map.hdr;awk -f csd3/ld-map.awk ${f}{}.bim) > work/INTERVAL-{}.map'
-cat $(seq 22 | awk '{printf "work/INTERVAL-" $1 ".map "}') | awk -vOFS="\t" '{print $8,$7}' | sort -k1,1 | gzip -f > INTERVAL/INTERVAL.rsid.gz
+## snpid - rsid mapping
 
-# bgen + bgen.bgi
+seq 22 | parallel --env f -j1 -C' ' '(cat csd3/ld-map.hdr;awk -f csd3/ld-map.awk ${f}{}.bim) > INTERVAL/INTERVAL-{}.map'
+cat $(seq 22 | awk '{printf "INTERVAL/INTERVAL-" $1 ".map "}') | awk -vOFS="\t" '{print $8,$7}' | sort -k1,1 | gzip -f > INTERVAL/INTERVAL.rsid.gz
 
-seq 22 | parallel --env f -j1 -C' ' 'qctool -g ${f}{}.bgen -map-id-data work/INTERVAL-{}.map -og work/INTERVAL-{}.bgen'
-seq 22 | parallel --env f -j1 -C' ' 'bgenix -g work/INTERVAL-{}.bgen -index'
+## bgen + bgen.bgi
 
-cat-bgen -g $(seq 22|awk '{printf "work/INTERVAL-" $1 ".bgen "}') -og work/INTERVAL.bgen -clobber
+seq 22 | parallel --env f -j1 -C' ' 'qctool -g ${f}{}.bgen -map-id-data INTERVAL/INTERVAL-{}.map -og INTERVAL/INTERVAL-{}.bgen'
+seq 22 | parallel --env f -j1 -C' ' 'bgenix -g INTERVAL/INTERVAL-{}.bgen -index'
 
-# bed + bim + fam
+cat-bgen -g $(seq 22|awk '{printf "INTERVAL/INTERVAL-" $1 ".bgen "}') -og INTERVAL/INTERVAL.bgen -clobber
 
-# NLRP12 - chr19:54,296,855-54,311,176
-qctool -g work/INTERVAL.bgen -s ${f}10.sample -excl-range 19:54296855-54500000 -ofiletype binary_ped -og work/INTERVAL
+# version with no duplicates and extended (chr19:53296855-54500000) NLRP12 region (chr19:54296855-54311176) in work/ directory
 
-### NOTE bgen, rsid, bed+bim+fam are moved to INTERVAL directory
+module load plink/2.00-alpha
+plink2 --bgen INTERVAL/INTERVAL.bgen -sample o5000-inf1-outlier_out-r2.sample --rm-dup force-first list --out dup
+
+## bgen
+
+qctool -g INTERVAL/INTERVAL.bgen -excl-range 19:53296855-54500000 -excl-rsids dup.rmdup.list -og work/INTERVAL.bgen
+
+## bed + bim + fam
+
+qctool -g work/INTERVAL.bgen -s ${f}10.sample -ofiletype binary_ped -og work/INTERVAL
+
+## snpid - rsid
+
+join -v2 <(cat $(seq 22 | \
+           awk '{printf "INTERVAL/INTERVAL-" $1 ".map "}') | \
+           awk -vOFS="\t" '!($9==19 && $10 >= 53296855 && $10 < 54500000){print $8,$7}' | \
+           sort -k1,1) dup.rmdup.list | > work/INTERVAL.rsid
+
