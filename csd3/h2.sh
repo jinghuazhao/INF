94b61bb7341a29642574e0062879985f1f72da42
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-09-11 10:27:28 +0100

    Work on CSD3

--- csd3/h2.sh
+++ csd3/h2.sh
@@ -8,9 +8,11 @@ cut -d' ' -f1-2,5-7 ${s} | awk 'NR>3' > ${rt}.covar
 cut -d' ' -f1-2,4,8-28 ${s} | awk 'NR>3' > ${rt}.qcovar
 cut -d' ' -f1-2,29- ${s} | awk 'NR>3' > ${rt}.pheno
 
-# plink --bfile ${rt} --indep-pairwise 500kb 1 0.80 --maf 0.01 --out $rt
-# plink --bfile ${rt} --extract ${rt}.prune.in --make-bed --out ${rt}.prune
-# plink --bfile ${rt}.prune --make-grm-bin --threads 2 --out ${rt}
+plink --bfile ${rt} \
+      --exclude range tryggve/high-LD-regions-hg19.txt \
+      --indep-pairwise 500kb 1 0.80 --maf 0.01 --out $rt
+plink --bfile ${rt} --extract ${rt}.prune.in --make-bed --out ${rt}.prune
+plink --bfile ${rt}.prune --make-grm-bin --threads 2 --out ${rt}
 
 sbatch --wait csd3/h2.sb
 
