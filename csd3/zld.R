e463c1efd86d6b212a10f598ead40db50dd47278
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-09-12 17:11:58 +0100

    Work on CSD3

--- csd3/zld.R
+++ csd3/zld.R
@@ -1,16 +1,17 @@
-# 10-9-2019 JHZ
+# 12-9-2019 JHZ
 
 options(digits=3, scipen=20, width=10000)
 pr <- Sys.getenv("pr")
 cat(pr, "\n")
-ld <- read.table(paste0(pr, ".ld"))
+z <- read.table(paste0(pr, ".z"),as.is=TRUE,header=TRUE)
+ld <- read.table(paste0(pr, ".ld"),col.names=with(z,rsid))
 snp <- read.table(paste0(pr, ".snp"), as.is=TRUE, header=TRUE)
 z <- subset(snp, abs(z) > 6.47)
-id <- as.integer(row.names(z))
-ldt <- ld[id,id]
+id <- with(z,index)
+rank <- with(z,order(index))
+ldt <- ld[id,id][rank,rank]
 ldt[upper.tri(ldt, diag=TRUE)] <- NA
-colnames(ldt) <- with(z, rsid)
-chk <- cbind(z[c("rsid","z")], ldt)
+chk <- cbind(rank,z[rank,c("index","rsid","z")],ldt)
 library(openxlsx)
 xlsx <- paste0(pr, "-zld.xlsx")
 unlink(xlsx, recursive = FALSE, force = TRUE)
