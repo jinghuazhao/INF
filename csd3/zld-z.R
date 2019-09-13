21825ad98bf061e12bf73f6817dc8f77de00eecf
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-09-13 11:07:23 +0100

    Work on CSD3

--- csd3/zld-z.R
+++ csd3/zld-z.R
@@ -3,24 +3,20 @@
 options(digits=3, scipen=20, width=10000)
 pr <- Sys.getenv("pr")
 cat(pr, "\n")
-k <- 50
 z <- read.table(paste0(pr, ".z"),as.is=TRUE,header=TRUE)
 ld <- read.table(paste0(pr, ".ld"),col.names=with(z,rsid))
 snp <- read.table(paste0(pr, ".snp"), as.is=TRUE, header=TRUE)
-variants <- snp[1:k,c("rsid","position","chromosome", "allele1", "allele2")]
-names(variants) <- c("RSID", "position", "chromosome", "A_allele", "B_allele")
-z <- subset(snp, rsid %in% with(variants, RSID))
+z <- subset(snp, abs(z) > 6.47)
 id <- with(z,index)
 rank <- 1:with(z,length(index))
 ldt <- ld[id,id][rank,rank]
 ldt[upper.tri(ldt, diag=TRUE)] <- NA
 chk <- cbind(rank,z[rank,c("index","rsid","z","group","corr_group","prob_group")],ldt)
 library(openxlsx)
-xlsx <- paste0(pr, ".zld-50.xlsx")
+xlsx <- paste0(pr, "-zld-z.xlsx")
 unlink(xlsx, recursive = FALSE, force = TRUE)
 wb <- createWorkbook(xlsx)
 f <- make.names(pr)
 addWorksheet(wb, paste0(f, ".zld"))
 writeDataTable(wb, paste0(f, ".zld"), chk)
 saveWorkbook(wb, file=xlsx, overwrite=TRUE)
-
