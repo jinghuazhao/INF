265260f5385bed1957f179abf49e1174fd0570a8
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-09-14 20:27:15 +0100

    Notes and programs on TRYGGVE

---
+++ tryggve/finemap.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+export job=132
+export TMPDIR=/data/jinhua/work
+export INF=/home/jinhua/INF
+export list=${INF}/work/INF1_nold.sentinels
+export p=$(awk -v job=${job} 'NR==job+1{print $1}' ${list})
+export chr=$(awk -v job=${job} 'NR==job+1{print $2}' ${list})
+export pos=$(awk -v job=${job} 'NR==job+1{print $3}' ${list})
+export r=$(awk -v job=${job} 'NR==job+1{print $4}' ${list})
+export pr=${p}-${r}
+export sumstats=${INF}/METAL/${p}-1.tbl.gz
+export flanking=1e6
+export start=$(awk -vpos=${pos} -vflanking=${flanking} 'BEGIN{start=pos-flanking;if(start<0) start=0;print start}')
+export end=$(awk -vpos=${pos} -vflanking=${flanking} 'BEGIN{print pos+flanking}')
+export bfile=${INF}/EUR
+export sample=${INF}/INTERVAL/o5000-inf1-outlier_in-r2.sample
+export snpid_rsid=${INF}/work/INTERVAL.rsid
+export study=INTERVAL
+export N=4994
+
+cd work
+# z0
+(
+  zcat ${sumstats} | head -1 | awk '{print $3,$1,$2,$5,$4,"MAF",$10,$11}'
+  zcat ${sumstats} | \
+  awk 'NR > 1' | \
+  sort -k3,3 | \
+  awk -vchr=${chr} -vstart=${start} -vend=${end} '
+  {
+    if ($1==chr && $2 >= start && $2 < end) {
+    if ($6 < 0.5) maf = $6; else maf = 1-$6
+    if (maf > 0 && maf <= 0.5 && $10 != "NA" && $11 != "NA") print $3, $1, $2, toupper($5), toupper($4), maf, $10, $11
+  }
+  } ' | \
+  join ${pr} -
+) > ${pr}.z0
+awk 'NR > 1{print $1} ' ${pr}.z0 > ${pr}.incl
+
+# bgen
+qctool -g ${bfile}.bgen -og ${pr}.bgen -ofiletype bgen -incl-rsids ${pr}.incl
+
+# bgi
+bgenix -g ${pr}.bgen -index -clobber
+ln -sf ${pr}.bgen.bgi ${pr}.bgi
+
+# z
+(
+  join <(awk 'NR > 1' ${pr}.z0 | cut -d' ' -f1,4,5  | sort -k1,1) \
+       <(bgenix -g ${pr}.bgen -list 2>&1 | awk 'NR>9 && NF==7'| cut -f2,6,7 | sort -k1,1) | \
+  awk '{print $1, ($2!=$4)}'
+) > ${pr}.flip
+(
+  awk 'BEGIN {print "rsid", "chromosome", "position", "allele1", "allele2", "maf", "beta", "se", "flip"}'
+  join <(awk 'NR > 1' ${pr}.z0) ${pr}.flip | awk '{if($9==1) {t=$4;$4=$5;$5=t};$7=-$7; print}'
+) > ${pr}.z
+
+# master
+(
+  echo "z;ld;snp;config;cred;log;n_samples"
+  echo "${pr}.z;${pr}.ld;${pr}.snp;${pr}.config;${pr}.cred;${pr}.log;$N"
+) > ${pr}.master
+
+# ldstore 1.1
+
+ldstore_v1.1 --bcor ${pr}-1 --bgen ${pr}.bgen --n-threads 1
+ldstore_v1.1 --bcor ${pr}-1 --merge 1
+ldstore_v1.1 --bcor ${pr}-1 --matrix ${pr}.ld
+rm ${pr}-1_*
+mv ${pr}-1 ${pr}.bcor
+
+function bcor2()
+# ldstore 2.0b
+{
+  (
+    echo "z;ld;bgen;bgi;bcor;bdose;snp;config;cred;log;n_samples"
+    echo "${pr}.z;${pr}.ld;${pr}.bgen;${pr}.bgi;${pr}.bcor;${pr}.bdose;${pr}.snp;${pr}.config;${pr}.cred;${pr}.log;$N"
+  ) > ${pr}.master2
+  ldstore_v2.0b --in-files ${pr}.master2 --write-bcor
+  ldstore_v2.0b --in-files ${pr}.master2 --bcor-to-text
+}
+
+# finemap
+export k=10
+rm -rf ${pr}.cred* ${pr}.dose ${pr}.snp ${pr}.config
+finemap_v1.3.1 --sss --in-files ${pr}.master --log --n-causal-snps $k \
+               --n-configs-top 500 --corr-config 0.95 --corr-group 0.9999 --group-snps
+
+# xlsx
+export snpid_rsid=${pr}
+R -q --no-save < ${INF}/csd3/finemap.R > ${pr}-finemap.log
+
+# zld
+R --no-save -q < ${INF}/csd3/zld.R > ${pr}-zld.log
+
+cd -
---
+++ tryggve/snpid_rsid.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+export job=132
+export TMPDIR=/data/jinhua/work
+export INF=/home/jinhua/INF
+export list=${INF}/work/INF1_nold.sentinels
+export p=$(awk -v job=${job} 'NR==job+1{print $1}' ${list})
+export chr=$(awk -v job=${job} 'NR==job+1{print $2}' ${list})
+export pos=$(awk -v job=${job} 'NR==job+1{print $3}' ${list})
+export r=$(awk -v job=${job} 'NR==job+1{print $4}' ${list})
+export pr=${p}-${r}
+export flanking=1e6
+export start=$(awk -vpos=${pos} -vflanking=${flanking} 'BEGIN{start=pos-flanking;if(start<0) start=0;print start}')
+export end=$(awk -vpos=${pos} -vflanking=${flanking} 'BEGIN{print pos+flanking}')
+export bfile=${INF}/EUR
+export sample=${INF}/INTERVAL/o5000-inf1-outlier_in-r2.sample
+export snpid_rsid=${INF}/work/INTERVAL.rsid
+
+cd work
+# unpruned
+awk -vchr=$chr -vlower=$start -vupper=$end '$1==chr && $4>=lower && $4 < upper{print $2}' ${bfile}.bim | \
+sort -k1,1 > ${pr}
+join -a2 -e "NA" ${snpid_rsid} ${pr} -o2.1,1.2 > ${pr}.rsid
+R --no-save -q <<END
+    f <- Sys.getenv("pr")
+    snpid_rsid <- read.table(paste0(f,".rsid"), as.is=TRUE, col.names=c("rsid","name"), fill=TRUE)
+    nmiss <- with(snpid_rsid,is.na(name))
+    snpid_rsid <- within(snpid_rsid, {name[nmiss] <- make.names(rsid[nmiss])})
+    save(snpid_rsid, file=paste0(f,".rda"))
+END
+
+module load plink/2.00-alpha
+# pruned
+plink2  --bgen ${bfile}.bgen -sample ${sample} \
+        --snp ${r} \
+        --window 1000 \
+        --maf 0.01 \
+        --indep-pairwise 1000kb 1 0.8 \
+        --out ${dir}/${pr}
+if [ $(grep ${r} ${dir}/${pr}.prune.in | wc -l) -eq 0 ]; then
+   (
+     echo ${r}
+     cat ${dir}/${pr}.prune.in
+   ) > $TMPDIR/${pr}
+   plink-1.9 --bfile ${bfile} \
+             --extract $TMPDIR/${pr} \
+             --r2 square \
+             --out $TMPDIR/${pr}
+   export i=$(paste $TMPDIR/${pr} $TMPDIR/${pr}.ld | cut -f1,2 | sort -r -k2,2g | awk 'NR==1 {print $1}')
+   sed -i 's/'"$i"'/'"$r"'/g' ${dir}/${pr}.prune.in
+fi
+sort -k1,1 ${dir}/${pr}.prune.in > $TMPDIR/${pr}.prune.in
+awk 'NR > 1' ${dir}/${p}.ma | \
+sort -k1,1 | \
+join -j1 - $TMPDIR/${pr}.prune.in | \
+awk '{print $1}' > ${dir}/${pr}.prune
+join -a2 -e "NA" <(sort -k1,1 ${snpid_rsid}) ${pr}.prune -o2.1,1.2 > ${pr}.prune.rsid
+R --no-save -q <<END
+    f <- Sys.getenv("pr")
+    snpid_rsid <- read.table(paste0(f,".prune.rsid"), as.is=TRUE, col.names=c("rsid","name"), fill=TRUE)
+    nmiss <- with(snpid_rsid,name=="")
+    snpid_rsid <- within(snpid_rsid, {name[nmiss] <- make.names(rsid[nmiss])})
+    save(snpid_rsid, file=paste0(f,".prune.rda"))
+END
+cd -
