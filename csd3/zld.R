25df3ff9316bbac5a1393de220d7fc6540b15099
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-09-15 08:53:45 +0100

    Work on CSD3

--- csd3/zld.R
+++ csd3/zld.R
@@ -1,22 +1,29 @@
-# 14-9-2019 JHZ
+# 15-9-2019 JHZ
 
+function <- chk(z)
+{
+  id <- with(z,index)
+  rank <- 1:with(z,length(index))
+  ldt <- ld[id,id][rank,rank]
+  ldt[upper.tri(ldt, diag=TRUE)] <- NA
+  chk <- cbind(rank,z[rank,c("index","rsid","z","log10bf","group","corr_group","prob_group","log10bf_group")],ldt)
+}
 options(digits=3, scipen=20, width=10000)
 pr <- Sys.getenv("pr")
-k <- 50
 z <- read.table(paste0(pr, ".z"), as.is=TRUE, header=TRUE)
 ld <- read.table(paste0(pr, ".ld"),col.names=with(z,rsid))
 snp <- read.table(paste0(pr, ".snp"), as.is=TRUE, header=TRUE)
-z <- head(snp, k)
-id <- with(z,index)
-rank <- 1:with(z,length(index))
-ldt <- ld[id,id][rank,rank]
-ldt[upper.tri(ldt, diag=TRUE)] <- NA
-chk <- cbind(rank,z[rank,c("index","rsid","z","group","corr_group","prob_group")],ldt)
+z1 <- head(snp, abs(z)>=6.47)
+zld1 <- zld(z1)
+z2 <- head(snp, nrows(z1))
+zld2 <- chk(z2)
 library(openxlsx)
-xlsx <- paste0(pr, ".zld-",k,".xlsx")
+xlsx <- paste0(pr, ".zld.xlsx")
 unlink(xlsx, recursive = FALSE, force = TRUE)
 wb <- createWorkbook(xlsx)
 f <- make.names(pr)
-addWorksheet(wb, paste0(f, ".zld"))
-writeDataTable(wb, paste0(f, ".zld"), chk)
+addWorksheet(wb, paste0(f, ".gwsig"))
+writeDataTable(wb, paste0(f, "gwsig"), zld1)
+addWorksheet(wb, paste0(f, ".topsnp"))
+writeDataTable(wb, paste0(f, ".topsnp"), zld2)
 saveWorkbook(wb, file=xlsx, overwrite=TRUE)
