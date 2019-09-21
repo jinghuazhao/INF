dc56ca410a1346c9b05117a00e680aa9c41481e1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-09-21 21:15:23 +0100

    Work on CSD3

--- csd3/swiss.sh
+++ csd3/swiss.sh
@@ -1,24 +1,27 @@
-export swiss_data=$HOME/.local/share/swiss/data
+# 21-9-2019 JHZ
 
+export TMPDIR=/rds/user/jhz22/hpc-work/work
+export INF=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/INF
+export tag=_nold
+
+for p in $(ls sentinels/*${tag}.p | sed 's|sentinels/||g;s|'"$tag"'.p||g'); do
+
+echo $p
 awk -v OFS="\t" '
 {
-   if (NR==1) print "MarkerName","CHR","POS","PVALUE";
+   if (NR==1) print "MarkerName","EPACTS","CHR","POS","PVALUE";
    else {
      gsub(/chr/,"",$1)
-     print $4,$1,$3,exp($13)
+     EPACTS=$1 ":" $3 "_" toupper($5) "/" toupper($6)
+     print $4,EPACTS,$1,$3,exp($13)
    }
-}' sentinels/IL.6_nold.p > work/IL.6.p
+}' ${INF}/sentinels/${p}_${tag}.p > work/${p}.p
 
-swiss --assoc work/IL.6.p \
-      --build hg19 \
-      --ld-clump-source 1000G_2014-11_EUR \
-      --variant-col MarkerName \
+swiss --assoc ${INF}/work/${p}.p \
+      --variant-col EPACTS \
       --chrom-col CHR \
       --pos-col POS \
       --pval-col PVALUE \
-      --dist-clump --clump-dist 1000000 --clump-p 5e-10 --out test
-
-#     --ld-gwas-source 1000G_2014-11_EUR \
-#     --gwas-cat data/gwascat_ebi_GRCh37p13.tab \
-
-
+      --include-cols MarkerName --trait ${p} \
+      --dist-clump --clump-dist 1000000 --clump-p 5e-10 --out ${INF}/work/${p}
+done
