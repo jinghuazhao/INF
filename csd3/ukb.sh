ffe17fd708044a24bac3ecae3ad5b255e27562a9
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-09-27 10:30:24 +0100

    Work on CSD3

--- csd3/ukb.sh
+++ csd3/ukb.sh
@@ -3,22 +3,20 @@
 export INF=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/INF
 export srcdir=${INF}/ukb
 
-function individual()
+sed '1d' work/INF1.merge | \
+sed 's/chr//g' | \
+awk -v srcdir=${srcdir} -v flanking=1e6 -v INF=${INF} '
 {
-  sed '1d' work/INF1.merge | \
-  sed 's/chr//g' | \
-  awk -v srcdir=${srcdir} -v flanking=1e6 -v INF=${INF} '
-  {
-    if ($2 >= flanking) start=$2-flanking;
-    else start = 0;
-    end = $3 + flanking
-    range = $1 ":" start "-" end;
-    if($1<=9) range0=0 range;
-    else range0=range
-    cmd=sprintf("qctool -g %s/ukb_imp_chr%d_v3.bgen -incl-range %s -ofiletype bgen -og ukb/%s-chr%s.bgen", srcdir, $1, range0, $5, $6)
-    print cmd
-  }'
-}
+  if ($2 >= flanking) start=$2-flanking;
+  else start = 0;
+  end = $3 + flanking
+  range = $1 ":" start "-" end;
+  if($1<=9) range0=0 range;
+  else range0=range
+  cmd=sprintf("qctool -g %s/ukb_imp_chr%d_v3.bgen -incl-range %s -ofiletype bgen -og ukb/%s-chr%s.bgen", srcdir, $1, range0, $5, $6)
+  print cmd
+}' > work/ukb.list
+for i in $(seq 22); do if [ -f ${srcdir}/ukb_imp_chr${i}_v3.bgen ]; then grep chr${i}_ work/ukb.list | bash; fi; done
 
 function combined ()
 {
