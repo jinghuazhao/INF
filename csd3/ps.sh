2c15dcde6de605f07b47b9e2d24d1dfad3dc2c3a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-10-18 11:35:55 +0100

    Work on CSD3

--- csd3/ps.sh
+++ csd3/ps.sh
@@ -1,4 +1,4 @@
-# 16-10-2019 JHZ
+# 18-10-2019 JHZ
 
 # To set it up
 # explicit rsid
@@ -6,13 +6,13 @@
   join <(sed '1d' work/INF1.merge | cut -f6 | sort -k1,1 | uniq) work/INTERVAL.rsid
 ) > work/INF1.merge.rsid
 
-# All of the sentinels
+# Novel loci
 R --no-save -q <<END
   require(phenoscanner)
   rsid <- with(read.table("work/INF1.merge.rsid",as.is=TRUE,col.names=c("snpid","rsid")), rsid)
-  r1 <- phenoscanner(snpquery=rsid[1:100], proxies = "EUR", pvalue = 1e-07, r2= 0.6, build=37)
+  r1 <- phenoscanner(snpquery=rsid[1:100], catalogue="GWAS", proxies = "EUR", pvalue = 1e-07, r2= 0.8, build=37)
   lapply(r1,dim)
-  r2 <- phenoscanner(snpquery=rsid[101:162], proxies = "EUR", pvalue = 1e-07, r2= 0.6, build=37)
+  r2 <- phenoscanner(snpquery=rsid[101:162], catalogue="GWAS", proxies = "EUR", pvalue = 1e-07, r2= 0.8, build=37)
   lapply(r2,dim)
   r <- list(rbind(with(r1,snps),with(r2,snps)),rbind(with(r1,results),with(r2,results)))
   lapply(r,dim)
