64e561ab80c4b76a60f19bac8327ed4f6ad70c4a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-11-06 19:42:02 +0000

    Work on CSD3

--- csd3/ma.sh
+++ csd3/ma.sh
@@ -15,13 +15,19 @@ do export p=${p}
      awk -vOFS="\t" '{$1="chr" $1; start=$2-1;$2=start "\t" $2;print}' | \
      awk '!($1 == "chr6" && $3 >= 25392021 && $3 < 33392022)'
    ) > ${p}.a
-   awk -vOFS="\t" '{if(NR==1) print $0, "N"; else print $0,NR-1}' work/${p}.merged > ${p}.b
-   bedtools intersect -wb -a ${p}.a -b ${p}.b > ${p}.ab
-   stata -b -q csd3/ma.do 
+   export p0=$(awk 'NR>1&&10^$13==0' ${p}.a | wc -l | cut -d' ' -f1)
    (
-     echo SNP A1 A2 freq b se p N
-     awk -f csd3/ma.awk ${p}.txt
+      echo SNP A1 A2 freq b se p N
+      if [ ${p0} -eq 0 ]; then
+         awk 'NR>1{print $4,toupper($5),toupper($6),$7,$11,$12,10^$13,$19}' ${p}.a
+         rm ${p}.a
+      else
+         awk -vOFS="\t" '{if(NR==1) print $0, "N"; else print $0,NR-1}' work/${p}.merged > ${p}.b
+         bedtools intersect -wb -a ${p}.a -b ${p}.b > ${p}.ab
+         stata -b -q csd3/ma.do 
+         awk -f csd3/ma.awk ${p}.txt
+         rm ${p}.a ${p}.b ${p}.ab ${p}.txt ${p}.dta ${p}0.dta
+      fi
    ) > sentinels/${p}.ma
-   rm ${p}.a ${p}.b ${p}.ab ${p}.txt ${p}.dta ${p}0.dta
 done
 rm ma.log
