752085f0b16bb649d25520dce8e961d8e0b1d492
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-11-11 16:46:41 +0000

    Work on CSD3

--- csd3/IL.18R1.sh
+++ csd3/IL.18R1.sh
@@ -25,28 +25,19 @@ function jma_cojo()
   }' | \
   awk 'NR != 1 && NR !=3 && NR != 4 && NR != 5 && NR != 6' > geno
   awk 'NR != 2' $s > phenocovar
-
+  (
   R --no-save -q <<\ \ END
     g <- read.table("geno",as.is=TRUE,header=TRUE)
     pc <-read.table("phenocovar",as.is=TRUE,header=TRUE)
     gpc <- merge(g,pc,by.x="SNPID",by.y="ID_1")
     s <- read.table("rsid",as.is=TRUE)
     require(stringr)
-    nog <- c("age", "sexPulse", "season", "plate", "bleed_to_process_time", paste0("PC",1:20), t(s))
+    nog <- c("age", "sexPulse", "season", "plate", "bleed_to_process_time", paste0("PC",1:10), t(s))
     rhs <- str_c(nog, collapse="+")
     m1 <- with(gpc, lm(as.formula(paste0("IL.18R1___Q13478~",rhs))))
     summary(m1)
   END
-}
-
-function assoc_test()
-## SNPTEST v2.5.2 for CKDGen-type finemap analysis
-{
-  qctool  -filetype bgen -g INTERVAL/per_chr/interval.imputed.olink.chr_2.bgen \
-          -ofiletype gen -og IL18R1.gen.gz -incl-range 101992675-103992675 -assume-chromosome 2 -s ${s}
-  snptest_v2.5.2 -data IL18R1.gen.gz ${s} -pheno IL.18R1___Q13478 -full_parameter_estimates \
-                 -condition_on $(cat rsid) \
-                 -frequentist 1 -method em -use_raw_phenotypes -use_raw_covariates -cov_all -o IL.18R1.out
+  ) > IL.18R1.log
 }
 
 function INF1()
@@ -59,9 +50,20 @@ function INTERVAL()
 # INTERVAL: solely the variant
 {
   jma_cojo INTERVAL IL.18R1-chr2:102992675_C_T.jma.cojo
-  assoc_test
 }
 
 INF1
 
 # rm snpid snpid-rsid rsid phenocovar geno IL18R1.gen.gz
+
+function assoc_test()
+## SNPTEST v2.5.2 for CKDGen-type finemap analysis
+{
+  qctool  -filetype bgen -g INTERVAL/per_chr/interval.imputed.olink.chr_2.bgen \
+          -ofiletype gen -og IL18R1.gen.gz -incl-range 101992675-103992675 -assume-chromosome 2 -s ${s}
+  snptest_v2.5.2 -data IL18R1.gen.gz ${s} -pheno IL.18R1___Q13478 -full_parameter_estimates \
+                 -condition_on $(cat rsid) \
+                 -frequentist 1 -method em -use_raw_phenotypes -use_raw_covariates -cov_all -o IL.18R1.out
+}
+
+# assoc_test
