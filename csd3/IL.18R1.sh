a7eec8f4e5b5df36f1b9cf73b97ce09dc845f0b1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-11-11 16:29:43 +0000

    Work on CSD3

--- csd3/IL.18R1.sh
+++ csd3/IL.18R1.sh
@@ -1,16 +1,16 @@
-# 30-7-2019 JHZ
-
-# IL.10R1 data
+#!/bin/bash
+# 11-11-2019 JHZ
 
 function jma_cojo()
+# IL.10R1 data: geno, phenocovar
 {
-  cut -f2 ${1}/${2} | awk 'NR>1' | sort -k1,1 > l
-  awk /chr2:/ work/INTERVAL.rsid | sort -k1,1 | join - l > ll
-  cut -d' ' -f2 ll > lll
+  cut -f2 ${1}/${2} | awk 'NR>1' | sort -k1,1 > snpid
+  awk /chr2:/ work/INTERVAL.rsid | sort -k1,1 | join - snpid > snpid-rsid
+  cut -d' ' -f2 snpidrsid > rsid
   export s=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/INF/INTERVAL/o5000-inf1-outlier_in-r2.sample
   qctool  -filetype bgen -g INTERVAL/per_chr/interval.imputed.olink.chr_2.bgen \
           -ofiletype dosage -og IL18R1.gen.gz -s ${s} \
-          -incl-rsids lll
+          -incl-rsids rsid
   zcat IL18R1.gen.gz | \
   awk '
   {
@@ -30,7 +30,7 @@ function jma_cojo()
     g <- read.table("geno",as.is=TRUE,header=TRUE)
     pc <-read.table("phenocovar",as.is=TRUE,header=TRUE)
     gpc <- merge(g,pc,by.x="SNPID",by.y="ID_1")
-    s <- read.table("lll",as.is=TRUE)
+    s <- read.table("rsid",as.is=TRUE)
     require(stringr)
     nog <- c("age", "sexPulse", "season", "plate", "bleed_to_process_time", paste0("PC",1:20), t(s))
     rhs <- str_c(nog, collapse="+")
@@ -43,27 +43,25 @@ function assoc_test()
 ## SNPTEST v2.5.2 for CKDGen-type finemap analysis
 {
   qctool  -filetype bgen -g INTERVAL/per_chr/interval.imputed.olink.chr_2.bgen \
-          -ofiletype gen -og IL18R1.gen.gz -incl-range 101810080-103810080 -assume-chromosome 2 -s ${s}
+          -ofiletype gen -og IL18R1.gen.gz -incl-range 101992675-103992675 -assume-chromosome 2 -s ${s}
   snptest_v2.5.2 -data IL18R1.gen.gz ${s} -pheno IL.18R1___Q13478 -full_parameter_estimates \
-                 -condition_on $(cat lll) \
+                 -condition_on $(cat rsid) \
                  -frequentist 1 -method em -use_raw_phenotypes -use_raw_covariates -cov_all -o IL.18R1.out
 }
 
 function INF1()
-# INF1
+# INF1: previously 102810080
 {
-  jma_cojo work/test chr2:102810080_A_G.jma.cojo
-  jma_cojo work chr2:102810080_A_G.jma.cojo
-  assoc_test
+  jma_cojo work chr2:102992675_C_T.jma.cojo
 }
 
 function INTERVAL()
-# INTERVAL
+# INTERVAL: solely the variant
 {
   jma_cojo INTERVAL IL.18R1-chr2:102992675_C_T.jma.cojo
   assoc_test
 }
 
-INTERVAL
+INF1
 
-# rm l ll lll phenocovar geno IL18R1.gen.gz
+# rm snpid snpid-rsid rsid phenocovar geno IL18R1.gen.gz
