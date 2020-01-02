380c638341f1ba06ad46be4aa2c91dc18c857ec1
jinghuazhao <jinghuazhao@hotmail.com>
2020-01-02 15:21:24 +0000

    Work on CSD3

--- csd3/ps.sh
+++ csd3/ps.sh
@@ -1,12 +1,12 @@
-# 6-12-2019 JHZ
+# 2-1-2020 JHZ
 
 for catalogue in eQTL pQTL mQTL methQTL GWAS
 do
   export catalogue=${catalogue}
-  R --no-save -q <csd3/ps.R > ps.log
+  R --no-save -q <csd3/ps.R > work/INF1.merge.${catalogue}.log
 done
 
 # gene
-R --no-save -q < csd3/ps.gene.R > ps.gene.log
+R --no-save -q < csd3/ps.gene.R > work/INF1.merge.gene.log
 
 phenoscanner -s T -c All -x EUR -p 0.0000001 --r2 0.6 -i INF1.merge.snp -o INF1
