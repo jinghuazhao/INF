a4d7fa7abc77e3969bc9ca767a11aa115e3f803e
jinghuazhao <jinghuazhao@hotmail.com>
2020-01-20 17:10:16 +0000

    Work on CSD3

--- csd3/annotate.sh
+++ csd3/annotate.sh
@@ -2,6 +2,7 @@
 
 export INF=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/INF
 export ANNOVAR=${HPC_WORK}/annovar
+export POLYPHEN=$HPC_WORK/polyphen-2.2.2
 export TMPDIR=/rds/user/jhz22/hpc-work/work
 
 cd work
@@ -92,6 +93,7 @@ rm a1 a2
 
 cd -
 
+# ProGeM
 # Bottom-up
 # https://www.gtexportal.org/home/datasets
 # Top-down (GenomicRanges, KEGGREST, STRINGdb)
@@ -102,38 +104,10 @@ cd -
 # http://string-db.org/
 # BiocManager::install("garfield")
 
-# https://cran.r-project.org/web/packages/rentrez/vignettes/rentrez_tutorial.html
-R --no-save <<END
-  library(rentrez)
-  library(XML)
-# set_entrez_key("")
-  Sys.getenv("ENTREZ_KEY")
-  entrez_dbs()
-  entrez_db_links("pubmed")
-  pubmed_fields <- entrez_db_searchable("pubmed")
-  r <- entrez_search(db="pubmed",term="(pQTLs OR (protein quantitative locus)) AND 2008:2020[DP] AND homo_sapiens[ORGN]",use_history=TRUE)
-  class(r)
-  names(r)
-  with(r,web_history)
-  unlink(paste("pubmed",c("fetch","summary"),sep="."))
-  for(i in seq(1,with(r,count),50))
-  {
-    cat(i+49, "records downloaded\r")
-    f <- entrez_fetch(db="pubmed", web_history=with(r,web_history), rettype="text", retmax=50, retstart=i)
-    write.table(f, col.names=FALSE, row.names=FALSE, file="pubmed.fetch", append=TRUE)
-    s <- entrez_summary(db="pubmed", web_history=with(r,web_history), rettype="text", retmax=50, retstart=i)
-    fields <- c("uid", "pubdate", "sortfirstauthor", "title", "source", "volume", "pages")
-    e <- extract_from_esummary(s, fields)
-    write.table(t(e), col.names=FALSE, row.names=FALSE, file="pubmed.summary", append=TRUE, sep="\t")
-  }
-  link.example <- function(id=600807)
-  {
-    upload <- entrez_post(db="omim", id=id)
-    asthma_variants <- entrez_link(dbfrom="omim", db="clinvar", cmd="neighbor_history", web_history=upload)
-    asthma_variants
-    snp_links <- entrez_link(dbfrom="clinvar", db="snp",
-                             web_history=asthma_variants$web_histories$omim_clinvar,
-                             cmd="neighbor_history")
-    all_links <- entrez_link(dbfrom='pubmed', id=id, db='all')
-  }
-END
+# Polyphen-2
+
+grep -v -w -f INF1.merge.cis INF1.merge | cut -f6 | sed '1d;s/_/ /;s/_/\//' > INF1.merge.pph.list
+mapsnps.pl -g hg19 -m -U -y INF1.merge.pph.input INF1.merge.pph.list 1>INF1.merge.pph.features 2>INF1.merge.log
+run_pph.pl INF1.merge.pph.input 1>INF1.merge.pph.output 2>INF1.merge.pph.log
+run_weka.pl INF1.merge.pph.output >INF1.merge.humdiv.output
+run_weka.pl -l $POLYPHEN/models/HumVar.UniRef100.NBd.f11.model INF1.merge.pph.output >INF1.merge.humvar.output
---
+++ csd3/reps.sh
@@ -0,0 +1,42 @@
+# 20-1-2020 JHZ
+
+export TMPDIR=/rds/user/jhz22/hpc-work/work
+
+# https://cran.r-project.org/web/packages/rentrez/vignettes/rentrez_tutorial.html
+R --no-save <<END
+  library(rentrez)
+  library(XML)
+# set_entrez_key("")
+  Sys.getenv("ENTREZ_KEY")
+  entrez_dbs()
+  entrez_db_links("pubmed")
+  pubmed_fields <- entrez_db_searchable("pubmed")
+  r <- entrez_search(db="pubmed",term="(pQTLs OR (protein quantitative trait locus)) AND 2008:2020[DP] AND homo_sapiens[ORGN]",use_history=TRUE)
+  class(r)
+  names(r)
+  with(r,web_history)
+  unlink(paste("pubmed",c("fetch","summary"),sep="."))
+  for(i in seq(1,with(r,count),50))
+  {
+    cat(i+49, "records downloaded\r")
+    f <- entrez_fetch(db="pubmed", web_history=with(r,web_history), rettype="text", retmax=50, retstart=i)
+    write.table(f, col.names=FALSE, row.names=FALSE, file="pubmed.fetch", append=TRUE)
+    s <- entrez_summary(db="pubmed", web_history=with(r,web_history), rettype="text", retmax=50, retstart=i)
+    fields <- c("uid", "pubdate", "sortfirstauthor", "title", "source", "volume", "pages")
+    e <- extract_from_esummary(s, fields)
+    write.table(t(e), col.names=FALSE, row.names=FALSE, file="pubmed.summary", append=TRUE, sep="\t")
+  }
+  link.example <- function(id=600807)
+  {
+    upload <- entrez_post(db="omim", id=id)
+    asthma_variants <- entrez_link(dbfrom="omim", db="clinvar", cmd="neighbor_history", web_history=upload)
+    asthma_variants
+    snp_links <- entrez_link(dbfrom="clinvar", db="snp",
+                             web_history=asthma_variants$web_histories$omim_clinvar,
+                             cmd="neighbor_history")
+    all_links <- entrez_link(dbfrom='pubmed', id=id, db='all')
+  }
+END
+
+join <(cut -f2,8,11-13,18,25,26,28,29 annotate/gwas_catalog_v1.0.2-associations_e99_r2020-01-16.tsv | sed '1d' | sort -k1,1) \
+     <(sort -k1,1 work/pubmed.summary)
