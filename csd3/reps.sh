2c0c20d6572a8c72e3336e6f0c64a604aed70320
jinghuazhao <jinghuazhao@hotmail.com>
2020-01-24 17:10:32 +0000

    Work on CSD3

--- csd3/reps.sh
+++ csd3/reps.sh
@@ -1,52 +1,41 @@
-# 23-1-2020 JHZ
+# 24-1-2020 JHZ
 
 export TMPDIR=$INF/work
 
-# https://cran.r-project.org/web/packages/rentrez/vignettes/rentrez_tutorial.html
 R --no-save <<END
-  library(rentrez)
-  library(XML)
-# set_entrez_key("")
-  Sys.getenv("ENTREZ_KEY")
-  entrez_dbs()
-  entrez_db_links("pubmed")
-  pubmed_fields <- entrez_db_searchable("pubmed")
-  r <- entrez_search(db="pubmed",term="(pQTLs OR (protein quantitative trait locus)) AND 2008:2020[DP] AND homo_sapiens[ORGN]",use_history=TRUE)
-  class(r)
-  names(r)
-  with(r,web_history)
-  unlink(paste("pubmed",c("fetch","summary"),sep="."))
-  for(i in seq(1,with(r,count),50))
-  {
-    cat(i+49, "records downloaded\r")
-    f <- entrez_fetch(db="pubmed", web_history=with(r,web_history), rettype="text", retmax=50, retstart=i)
-    write.table(f, col.names=FALSE, row.names=FALSE, file="pubmed.fetch", append=TRUE)
-    s <- entrez_summary(db="pubmed", web_history=with(r,web_history), rettype="text", retmax=50, retstart=i)
-    fields <- c("uid", "pubdate", "sortfirstauthor", "title", "source", "volume", "pages")
-    e <- extract_from_esummary(s, fields)
-    write.table(t(e), col.names=FALSE, row.names=FALSE, file="pubmed.summary", append=TRUE, sep="\t")
-  }
-  link.example <- function(id=600807)
-  {
-    upload <- entrez_post(db="omim", id=id)
-    asthma_variants <- entrez_link(dbfrom="omim", db="clinvar", cmd="neighbor_history", web_history=upload)
-    asthma_variants
-    snp_links <- entrez_link(dbfrom="clinvar", db="snp",
-                             web_history=asthma_variants$web_histories$omim_clinvar,
-                             cmd="neighbor_history")
-    all_links <- entrez_link(dbfrom='pubmed', id=id, db='all')
-  }
+  source("csd3/reps.inc")
+  xlsx <- "https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-018-0175-2/MediaObjects/41586_2018_175_MOESM4_ESM.xlsx"
+  st4 <- openxlsx::read.xlsx(xlsx, sheet=4, colNames=TRUE, startRow=5)
+  st19 <- openxlsx::read.xlsx(xlsx, sheet=19, colNames=TRUE, startRow=2)
+  st20 <- openxlsx::read.xlsx(xlsx, sheet=20, colNames=TRUE, startRow=2)
+  knownlist <- c(with(st19,PMID),"29875488")
+  revlist <- replace(knownlist,knownlist=="PMC4358658",25652787)
+  fetch_knownlist(revlist)
+  r1 <- merge(inf1,st20,by.x="uniprot",by.y="UniProt")
+  r2 <- merge(inf1,st4,by.x="uniprot",by.y="UniProt")
+  intersect(with(r1,uniprot),with(r2,uniprot))
+# https://cran.r-project.org/web/packages/rentrez/vignettes/rentrez_tutorial.html
+# Add a couple of recent papers
+# output list by uniprot ID
 END
+# LD with INF1.merge
 
-export traitmap=gwas_catalog_trait-mappings_r2020-01-16.tsv
+# Additional search
+
+cd annotate
+# https://www.ncbi.nlm.nih.gov/pmc/pmctopmid/
+if [ ! -f PMC-ids.csv.gz ]; then wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/PMC-ids.csv.gz; fi
+zgrep -e PMC4698720 -e PMC4358658 PMC-ids.csv.gz
+# https://www.ebi.ac.uk/gwas/api/search/downloads/alternative
 export assoc=gwas_catalog_v1.0.2-associations_e99_r2020-01-16.tsv
-
-cut -f2,8,11-13,18,22,25,26,28,29,35,36 annotate/$assoc > annotate/assoc.txt
-
+# https://www.ebi.ac.uk/gwas/api/search/downloads/trait_mappings
+export traitmap=gwas_catalog_trait-mappings_r2020-01-16.tsv
+cut -f2,8,11-13,18,22,25,26,28,29,35,36 $assoc > assoc.txt
 join -113 -25 -t$'\t' \
-    <(sed '1d' annotate/assoc.txt | sort -t$'\t' -k13,13) \
-    <(sed '1d' annotate/$traitmap | awk '/protein measure|protein levels/' | sort -t$'\t' -k5,5) | \
-cut -f2 | uniq > annotate/pmid
-
-join <(sort -k1,1 work/pubmed.summary | awk '!/mice|Mice|plant|Plant|rice|soybean|Soybean|tomato/') \
-     <(sed '1d' annotate/assoc.txt | sort -k1,1) > work/pubmed.left
+    <(sed '1d' assoc.txt | sort -t$'\t' -k13,13) \
+    <(sed '1d' $traitmap | awk '/protein measure|protein levels/' | sort -t$'\t' -k5,5) | \
+cut -f2 | uniq > pmid
+join <(awk '!/mice|Mice|plant|Plant|rice|soybean|Soybean|tomato/' pubmed.summary | sort -k1,1) \
+     <(sed '1d' assoc.txt | sort -k1,1) > pubmed.left
+cut -d' ' -f1 pubmed.left | uniq | wc -l
+cd -
