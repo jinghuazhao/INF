e612a4a152c6a8dad7369f9612847543d2352a7c
jinghuazhao <jinghuazhao@hotmail.com>
2020-02-10 09:56:19 +0000

    Work on CSD3

--- csd3/js.R
+++ csd3/js.R
@@ -10,18 +10,16 @@ r <- within(r,{x=x/1.3e8; y=y/1.3e8; log10p <- abs(log10p)})
 cols <- c("id","chr1","x","gene","target","log10p","chr2","y","col")
 d3c <- subset(r[cols],col=="blue")
 d3t <- subset(r[cols],col=="red")
-fixes <- function(col,d) paste(paste(prefix[col],d[,col],sep=":"),postfix)
 prefix <- c("Sentinel variant","CHR","POS","Mapped gene","Target","-log10(p)")
 postfix <- c("</br>")
-d1 <- list(x=d3c$x, y=d3c$y, z=d3c$log10p, text=as.list(apply(sapply(1:6,fixes,d3c),1,paste,collapse=" ")),
-           type="scatter3d", mode="markers", name="cis")
-d1$marker$symbol <- head(xdata[[1]]$marker$symbol,nrow(d3c))
-d2 <- list(x=d3t$x, y=d3t$y, z=d3t$log10p, text=as.list(apply(sapply(1:6,fixes,d3t),1,paste,collapse=" ")),
-           type="scatter3d", mode="markers", name="trans")
-d2$marker$symbol <- head(xdata[[2]]$marker$symbol,nrow(d3t))
+fixes <- function(col,d) paste(paste(prefix[col],d[,col],sep=":"),postfix)
 x <- SomaLogic$x
-x$data[[1]] <- d1
-x$data[[2]] <- d2
+x$data[[1]] <- list(x=d3c$x, y=d3c$y, z=d3c$log10p, text=as.list(apply(sapply(1:6,fixes,d3c),1,paste,collapse=" ")),
+           type="scatter3d", mode="markers", name="cis")
+x$data[[1]]$marker$symbol <- head(xdata[[1]]$marker$symbol,nrow(d3c))
+x$data[[2]] <- list(x=d3t$x, y=d3t$y, z=d3t$log10p, text=as.list(apply(sapply(1:6,fixes,d3t),1,paste,collapse=" ")),
+           type="scatter3d", mode="markers", name="trans")
+x$data[[2]]$marker$symbol <- head(xdata[[2]]$marker$symbol,nrow(d3t))
 Olink <- SomaLogic
 Olink$x <- x
 sink("js.json")
