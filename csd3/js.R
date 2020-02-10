94e223e167301ee9f9a5c8c8009e35131543ea0b
jinghuazhao <jinghuazhao@hotmail.com>
2020-02-10 10:04:03 +0000

    Work on CSD3

--- csd3/js.R
+++ csd3/js.R
@@ -2,18 +2,18 @@
 
 library(jsonlite)
 SomaLogic <- read_json("js.SomaLogic")
-xdata <- SomaLogic$x$data
 library(gap)
 d <- read.table("INF1.merge.cis.vs.trans",as.is=TRUE,header=TRUE)
 r <-  mhtplot2d(d)
 r <- within(r,{x=x/1.3e8; y=y/1.3e8; log10p <- abs(log10p)})
-cols <- c("id","chr1","x","gene","target","log10p","chr2","y","col")
-d3c <- subset(r[cols],col=="blue")
-d3t <- subset(r[cols],col=="red")
+x <- with(SomaLogic,x)
+xdata <- with(x,data)
 prefix <- c("Sentinel variant","CHR","POS","Mapped gene","Target","-log10(p)")
 postfix <- c("</br>")
 fixes <- function(col,d) paste(paste(prefix[col],d[,col],sep=":"),postfix)
-x <- SomaLogic$x
+cols <- c("id","chr1","x","gene","target","log10p","chr2","y","col")
+d3c <- subset(r[cols],col=="blue")
+d3t <- subset(r[cols],col=="red")
 x$data[[1]] <- list(x=d3c$x, y=d3c$y, z=d3c$log10p, text=as.list(apply(sapply(1:6,fixes,d3c),1,paste,collapse=" ")),
            type="scatter3d", mode="markers", name="cis")
 x$data[[1]]$marker$symbol <- head(xdata[[1]]$marker$symbol,nrow(d3c))
