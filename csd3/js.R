62d1b85132c13872f96309d8f9c884093690ef0d
jinghuazhao <jinghuazhao@hotmail.com>
2020-02-10 16:44:51 +0000

    Work on CSD3

--- csd3/js.R
+++ csd3/js.R
@@ -11,16 +11,16 @@ prefix <- c("Sentinel variant","CHR","POS","Mapped gene","Target","-log10(p)")
 postfix <- c("</br>")
 fixes <- function(col,d) paste(paste(prefix[col],d[,col],sep=":"),postfix)
 cols <- c("id","chr1","x","gene","target","log10p","chr2","y","col")
-a <- subset(r[cols],col=="blue")
-b <- subset(r[cols],col=="red")
-x$data[[1]] <- list(x=a$x, y=a$y, z=a$log10p, text=as.list(apply(sapply(1:6,fixes,a),1,paste,collapse=" ")),
-           type="scatter3d", mode="markers", name="trans")
-x$data[[1]]$marker$symbol <- rep('circle',nrow(a))
-x$data[[1]]$marker$size <- 3
-x$data[[2]] <- list(x=b$x, y=b$y, z=b$log10p, text=as.list(apply(sapply(1:6,fixes,b),1,paste,collapse=" ")),
+a <- subset(r[cols],col=="red")
+x$data[[2]] <- list(x=a$x, y=a$y, z=a$log10p, text=as.list(apply(sapply(1:6,fixes,a),1,paste,collapse=" ")),
            type="scatter3d", mode="markers", name="cis")
-x$data[[2]]$marker$symbol <- rep('circle',nrow(b))
+x$data[[2]]$marker$symbol <- rep('circle',nrow(a))
 x$data[[2]]$marker$size <- 3
+b <- subset(r[cols],col=="blue")
+x$data[[1]] <- list(x=b$x, y=b$y, z=b$log10p, text=as.list(apply(sapply(1:6,fixes,b),1,paste,collapse=" ")),
+           type="scatter3d", mode="markers", name="trans")
+x$data[[1]]$marker$symbol <- rep('circle',nrow(b))
+x$data[[1]]$marker$size <- 3
 Olink <- SomaLogic
 Olink$x <- x
 sink("js.json")
