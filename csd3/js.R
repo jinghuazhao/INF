197e085cc95f73dc3b9a51792fa974c52629f369
jinghuazhao <jinghuazhao@hotmail.com>
2020-02-10 14:44:38 +0000

    Work on CSD3

--- csd3/js.R
+++ csd3/js.R
@@ -7,7 +7,6 @@ d <- read.table("INF1.merge.cis.vs.trans",as.is=TRUE,header=TRUE)
 r <-  mhtplot2d(d)
 r <- within(r,{x=x/1.3e8; y=y/1.3e8; log10p <- abs(log10p)})
 x <- with(SomaLogic,x)
-xdata <- with(x,data)
 prefix <- c("Sentinel variant","CHR","POS","Mapped gene","Target","-log10(p)")
 postfix <- c("</br>")
 fixes <- function(col,d) paste(paste(prefix[col],d[,col],sep=":"),postfix)
@@ -16,12 +15,12 @@ d3c <- subset(r[cols],col=="blue")
 d3t <- subset(r[cols],col=="red")
 x$data[[1]] <- list(x=d3c$x, y=d3c$y, z=d3c$log10p, text=as.list(apply(sapply(1:6,fixes,d3c),1,paste,collapse=" ")),
            type="scatter3d", mode="markers", name="trans")
-x$data[[1]]$marker$symbol <- head(xdata[[1]]$marker$symbol,nrow(d3c))
-x$data[[1]]$marker$size <- 4
+x$data[[1]]$marker$symbol <- rep('circle',nrow(d3c))
+x$data[[1]]$marker$size <- 3
 x$data[[2]] <- list(x=d3t$x, y=d3t$y, z=d3t$log10p, text=as.list(apply(sapply(1:6,fixes,d3t),1,paste,collapse=" ")),
            type="scatter3d", mode="markers", name="cis")
-x$data[[2]]$marker$symbol <- head(xdata[[2]]$marker$symbol,nrow(d3t))
-x$data[[2]]$marker$size <- 4
+x$data[[2]]$marker$symbol <- rep('circle',nrow(d3t))
+x$data[[2]]$marker$size <- 3
 Olink <- SomaLogic
 Olink$x <- x
 sink("js.json")
@@ -29,4 +28,3 @@ toJSON(Olink,auto_unbox=TRUE,pretty=TRUE)
 sink()
 
 # sed -i 's|<\\/br>|\\u003c/br>|g' js.json
-
