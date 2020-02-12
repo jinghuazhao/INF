2bb82af9d0101675e460eda36720a264a056c481
jinghuazhao <jinghuazhao@hotmail.com>
2020-02-12 17:02:25 +0000

    Work on CSD3

--- csd3/plink.sh
+++ csd3/plink.sh
@@ -1,10 +1,8 @@
 # 12-2-2020 JHZ
 
 export UKB=/rds/project/jmmh2/rds-jmmh2-post_qc_data/uk_biobank/imputed/uk10k_hrc/HRC_UK10K
-qctool \
-    -g ${UKB}/ukb_imp_chr#_v3.bgen -s $UKB/ukb_BP_imp_v3.sample \
-    -ofiletype bgen_v1.1 -og work/crp.score.bgen \
-    -incl-snpids work/INF1.merge.ukbsnpid
+qctool -g ${UKB}/ukb_imp_chr#_v3.bgen -s $UKB/ukb_BP_imp_v3.sample \
+       -ofiletype bgen_v1.1 -og work/crp.score.bgen -incl-snpids work/INF1.merge.ukbsnpid
 
 grep -f work/INF1.merge.ukbsnpid -w work/crp.ukb | \
 awk '
@@ -17,12 +15,11 @@ awk '
 export p="0.001 0.05 0.1 0.2 0.3 0.4 0.5"
 awk 'BEGIN{split(ENVIRON["p"],a);for(i=1;i<=length(a);i++) print a[i],0,a[i]}' > work/crp.range_list
 
-plink \
-    --bgen work/crp.score.bgen --sample $UKB/ukb_BP_imp_v3.sample \
-    --score work/crp.SNP.EA.beta.pvalue 1 2 3 double-dosage \
-    --q-score-range work/crp.range_list work/crp.SNP.EA.beta.pvalue 1 4 \
-    --pheno work/crp.cvd --pheno-name cv \
-    --out work/crp.score
+plink --bgen work/crp.score.bgen --sample $UKB/ukb_BP_imp_v3.sample \
+      --score work/crp.SNP.EA.beta.pvalue 1 2 3 double-dosage \
+      --q-score-range work/crp.range_list work/crp.SNP.EA.beta.pvalue 1 4 \
+      --pheno work/crp.cvd --pheno-name cv \
+      --out work/crp.score
 
 R --no-save <<END
   p <- Sys.getenv("p")
