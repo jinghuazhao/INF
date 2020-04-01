26efa0083a8e0a368e637a9eb2051dd99db70b9b
jinghuazhao <jinghuazhao@hotmail.com>
2020-04-01 15:55:27 +0100

    Work on CSD3

--- csd3/lookup.sh
+++ csd3/lookup.sh
@@ -2,6 +2,7 @@
 
 function check()
 {
+  grep $1 work/INF1.merge.rsid
   grep $1 work/INF1.merge.cis.vs.trans
   grep $1 work/INF1.merge.cis.vs.trans | cut -d' ' -f2 | sed 's/"//g'
   export prot=$(grep $1 work/INF1.merge.cis.vs.trans | cut -d' ' -f2)
@@ -10,26 +11,26 @@ function check()
 
 function Sun()
 {
-  grep $1 pQTL.Sun-B_pQTL_EUR_2017
+  grep $1 work/ps/pQTL.Sun-B_pQTL_EUR_2017
   check $1
 }
 
 function Folkersen()
 {
-  grep $1 pQTL.Folkersen-L_Proteins_EUR_2017
+  grep $1 work/ps/pQTL.Folkersen-L_Proteins_EUR_2017
   check $1
 }
 
 function Suhre()
 {
-  grep $1 pQTL.Suhre-K_pQTL_EUR_2017
+  grep $1 work/ps/pQTL.Suhre-K_pQTL_EUR_2017
   check $1
 }
 
 function pQTL()
 {
-  grep $1 pQTL.pQTL_2017
+  grep $1 work/ps/pQTL.pQTL_2017
   check $1
 }
 
-Suhre $1
+Folkersen $1
