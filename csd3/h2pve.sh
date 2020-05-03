24faaf9d3d77afdfd1edae25e0559842c30a1ffb
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-03 21:48:59 +0100

    Work on CSD3

---
+++ csd3/h2pve.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+join <(sort -k1,1 h2.tsv) <(sed '1d' pve.dat | sort -k1,1) > h2pve.dat
+R --no-save -q <<END
+  h2pve <- read.table("h2pve.dat",col.names=c("prot","h2","h2se","pve","sepve","m"))
+  subset(h2pve, h2>0.3 & pve > 0.3)
+  png("h2pve.png",width=6,height=5,units="in",pointsize=4,res=300)
+  attach(h2pve)
+  cor(h2,pve)
+  plot(h2,pve,pch=19)
+  abline(lm(pve~h2), col="red")
+  lines(lowess(h2,pve), col="blue")
+  detach(h2pve)
+  dev.off()
+END
