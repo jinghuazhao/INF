e9e692069b02229e5e9c99098e534dd7adb09688
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-06 18:06:10 +0100

    Work on CSD3

--- csd3/ma.do
+++ csd3/ma.do
@@ -1,14 +1,21 @@
-// 6-11-2019 JHZ
+// 6-5-2020 JHZ
 
 local p : env p
 insheet x1-x25 using `p'.ab, clear
 save `p'.dta, replace
 use `p'.dta if x13 < -323
-sort x1 x25 x13
-by x1 x25: gen index=_n
-save `p'0.dta, replace
-use `p'.dta if x13 >= -323
-append using `p'0.dta
+gen index=.
+if _N>0 {
+  sort x1 x25 x13
+  by x1 x25: replace index=_n
+}
+append using `p'.dta
+drop if index==. & x13 < -323
 replace index=0 if index==.
 sort x25 x4
 outsheet x1-x25 index using `p'.txt, replace noquote
+
+// x1 = chr1
+// x4 = SNPID
+// x13= log10(p)
+
--- csd3/ma.sh
+++ csd3/ma.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/bash
-# 6-11-2019 JHZ
 
 module load ceuadmin/stata/15
 
-export tag=_nold
 export inf1=$(ls METAL/*-1.tbl.gz | sed 's|METAL/||g;s/-1.tbl.gz//g;s/\*//g')
 
 for p in ${inf1[@]}
-do export p=${p}
+do
+   export p=${p}
    echo ${p}
    (
      zcat METAL/${p}-1.tbl.gz | head -1 | awk -vOFS="\t" '{$1="Chrom";$2="Start" "\t" "End";print}'
      zcat sentinels/${p}.p.gz | \
-     awk -vOFS="\t" '{$1="chr" $1; start=$2-1;$2=start "\t" $2;print}' | \
-     awk '!($1 == "chr6" && $3 >= 25392021 && $3 < 33392022)'
+     awk -vOFS="\t" '{$1="chr" $1; start=$2-1;$2=start "\t" $2;print}'
    ) > ${p}.a
    export p0=$(awk 'NR>1&&10^$13==0' ${p}.a | wc -l | cut -d' ' -f1)
    (
@@ -26,7 +24,7 @@ do export p=${p}
          bedtools intersect -wb -a ${p}.a -b ${p}.b > ${p}.ab
          stata -b -q csd3/ma.do 
          awk -f csd3/ma.awk ${p}.txt
-         rm ${p}.a ${p}.b ${p}.ab ${p}.txt ${p}.dta ${p}0.dta
+         rm ${p}.a ${p}.b ${p}.ab ${p}.txt ${p}.dta
       fi
    ) > sentinels/${p}.ma
     export lines=$(wc -l sentinels/${p}.ma | cut -d' ' -f1)
@@ -36,3 +34,6 @@ do export p=${p}
     fi
 done
 rm ma.log
+
+# The Stata code implements an old trick (Sun et al. 2018) such that only one p==0 is kept and others reset to 1.
+# However, the latter version of GCTA uses effect size and this is unnecessary.
