4b9c267c23d94015e20a43e5a56c8bee38e79125
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-07 17:15:30 +0100

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/finemap.sh
+++ rsid/finemap.sh
@@ -4,9 +4,7 @@
 # module load gcc/5.4.0 lapack/3.8.0 qctool/2.0.6
 # module load bgen/20180807
 
-join -v2 <(sed '1d' sentinels/INF1.jma-rsid | cut -f1,2 | tr '\t' '-' | sort) \
-         <(sed '1d' work/INF1.merge-rsid | cut -f5,6 | awk '{print $1 "-" $2,NR}' | sort -k1,1) | \
-tr '-' ' ' | \
+awk 'NR>1 {print $5,$6,NR-1}' work/INF1.merge-rsid | \
 parallel --env INF -C' ' '
   echo {1}-{2} {3}
   ${INF}/rsid/finemap.ini {3}
