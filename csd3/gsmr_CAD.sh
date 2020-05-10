afc40c9026dd683048f2709db20de348288d63ad
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-10 20:31:23 +0100

    Work on CSD3

--- csd3/gsmr_CAD.sh
+++ csd3/gsmr_CAD.sh
@@ -1,4 +1,35 @@
-# 30-8-2019 JHZ
+# 10-5-2020 JHZ
+
+(
+  echo "SNP A1 A2 freq b se p N"
+  unzip -p -c cad.additive.Oct2015.pub.zip | \
+  awk '
+  {
+    CHR=$2
+    POS=$3
+    a1=$4
+    a2=$5
+    if (a1>a2) snpid="chr" CHR ":" POS "_" a2 "_" a1;
+    else snpid="chr" CHR ":" POS "_" a1 "_" a2
+    if (substr($1,1,2)=="rs"||substr($1,1,3)=="chr"||substr($1,1,3)=="MER")
+       print snpid, a1, a2, $6, $9, $10, $11, 185000
+  }'
+) > gsmr_CAD.txt
+
+# unzip -p -c CAD/cad.additive.Oct2015.pub.zip | head -1 | sed 's/\t/\n/g' | awk '{print "#" NR, $1}'
+#1 markername
+#2 chr
+#3 bp_hg19
+#4 effect_allele
+#5 noneffect_allele
+#6 effect_allele_freq
+#7 median_info
+#8 model
+#9 beta
+#10 se_dgc
+#11 p_dgc
+#12 het_pvalue
+#13 n_studies
 
 if [ -f work/INF1_CAD.gsmr ]; then rm work/INF1_CAD.gsmr; fi
 (
--- csd3/gsmr_FEV1.sh
+++ csd3/gsmr_FEV1.sh
@@ -1,4 +1,43 @@
-# 19-8-2019 JHZ
+# 10-5-2020 JHZ
+
+(
+  echo "SNP A1 A2 freq b se p N"
+  gunzip -c UKB_FEV1_results.txt.gz | awk '
+  {
+    CHR=$2
+    POS=$3
+    a1=$4
+    a2=$5
+    if (a1>a2) snpid="chr" CHR ":" POS "_" a2 "_" a1;
+    else snpid="chr" CHR ":" POS "_" a1 "_" a2
+    if (NR>1) print snpid, a1, a2, $7, $8, $9, $10, 321047
+  }'
+) > gsmr_FEV1.txt
+
+# gunzip -c UKB_FEV1_results.txt.gz | head -1 | sed 's/\t/\n/g' | awk '{print "#" NR, $1}'
+#1 #SNP
+#2 Chromosome
+#3 Position_b37
+#4 Coded
+#5 Non_coded
+#6 INFO
+#7 Coded_freq
+#8 beta
+#9 SE_GC
+#10 P_GC
+
+# gunzip -c SpiroMeta_FEV1_results.txt.gz | head -1 | sed 's/\t/\n/g' | awk '{print "#" NR, $1}'
+#1 #SNP
+#2 Chromosome
+#3 Position_b37
+#4 Coded
+#5 Non_coded
+#6 N
+#7 Neff
+#8 Coded_freq
+#9 beta
+#10 SE
+#11 P
 
 if [ -f work/INF1_FEV1.gsmr ]; then rm work/INF1_FEV1.gsmr; fi
 (
