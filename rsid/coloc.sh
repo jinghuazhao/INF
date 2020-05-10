e5ece2dfde09b04610c541d1a246c136b09e9a73
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-10 21:11:33 +0100

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/coloc.sh
+++ rsid/coloc.sh
@@ -20,3 +20,10 @@ R --no-save -q <<END
   # Colocalisation analyses
   results <- hyprcoloc(betas, ses, trait.names=traits, snp.id=rsid)
 END
+
+zcat Whole_Blood.allpairs.txt.gz | cut -f1 | awk 'NR>1{print substr($1,1,15)}' | sort | uniq > work/wb
+R --no-save -q <<END
+   wb <- scan("work/wb", what="")
+   g <- within(subset(grex::grex(wb),!is.na(uniprot_id)),{uniprot=trimws(uniprot_id)})
+   m <- merge(gap::inf1,g,by="uniprot",all.x=TRUE)
+END
