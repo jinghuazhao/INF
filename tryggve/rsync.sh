a367e2b4fcb7ed878202f42783c13262f8ca9b1d
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-10 21:22:01 +0100

    Notes and programs on TRYGGVE

--- tryggve/rsync.sh
+++ tryggve/rsync.sh
@@ -1,4 +1,4 @@
-# 10-10-2019 JHZ
+# 10-5-2020 JHZ
 
 rsync -avrzP . jinhua@10.44.124.6:/home/jinhua/INF \
-      --exclude=.git --exclude=sumstats --exclude=METAL --exclude=work
+      --exclude=.git --exclude=ds --exclude=sumstats --exclude=METAL --exclude=work
