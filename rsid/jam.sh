36a0f6d8fb85082835fb08d1a510715a45de122a
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-10 12:11:14 +0100

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/jam.sh
+++ rsid/jam.sh
@@ -11,7 +11,7 @@ module load gcc/6
 cut -d' ' -f1,2 ${sample} | \
 awk 'NR>2' > ${INF}/work/${study}.id
 awk 'NR>1 {print $5,$6,NR-1}' ${INF}/work/INF1.merge-rsid | \
-parallel -j2 --env INF --env study --env sample --env N --env TMPDIR --env dir -C' ' '
+parallel --env INF --env study --env sample --env N --env TMPDIR --env dir -C' ' '
   echo {1}-{2} {3}
   ${INF}/rsid/jam.ini {3}
 '
