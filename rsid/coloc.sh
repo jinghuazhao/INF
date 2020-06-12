b71b27e182dec21d53435ed261dfce96734379fd
jinghuazhao <jinghuazhao@hotmail.com>
2020-06-12 15:28:52 +0100

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/coloc.sh
+++ rsid/coloc.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 export trait=$1
-export tissue=Brain_Hypothalamus.P01
+export tissue=Whole_Blood.P01
+export P01=$(echo ${tissue} | sed 's/.P01//')
 export eQTL=${HOME}/genetics/VCF-liftover
 export FUSION=${HPC_WORK}/fusion_twas
 
@@ -46,29 +47,31 @@ gunzip -c ${INF}/METAL/${trait}-1.tbl.gz | \
   awk -v OFS='\t' 'BEGIN{print "SNP","A1","A2","N","CHISQ","Z"}'
   awk -v OFS='\t' 'NR>1{print $3,toupper($4),toupper($5),$18,($10/$11)^2,$10/$11}'
 ) > ${trait}.sumstats
-
+ln -sf ${FUSION}/WEIGHTS WEIGHTS
 (
   awk -v OFS='\t' 'BEGIN{print "PANEL","FILE","ID","CHR","P0","P1","HSQ","BEST.GWAS.ID","BEST.GWAS.Z",
                   "EQTL.ID","EQTL.R2","EQTL.Z","EQTL.GWAS.Z","NSNP","NWGT",
                   "MODEL","MODELCV.R2","MODELCV.PV","TWAS.Z","TWAS.P",
                   "COLOC.PP0","COLOC.PP1","COLOC.PP2","COLOC.PP3","COLOC.PP4"}'
-  seq 22 | \
-  parallel --env FUSION --env trait --env tissue -C' ' '
-  Rscript ${FUSION}/FUSION.assoc_test.R \
-          --sumstats ${trait}.sumstats \
-          --weights ${FUSION}/WEIGHTS/${tissue}.pos \
-          --weights_dir ${FUSION}/WEIGHTS \
-          --ref_ld_chr ${FUSION}/LDREF/1000G.EUR. \
-          --chr {} \
-          --coloc_P 5e-8 \
-          --GWASN 15335 \
-          --out ${trait}-${tissue}-{}.dat
-  cat ${trait}-${tissue}-{}.dat | \
-  if [ {} -eq 1 ]; then cat; else awk "NR>1"; fi
-  rm ${trait}-${tissue}-{}.dat
-  '
-) | \
-(
-  grep -v -e WARNING -e skipped -e complete -e consider
+  (
+    seq 22 | \
+    parallel --env FUSION --env trait --env tissue -C' ' '
+    Rscript ${FUSION}/FUSION.assoc_test.R \
+            --sumstats ${trait}.sumstats \
+            --weights ${FUSION}/WEIGHTS/${tissue}.pos \
+            --weights_dir WEIGHTS \
+            --ref_ld_chr ${FUSION}/LDREF/1000G.EUR. \
+            --chr {} \
+            --coloc_P 1e-4 \
+            --GWASN 15335 \
+            --out ${trait}-${tissue}-{}.dat
+    cat ${trait}-${tissue}-{}.dat | \
+    if [ {} -eq 1 ]; then cat; else awk "NR>1"; fi
+    rm ${trait}-${tissue}-{}.dat
+    '
+  ) | \
+  grep -v -e WARNING -e skipped -e complete -e consider -e MHC -e TWAS | \
+  sed "s|WEIGHTS/${P01}/${P01}.||g;s/.wgt.RDat//g" | \
+  sort -k4,4n -k5,5n
 ) > ${trait}-${tissue}.dat
 cd -
