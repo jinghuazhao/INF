5609d21773a8bb34e3034d7b63ab699ccd87ef83
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-10-06 22:09:34 +0100

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/efo.R
+++ rsid/efo.R
@@ -1,34 +1,82 @@
-library(ontologyIndex)
+# https://bioconductor.org/packages/release/bioc/vignettes/gwascat/inst/doc/gwascatOnt.html
 
-id <- function(ontology)
-{
-  inflammatory <- grep(ontology$name,pattern="inflammatory")
-  immune <- grep(ontology$name,pattern="immune")
-  inf <- union(inflammatory,immune)
-  list(id=ontology$id[inf],name=ontology$name[inf])
-}
+library(gwascat)
+requireNamespace("graph")
+makeCurrentGwascat()
+data(efo.obo.g)
+efo.obo.g
+head(graph::nodes(efo.obo.g))
+graph::nodeData(efo.obo.g, "EFO:0000540")
+ue = graph::ugraph(efo.obo.g)
+neighISD = graph::adj(ue, "EFO:0000540")[[1]]
+sapply(graph::nodeData(graph::subGraph(neighISD, efo.obo.g)), "[[", "name")
+requireNamespace("RBGL")
+p = RBGL::sp.between(efo.obo.g, "EFO:0000685", "EFO:0000001")
+sapply(graph::nodeData(graph::subGraph(p[[1]]$path_detail, efo.obo.g)), "[[", "name")
+data(ebicat37)
+names(S4Vectors::mcols(ebicat37))
+adinds = grep("autoimmu|inflamm", ebicat37$MAPPED_TRAIT)
+adgr = ebicat37[adinds]
+adgr
+write.table(as.data.frame(adgr),file="docs/gwas_autoinfl.tsv",quote=FALSE,row.names=FALSE,sep="\t")
+
+# A list of 30 excluding Osteoarthritis based on
+# http://pi.well.ox.ac.uk:3010/pidb/gateway
+
+fang_list <- subset(read.delim("docs/fang.list.txt"),fullname!="Osteoarthritis")
+nd = graph::nodeData(efo.obo.g)
+alldef = sapply(nd, function(x) unlist(x[["def"]]))
+allnames = sapply(nd, function(x) unlist(x[["name"]]))
+alld = sapply(alldef, function(x) if(is.null(x)) return(" ") else x[1])
+df = data.frame(id = names(allnames), concept=as.character(allnames), def=unlist(alld))
+autoinfl = df[grep("autoimm|inflam", df$def, ignore.case=TRUE),]
+requireNamespace("DT")
+suppressWarnings({DT::datatable(autoinfl, rownames=FALSE, options=list(pageLength=5))})
+inf1 <- data.frame()
+for (str in with(fang_list,fullname)) inf1 <- rbind(inf1,df[grep(str, df$def, ignore.case=TRUE),])
+options(width=200)
+inf1 <- unique(inf1)
+write.table(inf1,file="docs/fang.efos.txt",quote=FALSE,row.names=FALSE,sep="\t")
+
+# https://cran.r-project.org/web/packages/ontologyIndex/vignettes/intro-to-ontologyX.html
+
+library(ontologyIndex)
 # GO
 data(go)
-goidname <- id(go)
 # EFO
 file <- "annotate/efo.obo"
 get_relation_names(file)
 efo <- get_ontology(file, extract_tags="everything")
-length(efo) # 89
-length(efo$id) # 27962
+
+id <- function(ontology)
+{
+  length(ontology)
+  length(ontology$id)
+  inf <- grep(ontology$name,pattern="immune|inflammatory")
+  data.frame(id=ontology$id[inf],name=ontology$name[inf])
+}
+
+goidname <- id(go)
 efoidname <- id(efo)
 diseases <- get_descendants(efo,"EFO:0000408")
 efo_0000540 <- get_descendants(efo,"EFO:0000540")
 efo_0000540name <- efo$name[efo_0000540]
 isd <- data.frame(efo_0000540,efo_0000540name)
-save(efo,diseases,isd,efoidname,goidname, file="work/efo.rda")
-write.table(isd,file="work/efo_0000540.csv",col.names=FALSE,row.names=FALSE,sep=",")
-pdf("work/efo_0000540.pdf",height=15,width=15)
-library(ontologyPlot)
-onto_plot(efo,efo_0000540)
-dev.off()
 
-# http://www.ebi.ac.uk/efo/releases/v3.14.0/efo.owl
+fang_efo <- with(inf1,id)
+fang_disease <- data.frame(id=efo$id[fang_efo],name=efo$name[fang_efo])
+save(efo,diseases,fang_disease,isd,efoidname,goidname, file="work/efo.rda")
+write.table(isd,file="work/efo_0000540.csv",col.names=FALSE,row.names=FALSE,sep=",")
+
+efo_0000540_plot <- function()
+{
+  library(ontologyPlot)
+  pdf("work/efo_0000540.pdf",height=15,width=15)
+  onto_plot(efo,efo_0000540)
+  dev.off()
+}
+
 # http://www.ebi.ac.uk/efo/efo.obo
+# http://www.ebi.ac.uk/efo/releases/v3.14.0/efo.owl
 # Malone J, et al. (2010) Modeling sample variables with an Experimental Factor Ontology. Bioinformatics, 26(8): 1112–1118.
 # Fang H, et al. (2019). A genetics-led approach defines the drug target landscape of 30 immune-related traits. Nature Genetics 51(7): 1082-1091.
--- rsid/pqtlGWAS.R
+++ rsid/pqtlGWAS.R
@@ -5,16 +5,17 @@ library(pQTLtools)
 
 INF <- Sys.getenv("INF")
 INF1_metal <- within(read.delim(file.path(INF,"work","INF1.METAL"),as.is=TRUE),{
-                     rsID=rsid;
-                     hg19_coordinates=paste0("chr",Chromosome,":",Position)})
+                     hg19_coordinates=paste0("chr",Chromosome,":",Position)}) %>%
+              rename(INF1_rsid=rsid) %>%
+              rename(Total=N)
 
 INF1_aggr <- INF1_metal %>%
-  select(Chromosome,Position,prot,hg19_coordinates,MarkerName,rsID,Allele1,Allele2,Freq1,Effect,StdErr,log.P.,cis.trans) %>%
-  group_by(Chromosome,Position,MarkerName,rsID,hg19_coordinates) %>%
+  select(Chromosome,Position,prot,hg19_coordinates,MarkerName,Allele1,Allele2,Freq1,Effect,StdErr,log.P.,cis.trans,INF1_rsid) %>%
+  group_by(Chromosome,Position,MarkerName,INF1_rsid,hg19_coordinates) %>%
   summarise(nprots=n(),
             prots=paste(prot,collapse=";"),
-            A1=paste(toupper(Allele1),collapse=";"),
-            A2=paste(toupper(Allele2),collapse=";"),
+            Allele1=paste(toupper(Allele1),collapse=";"),
+            Allele2=paste(toupper(Allele2),collapse=";"),
             EAF=paste(Freq1,collapse=";"),
             Effects=paste(Effect,collapse=";"),
             SEs=paste(StdErr,collapse=";"),
@@ -23,7 +24,7 @@ INF1_aggr <- INF1_metal %>%
   ) %>%
   data.frame()
 
-rsid <- INF1_aggr[["rsID"]]
+rsid <- INF1_aggr[["INF1_rsid"]]
 catalogue <- "GWAS"
 proxies <- "EUR"
 p <- 5e-8 / nrow(INF1_metal)
@@ -32,16 +33,62 @@ build <- 37
 
 r <- snpqueries(rsid, catalogue=catalogue, proxies=proxies, p=p, r2=r2, build=build)
 lapply(r,dim)
-ps <- with(r,right_join(snps,results))
+ps <- subset(with(r,right_join(snps,results)),select=-c(hg38_coordinates,ref_hg38_coordinates,pos_hg38,ref_pos_hg38,dprime))
 save(INF1_aggr,r,ps,file=file.path(INF,"work","INF1.merge.GWAS"))
 
-efo_list_immune <- subset(read.csv("work/efo_list_annotated.csv",as.is=TRUE),immune_mediated==1)
-isd1 <- subset(merge(INF1_aggr,subset(ps,efo%in%with(efo_list_immune,EFO)),by="hg19_coordinates"),select=-c(Chromosome,Position))
-write.table(isd1,file="isd1.tsv",row.names=FALSE,quote=FALSE,sep="\t")
+isd123 <- function()
+{
+  efo_list_immune <- subset(read.csv("work/efo_list_annotated.csv",as.is=TRUE),immune_mediated==1)
+  isd1 <- merge(aggr,subset(ps,efo%in%with(efo_list_immune,EFO)),by="hg19_coordinates")
+  write.table(isd1,file="isd1.tsv",row.names=FALSE,quote=FALSE,sep="\t")
 
-load("work/efo.rda")
-efo_0000540 <- gsub(":","_",as.data.frame(isd)[["efo_0000540"]])
-isd2 <- subset(merge(INF1_aggr,subset(ps,efo%in%efo_0000540),by="hg19_coordinates"),select=-c(Chromosome,Position))
-write.table(isd2,file="isd2.tsv",row.names=FALSE,quote=FALSE,sep="\t")
+  load("work/efo.rda")
+  efo_0000540 <- gsub(":","_",as.data.frame(isd)[["efo_0000540"]])
+  isd2 <- merge(aggr,subset(ps,efo%in%efo_0000540),by="hg19_coordinates")
+  write.table(isd2,file="isd2.tsv",row.names=FALSE,quote=FALSE,sep="\t")
 
-isd2[c("MarkerName","rsID","prots","trait","efo","study","pmid","ancestry","year","beta","se")]
+  fang_efo <- gsub(":","_",with(read.delim("doc/fang.efos.txt",as.is=TRUE),id))
+  isd3 <- merge(aggr,subset(ps,efo%in%fang_efo),by="hg19_coordinates")
+  write.table(isd3,file="isd3.tsv",row.names=FALSE,quote=FALSE,sep="\t")
+}
+
+metal <- subset(within(INF1_metal,{HLA <- as.numeric(Chromosome==6 & Position >= 25392021 & Position <= 33392022)}),
+                select=-c(Chromosome,Position,INF1_rsid,Direction))
+aggr <- subset(within(INF1_aggr,{HLA <- as.numeric(Chromosome==6 & Position >= 25392021 & Position <= 33392022)}),
+               select=-c(Chromosome,Position,INF1_rsid))
+jimmy <- read.delim("doc/immune.efos.txt",as.is=TRUE)
+jimmy_efo <- gsub(":","_",with(jimmy,id))
+long <- merge(metal,subset(ps,efo%in%jimmy_efo),by="hg19_coordinates")
+short <- merge(aggr,subset(ps,efo%in%jimmy_efo),by="hg19_coordinates")
+
+require(openxlsx)
+xlsx <- "work/pqtlgwas.xlsx"
+wb <- createWorkbook(xlsx)
+addWorksheet(wb, "METAL")
+writeDataTable(wb, "METAL", subset(INF1_metal,select=-c(INF1_rsid,hg19_coordinates)))
+addWorksheet(wb, "ps")
+writeDataTable(wb, "ps", ps)
+addWorksheet(wb, "EFO")
+writeDataTable(wb, "EFO", jimmy)
+addWorksheet(wb, "long")
+writeDataTable(wb, "long", 
+               subset(long,select=-c(hg19_coordinates,ref_hg19_coordinates,ref_protein_position,ref_amino_acids,snp,snpid,protein_position,amino_acids)))
+addWorksheet(wb, "short")
+writeDataTable(wb, "short", 
+               subset(short,select=-c(hg19_coordinates,ref_hg19_coordinates,ref_protein_position,ref_amino_acids,snp,snpid,protein_position,amino_acids)))
+saveWorkbook(wb, file=xlsx, overwrite=TRUE)
+
+view <- function(id,efoid,
+                 v=c("MarkerName","Allele1","Allele2","a1","a2","efo","ref_a1","ref_a1","proxy","r2","beta","se","p","trait","ancestry","pmid","study"))
+{
+  options(width=200)
+  cat(id,efoid,"\n")
+  d <- subset(short[v],MarkerName==id & efo==efoid)
+  subset(d,select=-c(MarkerName,efo))
+}
+# celiac
+view("chr12:111884608_C_T", "EFO_0001060")
+# T1D
+view("chr12:111884608_C_T", "EFO_0001359")
+# Hypothyroidism
+view("chr12:111884608_C_T", "EFO_0004705")
