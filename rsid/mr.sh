9918592ce90efb8b6934dc16d69416984ee9ea23
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-10-30 15:56:41 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/mr.sh
+++ rsid/mr.sh
@@ -11,9 +11,10 @@ parallel -j1 -C' ' '
       {
         if(ENVIRON["suffix"]=="cis") if(\$1==chr && \$2>=start-M && \$2 <= end+M && \$9<=logp) print; else if(\$9<=logp) print
       }" > work/{2}.mri-${suffix}
-  cut -f3 work/{2}.mrin > work/{2}.mrs-${suffix}
+  cut -f3 work/{2}.mri-${suffix} > work/{2}.mrs-${suffix}
   plink --bfile INTERVAL/cardio/INTERVAL --extract work/{2}.mrs-${suffix} --geno 0.1 --mind 0.1 --maf 0.005 --indep-pairwise 1000kb 1 0.1 \
         --out work/{2}-${suffix}
+  if [ -f work/{2}-${suffix} ]; then
   (
     echo -e "rsid\tChromosome\tPosition\tAllele1\tAllele2\tFreq1\tEffect\tStdErr\tlogP\nN"
     grep -w -f work/{2}-${suffix}.prune.in work/{2}.mri-${suffix} | \
@@ -23,6 +24,7 @@ parallel -j1 -C' ' '
     cut -d" " -f1 --complement | \
     tr " " "\t"
   ) | gzip -f > work/{2}.mrx-${suffix}
+  fi
 '
 }
 
