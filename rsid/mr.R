1500c64ad17bdab920f54a9b5265ffd6c3f08e33
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-10-30 20:54:08 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/mr.R
+++ rsid/mr.R
@@ -1,28 +1,32 @@
-prot <- Sys.getenv("prot")
-suffix <- Sys.getenv("suffix")
-outcomes <- Sys.getenv("outcomes")
-
 library(TwoSampleMR)
-gz <- gzfile(paste0("work/",prot,".mrx",suffix))
+gz <- gzfile(paste0("work/mr/",prot,".mrx",suffix))
 d <- within(read.delim(gz,as.is=TRUE),{P <- 10^-logP})
 exposure_dat <- format_data(d, type="exposure", snp_col = "rsid", effect_allele_col = "Allele1", other_allele_col = "Allele2",
                             eaf_col = "Freq1", beta_col = "Effect", se_col = "StdErr", pval_col = "P", log_pval = FALSE,
                             samplesize_col = "N")
-outcome_dat <- extract_outcome_data(exposure_dat$SNP, outcomes, proxies = 1, rsq = 0.8, align_alleles = 1, palindromes = 1,
-                                    maf_threshold = 0.3)
-dat <- harmonise_data(exposure_dat, outcome_dat, action = 2)
-res_mr <- mr(dat)
-mr_heterogeneity(dat)
-mr_pleiotropy_test(dat)
-res_single <- mr_singlesnp(dat)
-res_loo <- mr_leaveoneout(dat)
-save(res_mr,res_single,res_loo,file=paste0("work/",prot,".mro",suffix),quote=FALSE,row.names=FALSE)
-pdf(paste0("work/",prot,suffix,".pdf"))
-mr_scatter_plot(res_mr, dat)
-mr_forest_plot(res_single)
-mr_leaveoneout_plot(res_loo)
-mr_funnel_plot(res_single)
-dev.off()
+library(openxlsx)
+url <- "https://jhz22.user.srcf.net/INF/latest/INF1.latest.xlsx"
+efo <- subset(openxlsx::read.xlsx(url, sheet="EFO", colNames=TRUE, skipEmptyRows=TRUE, cols=c(1:4), rows=c(1:78)),!is.na(MRBASEID))
+INF1_merge <- read.delim("work/INF1.merge",as.is=TRUE)
+for (suffix in c("cis","pan")) for(prot in with(INF1_merge,unique(prot))) for(outcomes in with(efo,MRBASEID))
+{
+  cat(suffix,"-",prot,"-",outcomes,"\n")
+  outcome_dat <- extract_outcome_data(exposure_dat$SNP, outcomes, proxies = 1, rsq = 0.8, align_alleles = 1, palindromes = 1,
+                                      maf_threshold = 0.3)
+  dat <- harmonise_data(exposure_dat, outcome_dat, action = 2)
+  res_mr <- mr(dat)
+  mr_heterogeneity(dat)
+  mr_pleiotropy_test(dat)
+  res_single <- mr_singlesnp(dat)
+  res_loo <- mr_leaveoneout(dat)
+  save(res_mr,res_single,res_loo,file=paste0("work/mr/",prot,"-",outcomes,".mro",suffix),quote=FALSE,row.names=FALSE)
+  pdf(paste0("work/mr/",prot,"-",outcomes,suffix,".pdf"))
+  mr_scatter_plot(res_mr, dat)
+  mr_forest_plot(res_single)
+  mr_leaveoneout_plot(res_loo)
+  mr_funnel_plot(res_single)
+  dev.off()
+}
 
 heightMR <- function()
 {
--- rsid/mr.sh
+++ rsid/mr.sh
@@ -2,33 +2,32 @@
 
 function MR_dat()
 {
-cut -f3,8,9,10 doc/olink.inf.panel.annot.tsv | grep -v BDNF | sed 's/"//g' | sort -k1,1 | join -12 work/inf1.tmp - | \
+cut -f3 work/INF1.METAL | sed '1d' | sort | uniq | grep -w -f - work/INF1.merge.genes | \
 parallel -j1 -C' ' '
   echo --- {2} ---
   gunzip -c METAL/{2}-1.tbl.gz | \
   cut -f1-6,10-12,18 | \
-  tr "\t" " " | \
   awk -vchr={3} -vstart={4} -vend={5} -vM=1e6 -vlogp=-5.45131 "
       {
         if(ENVIRON["suffix"]=="cis") {if(\$1==chr && \$2>=start-M && \$2 <= end+M && \$9<=logp) print} else if(\$9<=logp) print
-      }" > work/{2}.mri-${suffix}
-  cut -d" " -f3 work/{2}.mri-${suffix} > work/{2}.mrs-${suffix}
-  plink --bfile INTERVAL/cardio/INTERVAL --extract work/{2}.mrs-${suffix} \
-        --geno 0.1 --mind 0.1 --maf 0.005 --indep-pairwise 1000kb 1 0.1 --out work/{2}-${suffix}
+      }" > work/mr/{2}.mri-${suffix}
+  cut -f3 work/mr/{2}.mri-${suffix} > work/mr/{2}.mrs-${suffix}
+  plink --bfile INTERVAL/cardio/INTERVAL --extract work/mr/{2}.mrs-${suffix} \
+        --geno 0.1 --mind 0.1 --maf 0.005 --indep-pairwise 1000kb 1 0.1 --out work/mr/{2}-${suffix}
   (
     echo -e "rsid\tChromosome\tPosition\tAllele1\tAllele2\tFreq1\tEffect\tStdErr\tlogP\tN"
-    grep -w -f work/{2}-${suffix}.prune.in work/{2}.mri-${suffix} | \
+    grep -w -f work/mr/{2}-${suffix}.prune.in work/mr/{2}.mri-${suffix} | \
     awk "{\$3=\"chr\"\$1\":\"\$2;\$8=-\$8;print}" | \
     sort -k3,3 | \
     join -23 -12 work/snp_pos - | \
     cut -d" " -f1 --complement | \
     tr " " "\t"
-  ) | gzip -f > work/{2}.mrx-${suffix}
+  ) | gzip -f > work/mr/{2}.mrx-${suffix}
 '
 }
 
+cut -f3,8,9,10 doc/olink.inf.panel.annot.tsv | grep -v BDNF | sed 's/"//g' | sort -k1,1 | join -12 work/inf1.tmp - > work/INF1.merge.genes
+if [ ! -d work/mr ]; then mkdir work/mr; fi
 for type in cis pan; do export suffix=${type}; MR_dat; done
 
-export outcomes="ukb-b-20208"
-cut -f3,8,9,10 doc/olink.inf.panel.annot.tsv | grep -v BDNF | sed 's/"//g' | sort -k1,1 | join -12 work/inf1.tmp - | \
-parallel -j1 -C' ' 'export prot={2}; R --no-save <rsid/mr.R'
+R --no-save <rsid/mr.R
