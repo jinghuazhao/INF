14c6cefc0e70fb951c5237cf7bc244a1b7cde4ba
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-10-31 15:39:59 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/mr.sh
+++ rsid/mr.sh
@@ -7,10 +7,9 @@ parallel -j1 -C' ' '
   echo --- {2} ---
   gunzip -c METAL/{2}-1.tbl.gz | \
   cut -f1-6,10-12,18 | \
-  awk -vchr={3} -vstart={4} -vend={5} -vM=1e6 -vlogp=-5.45131 "
-      {
-        if(ENVIRON["suffix"]=="cis") {if(\$1==chr && \$2>=start-M && \$2 <= end+M && \$9<=logp) print} else if(\$9<=logp) print
-      }" > work/mr/{2}-${suffix}.mri
+  awk -vchr={3} -vstart={4} -vend={5} -vM=1e6 -vlogp=-5.45131 -vsuffix=${suffix} "
+        (suffix==\"cis\" && \$1==chr && \$2>=start-M && \$2 <= end+M && \$9<=logp) || (suffix==\"pan\" && \$9<=logp)
+      " > work/mr/{2}-${suffix}.mri
   cut -f3 work/mr/{2}-${suffix}.mri > work/mr/{2}-${suffix}.mrs
   plink --bfile INTERVAL/cardio/INTERVAL --extract work/mr/{2}-${suffix}.mrs \
         --geno 0.1 --mind 0.1 --maf 0.005 --indep-pairwise 1000kb 1 0.1 --out work/mr/{2}-${suffix}
@@ -26,7 +25,9 @@ parallel -j1 -C' ' '
 '
 }
 
-cut -f3,8,9,10 doc/olink.inf.panel.annot.tsv | grep -v BDNF | sed 's/"//g' | sort -k1,1 | join -12 work/inf1.tmp - > work/INF1.merge.genes
+if [ ! -f work/INF1.merge.genes ]; then
+   cut -f3,8,9,10 doc/olink.inf.panel.annot.tsv | grep -v BDNF | sed 's/"//g' | sort -k1,1 | join -12 work/inf1.tmp - > work/INF1.merge.genes
+fi
 if [ ! -d work/mr ]; then mkdir work/mr; fi
 for type in cis pan; do export suffix=${type}; MR_dat; done
 
