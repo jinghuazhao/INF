79b4ecc4a080935d3bb93af6d5a4bfb900c1557a
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-11-14 17:18:59 +0000

    merged

--- rsid/efo_inf.R
+++
@@ -1,3 +0,0 @@
-url <- "https://jhz22.user.srcf.net/INF1.latest.xlsx"
-efo <- subset(openxlsx::read.xlsx(url, sheet="EFO", colNames=TRUE, skipEmptyRows=TRUE, cols=c(1:4), rows=c(1:78)),!is.na(MRBASEID))
-
