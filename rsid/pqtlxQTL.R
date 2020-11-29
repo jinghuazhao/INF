128d379a11b0f851400237791dcdf1523c44776d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-11-29 12:15:53 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/pqtlxQTL.R
+++ rsid/pqtlxQTL.R
@@ -16,8 +16,6 @@ for (catalogue in c("eQTL","mQTL","pQTL"))
 }
  
 INF <- Sys.getenv("INF")
-f <- paste0(file.path(INF,"work","INF1.merge."),"eQTL")
-load(f)
 INF1_aggr <- within(left_join(read.delim(file.path(INF,"work","INF1.METAL"),as.is=TRUE),inf1),{
                       hg19_coordinates <- paste0("chr",Chromosome,":",Position)
                       gene_snpid <- paste0(gene,"-",MarkerName)
@@ -30,6 +28,8 @@ r2 <- 0.8
 build <- 37
 # query()
 
+f <- paste0(file.path(INF,"work","INF1.merge."),"eQTL")
+load(f)
 eQTL <- within(subset(ps,hgnc%in%INF1_aggr$gene), {gene_snpid <- paste0(hgnc,"-",snpid)}) %>%
               select(hgnc,ensembl,rsid,hg19_coordinates,a1,a2,eur,consequence,
               study,pmid,ancestry,year,tissue,exp_gene,exp_ensembl,proxy,r2,beta,se,p,dataset,gene_snpid)
@@ -53,8 +53,8 @@ png("pQTL_eQTL.png",res=300,width=16,height=12,units="in")
 setHook("grid.newpage", function() pushViewport(viewport(x=1,y=1,width=0.9, height=0.9, name="vp", just=c("right","top"))), action="prepend")
 pheatmap(tbl, legend=FALSE, angle_col="45", color=col, width=8, height=40, cluster_rows=FALSE, cluster_cols=FALSE, fontsize=12)
 setHook("grid.newpage", NULL, "replace")
-grid.text("pQTL-protein", y=-0.07, gp=gpar(fontsize=15))
-grid.text("Tissue", x=-0.07, rot=90, gp=gpar(fontsize=15))
+grid.text("Tissue", y=-0.07, gp=gpar(fontsize=15))
+grid.text("pQTL", x=-0.07, rot=90, gp=gpar(fontsize=15))
 dev.off()
 library(gap)
 aux <- with(with(eQTL_overlap, cbind(inv_chr_pos_a1_a2(MarkerName)[c("chr","pos")],rsid,Allele1,Allele2,prot,HLA,cis.trans,tissue)), {
@@ -108,24 +108,22 @@ highchart() %>%
                 hcaes(x = f1,y = f2,value = v),
                 dataLabels = list(enabled = FALSE))
 
-ips <- subset(merge(INF1_aggr,ps,by="hg19_coordinates",all.y=TRUE),
-              select=-c(snpid,
-                        hg19_coordinates,hg38_coordinates,ref_hg19_coordinates,ref_hg38_coordinates,
+f <- paste0(file.path(INF,"work","INF1.merge."),"pQTL")
+load(f)
+ips <- subset(merge(INF1_aggr,within(subset(ps,hgnc%in%INF1_aggr$gene),{gene_snpid <- paste0(hgnc,"-",snpid)}),
+                    by="gene_snpid",all.y=TRUE),select=-c(snpid,
+                        hg38_coordinates,ref_hg19_coordinates,ref_hg38_coordinates,
                         ref_pos_hg19, ref_pos_hg38, ref_protein_position, ref_amino_acids, ref_ensembl,
                         rsid, pos_hg19, pos_hg38, protein_position, amino_acids, ensembl,
                         dprime, efo, n, n_studies, unit, direction))
+write.table(ips,file=file.path(INF,"work","pQTL.tsv"),row.names=FALSE,quote=FALSE,sep="\t")
+
+# INTERVAL data under SomaLogic
 SL <- SomaLogic160410 %>% select(SOMAMER_ID,UniProt,Target,TargetFullName,chr,entGene) %>% rename(trait=TargetFullName)
 pQTL <- dplyr::left_join(ips,SL)
-write.table(pQTL,file=paste0(f,"-SomaLogic.tsv"),row.names=FALSE,quote=FALSE,sep="\t")
-write.table(subset(pQTL,UniProt%in%uniprot),file=paste0(f,"-ps.tsv"),row.names=FALSE,quote=FALSE,sep="\t")
-
-pQTL <- dplyr::left_join(subset(ips,pmid!=29875488),SL)
-subset(pQTL[c("INF1_rsid","uniprot","UniProt","trait","ref_rsid","proxy","ref_a1","ref_a2","Allele1","Allele2","snp")],UniProt%in%uniprot)
-# long and comprehensive list with less variables to fit screen
-pQTL <- dplyr::left_join(subset(ips,pmid==29875488),SL)
-subset(pQTL[c("INF1_rsid","uniprot","UniProt","trait","ref_rsid","proxy","ref_a1","ref_a2")],UniProt%in%uniprot)
-gap::pvalue(-1.102/0.016)
-gap::pvalue(-1.114/0.0157)
+INTERVAL <- subset(pQTL,pmid==29875488)
+write.table(ips,file=file.path(INF,"work","pQTL-SomaLogic.tsv"),row.names=FALSE,quote=FALSE,sep="\t")
+# various checks
 rs12075 <-c("P51671","P80162","P13500","P80075","P80098","Q99616")
 subset(SomaLogic160410,UniProt%in%rs12075)
 subset(SomaLogic160410,TargetFullName%in%c("Eotaxin","Corneodesmosin","C-C motif chemokine 14","C-C motif chemokine 26"))
@@ -133,6 +131,3 @@ subset(SomaLogic160410,TargetFullName=="SLAM family member 7")
 subset(SomaLogic160410,UniProt=="P50591")
 subset(SomaLogic160410,UniProt=="O14625")
 subset(SomaLogic160410,UniProt=="P15692")
-
-#genes <- scan("work/INF1.gene",what="")
-#r <- genequeries(genes,catalogue="eQTL",build=37,p=5e-8,proxies="EUR",r2=0.8)
