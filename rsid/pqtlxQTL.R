5126d283c2d451638d246fe186093f41f62ca7a2
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-12-05 22:44:38 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/pqtlxQTL.R
+++ rsid/pqtlxQTL.R
@@ -1,15 +1,5 @@
-options(width=500)
-
-library(dplyr)
-library(pQTLtools)
-
-proxies <- "EUR"
-p <- 5e-8
-r2 <- 0.8
-build <- 37
-
 query <- function(rsid=INF1_aggr[["INF1_rsid"]])
-for (catalogue in c("eQTL","mQTL","pQTL"))
+for (catalogue in c("eQTL","pQTL"))
 {
   r <- snpqueries(rsid, catalogue=catalogue, proxies=proxies, p=p, r2=r2, build=build)
   lapply(r,dim)
@@ -17,9 +7,13 @@ for (catalogue in c("eQTL","mQTL","pQTL"))
   f <- paste0(file.path(INF,"work","INF1.merge."),catalogue)
   save(INF1_aggr,r,ps,file=f)
   ips <- subset(merge(INF1_aggr,ps,by="hg19_coordinates"),select=-c(hg19_coordinates,Chromosome,Position))
-  write.table(ips,file=file.path(INF,"work",paste0(f,".tsv")),row.names=FALSE,quote=FALSE,sep="\t")
+  write.table(ips,file=paste0(f,".tsv"),row.names=FALSE,quote=FALSE,sep="\t")
 }
  
+options(width=500)
+library(dplyr)
+library(pQTLtools)
+proxies <- "EUR"; p <- 5e-8; r2 <- 0.8; build <- 37
 INF <- Sys.getenv("INF")
 metal <- read.delim(file.path(INF,"work","INF1.METAL"),as.is=TRUE)
 INF1 <- within(left_join(subset(metal,cis.trans=="trans"),inf1),{
@@ -28,8 +22,6 @@ INF1 <- within(left_join(subset(metal,cis.trans=="trans"),inf1),{
                }) %>% rename(INF1_rsid=rsid) %>% rename(Total=N) %>% rename(gene_gwas=gene) %>% rename(uniprot_gwas=uniprot)
 r <- snpqueries(INF1[["INF1_rsid"]], catalogue="None", proxies="EUR", p=p, r2=r2, build=build)
 snps <- with(r,snps)[c("snpid","hgnc")] %>% rename(gene=hgnc)
-# write.table(with(r,snps),file="INF1.pQTL-trans.tsv",quote=FALSE,row.names=FALSE)
-# Somehow there is discrepancy with PhenoScanner so abandon it
 # load(file.path(INF,"work","INF1.merge.trans.anno.rda"))
 INF1_aggr <- within(merge(INF1,snps,by.x="MarkerName",by.y="snpid"), {gene_snpid <- paste0(gene,"-",MarkerName)})
 # r <- snpqueries(snplist=with(trans,rsid),catalogue="None")
@@ -37,7 +29,7 @@ INF1_aggr <- within(merge(INF1,snps,by.x="MarkerName",by.y="snpid"), {gene_snpid
 query()
 f <- file.path(INF,"work","INF1.merge.eQTL")
 load(f)
-eQTL <- within(subset(ps,hgnc%in%INF1_aggr$gene), {gene_snpid <- paste0(hgnc,"-",snpid)}) %>%
+eQTL <- within(subset(ps,hgnc==exp_gene), {gene_snpid <- paste0(hgnc,"-",snpid)}) %>%
               select(hgnc,ensembl,rsid,hg19_coordinates,a1,a2,eur,consequence,
               study,pmid,ancestry,year,tissue,exp_gene,exp_ensembl,proxy,r2,beta,se,p,dataset,gene_snpid)
 eQTL <- within(subset(eQTL,tissue!="Normal prepouch ileum"), {
@@ -45,20 +37,23 @@ eQTL <- within(subset(eQTL,tissue!="Normal prepouch ileum"), {
   tissue <- gsub("Visceral abdominal fat","Adipose visceral omentum",tissue)
   tissue <- gsub("ba9","BA9",tissue)
   tissue <- gsub("ba24","BA24",tissue)
-  tissue <- gsub("^Blood|Monocytes|Peripheral blood|Neutrophils|Peripheral blood monocytes|T cells|Whole Blood|Lymphoblastoid cell lines","Whole blood",tissue)
+  tissue <- gsub("^Blood|Monocytes|Peripheral blood|Neutrophils|Peripheral blood monocytes","Whole blood",tissue)
+  tissue <- gsub("T cells|Whole Blood|Lymphoblastoid cell lines","Whole blood",tissue)
   tissue <- gsub("Breast tumors", "Breast mammary tissue", tissue)
   tissue <- gsub("Skin not sun exposed suprapubic|Skin sun exposed lower leg","Skin",tissue)
 })
 save(eQTL,file=file.path(INF,"work","pQTL.eQTL.rda"))
 eQTL_overlap <- subset(merge(INF1_aggr,eQTL,by="gene_snpid"),select=-c(Chromosome,Position))
 write.table(eQTL_overlap,file=file.path(INF,"work","pQTL_eQTL.tsv"),quote=FALSE,row.names=FALSE,sep="\t")
-tbl.cis <- with(within(subset(eQTL_overlap,cis.trans=="cis"),{rsidProts <- paste0(INF1_rsid," (",gene,")")}),table(rsidProts,tissue))
+tbl.cis <- with(within(subset(eQTL_overlap,cis.trans=="cis"),{rsidProts <- paste0(INF1_rsid," (",gene_gwas,")")}),
+                table(rsidProts,tissue))
 tbl.cis[tbl.cis>1] <- 1
 sum(tbl.cis)
-tbl.trans <- with(within(subset(eQTL_overlap,cis.trans=="trans"),{rsidProts <- paste0(INF1_rsid," (",gene,")")}),table(rsidProts,tissue))
+tbl.trans <- with(within(subset(eQTL_overlap,cis.trans=="trans"),{rsidProts <- paste0(INF1_rsid," (",gene_gwas,")")}),
+                  table(rsidProts,tissue))
 tbl.trans[tbl.trans>1] <- 1
 sum(tbl.trans)
-tbl <- with(within(eQTL_overlap,{rsidProts <- paste0(INF1_rsid," (",gene,")")}),table(rsidProts,tissue))
+tbl <- with(within(eQTL_overlap,{rsidProts <- paste0(INF1_rsid," (",gene_gwas,")")}),table(rsidProts,tissue))
 tbl[tbl>1] <- 1
 write.table(as.data.frame.matrix(tbl),file=file.path(INF,"work","pQTL_eQTL_matrix.tsv"),quote=FALSE,sep="\t")
 library(pheatmap)
