b02f6061e7baea3e1a65456b06736a9bc03ba076
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-12-06 20:43:41 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/pQTL.R
+++ rsid/pQTL.R
@@ -14,21 +14,19 @@ INF1_aggr <- within(merge(INF1,with(r,snps)[c("snpid","hgnc")],by.x="MarkerName"
              }) %>% rename(gene=hgnc)
 r <- snpqueries(INF1_aggr[["INF1_rsid"]], catalogue=catalogue, proxies=proxies, p=p, r2=r2, build=build)
 lapply(r,dim)
-ps <- subset(with(r,right_join(snps,results)),
-             select=-c(ref_hg19_coordinates,ref_hg38_coordinates, ref_pos_hg19, ref_pos_hg38, ref_protein_position,
-                       ref_amino_acids, ref_ensembl))
+ps <- subset(with(r,right_join(snps,results)),select=-c(ref_rsid, ref_hg19_coordinates, ref_hg38_coordinates,
+                         ref_chr, ref_pos_hg19, ref_pos_hg38, ref_a1, ref_a2, ref_eur, ref_consequence,
+                         ref_protein_position, ref_amino_acids, ref_ensembl, pos_hg19, pos_hg38, hg38_coordinates,
+                         protein_position, amino_acids, ensembl, dprime, efo, n, n_studies, unit, direction))
 f <- file.path(INF,"work","pQTL",paste0("INF1.merge.",catalogue))
 save(INF1_aggr,r,ps,file=f)
 ips <- subset(merge(INF1_aggr,ps,by="hg19_coordinates"),select=-c(hg19_coordinates,Chromosome,Position))
 write.table(ips,file=paste0(f,".tsv"),row.names=FALSE,quote=FALSE,sep="\t")
-ips <- subset(merge(within(INF1_aggr,{prot_snpid <- paste0(prot,"-",MarkerName)}),
-                    within(subset(ps,hgnc%in%INF1_aggr$gene),{gene_snpid <- paste0(hgnc,"-",snpid)}),
-                    by="gene_snpid",all.y=TRUE),select=-c(hg38_coordinates,
-                        rsid, pos_hg19, pos_hg38, protein_position, amino_acids, ensembl,
-                        dprime, efo, n, n_studies, unit, direction))
-write.table(ips[c("prot","uniprot_gwas","INF1_rsid","prot_snpid","cis.trans","proxy","r2","study","pmid","target.short","trait")],
+ips <- merge(within(INF1_aggr,{prot_snpid <- paste0(prot,"-",MarkerName)}),
+             within(subset(ps,hgnc%in%INF1_aggr$gene),{gene_snpid <- paste0(hgnc,"-",snpid)}), by="gene_snpid",all.y=TRUE)
+write.table(ips[c("uniprot_gwas","INF1_rsid","prot_snpid","cis.trans","proxy","r2","p","study","pmid","target.short","trait")],
       file="pQTL.log",quote=FALSE,row.names=FALSE,sep="\t")
-simple <- ips%>%select(INF1_rsid,prot,uniprot_gwas,target.short,prot_snpid,chr.x,chr.y,HLA,cis.trans,hgnc,proxy,r2,study,pmid,trait)
+simple <- ips%>%select(INF1_rsid,prot,uniprot_gwas,target.short,prot_snpid,chr.x,chr.y,HLA,cis.trans,hgnc,proxy,r2,p,study,pmid,trait)
 write.table(simple,file=file.path(INF,"work","pQTL","pQTL.tsv"),col.names=TRUE,row.names=FALSE,quote=FALSE,sep="\t")
 repl <- with(SomaLogic160410,is.na(extGene))
 SomaLogic160410[repl,"extGene"] <- SomaLogic160410[repl,"entGene"]
@@ -36,5 +34,5 @@ SL <- SomaLogic160410 %>% select(SOMAMER_ID,UniProt,Target,TargetFullName,chr,ex
 pQTL <- dplyr::left_join(simple,SL)
 INTERVAL <- subset(pQTL,pmid==29875488) %>%
             select(prot_snpid,chr.x,chr.y,chr,INF1_rsid,prot,uniprot_gwas,HLA,cis.trans,
-                   proxy,r2,study,pmid,chr,hgnc,trait,target.short,Target,TargetFullName)
+                   proxy,r2,p,study,pmid,chr,hgnc,trait,target.short,Target,TargetFullName)
 write.table(INTERVAL,file=file.path(INF,"work","pQTL","pQTL-SomaLogic.tsv"),col.names=TRUE,row.names=FALSE,quote=FALSE,sep="\t")
