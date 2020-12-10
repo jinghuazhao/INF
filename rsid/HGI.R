442ffe97cc876d84191c8179e60ca7417b0b7f4b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-12-10 21:46:32 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/HGI.R
+++ rsid/HGI.R
@@ -4,6 +4,65 @@ library(pQTLtools)
 library(TwoSampleMR)
 INF <- Sys.getenv("INF")
 
+MR <- function(clumping=FALSE)
+{
+  f <- file.path(INF,"work","HGI","INF.ins")
+  ivs <- read.delim(f,sep=" ")
+  for(row in 1:nrow(ivs))
+  {
+    prot <- ivs[row,"Phenotype"]
+    rsid <- ivs[row,"SNP"]
+    cat(row, prot,"-",rsid,"\n")
+    d <- read.delim(file.path(INF,"work","HGI",paste0(prot,"-",rsid,".mrx")))
+    d <- within(d,{P <- 10^logP})
+    e <- format_data(d, type="exposure", phenotype_col="prot", header = TRUE, snp_col = "rsid",
+                     effect_allele_col = "Allele1", other_allele_col = "Allele2",
+                     eaf_col = "Freq1", beta_col = "Effect", se_col = "StdErr", pval_col = "P", log_pval = FALSE,
+                     samplesize_col = "N")
+    d <- read.delim(file.path(INF,"work","HGI",paste0(prot,"-",rsid,"_1e-5")))
+    if (nrow(d)<=1) next
+    o <- format_data(d, type="outcome", header = TRUE, snp_col = "rsid",
+                     effect_allele_col = "ALT", other_allele_col = "REF",
+                     eaf_col = "all_meta_AF", 
+                     beta_col = "all_inv_var_meta_beta",
+                     se_col = "all_inv_var_meta_sebeta",
+                     pval_col = "all_inv_var_meta_p", log_pval = FALSE,
+                     samplesize_col = "all_meta_sample_N")
+    if (clumping)
+    {
+      outcome_dat <- clump_data(o)
+      if (nrow(outcome_dat)==0) next
+      d <- merge(e,outcome_dat,by="SNP")
+      if (nrow(d)<=1) next
+      e <- subset(e,SNP%in%with(d,SNP))
+      o <- subset(outcome_dat,SNP%in%with(d,SNP))
+      if (nrow(e)==0 | nrow(outcome_dat)==0) next
+      dat <- harmonise_data(e, outcome_dat, action = 1)
+    } else {
+      d <- merge(e,o,by="SNP")
+      if (nrow(d)<=1) next
+      e <- subset(e,SNP%in%with(d,SNP))
+      o <- subset(o,SNP%in%with(d,SNP))
+      if (nrow(e)==0 | nrow(o)==0) next
+      dat <- harmonise_data(e, o, action = 1)
+    }
+    cat(nrow(dat), "\n")
+    directionality <- directionality_test(dat)
+    result <- mr(dat)
+    heterogeneity <- mr_heterogeneity(dat)
+    pleiotropy <- mr_pleiotropy_test(dat)
+    single <- mr_singlesnp(dat)
+    loo <- mr_leaveoneout(dat)
+    prefix <- paste0("MR-",prot,"-",rsid)
+    invisible(lapply(c("directionality","result","heterogeneity","pleiotropy","single","loo"), function(x) {
+                    v <- lapply(x, function(x) tryCatch(get(x), error=function(e) NULL))[[1]]
+                    if (!is.null(v)) write.table(format(v,digits=3),file=file.path(INF,"work","HGI",paste0(prefix,"-",x,".txt")),
+                                                 quote=FALSE,row.names=FALSE,sep="\t")
+               }))
+  }
+}
+MR()
+
 pqtlMR <- function()
 {
   f <- file.path(INF,"work","HGI","INF.ins")
@@ -12,11 +71,12 @@ pqtlMR <- function()
   {
     prot <- ivs[row,"Phenotype"]
     rsid <- ivs[row,"SNP"]
+    cat(row, prot,"-",rsid,"\n")
     e <- format_data(ivs[row,], type="exposure", header = TRUE, snp_col = "SNP",
                      effect_allele_col = "Allele1", other_allele_col = "Allele2",
                      eaf_col = "EAF", beta_col = "Effect", se_col = "StdErr", pval_col = "P", log_pval = FALSE,
                      samplesize_col = "N")
-    d <- read.delim(file.path(INF,paste0(prot,"-",rsid)))
+    d <- read.delim(file.path(INF,"work","HGI",paste0(prot,"-",rsid)))
     o <- format_data(d, type="outcome", header = TRUE, snp_col = "rsid",
                      effect_allele_col = "ALT", other_allele_col = "REF",
                      eaf_col = "all_meta_AF", 
@@ -41,50 +101,4 @@ pqtlMR <- function()
                }))
   }
 }
-
-MR <- function()
-{
-  f <- file.path(INF,"work","HGI","INF.ins")
-  ivs <- read.delim(f,sep=" ")
-  for(row in 1:nrow(ivs))
-  {
-    prot <- ivs[row,"Phenotype"]
-    rsid <- ivs[row,"SNP"]
-    d <- read.delim(file.path(INF,"work","HGI",paste0(prot,"-",rsid,".mrx")))
-    d <- within(d,{P <- 10^logP})
-    e <- format_data(d, type="exposure", phenotype_col="prot", header = TRUE, snp_col = "rsid",
-                     effect_allele_col = "Allele1", other_allele_col = "Allele2",
-                     eaf_col = "Freq1", beta_col = "Effect", se_col = "StdErr", pval_col = "P", log_pval = FALSE,
-                     samplesize_col = "N")
-    d <- read.delim(file.path(INF,"work","HGI",paste0(prot,"-",rsid,"_1e-5")))
-    if (nrow(d)==0) next
-    o <- format_data(d, type="outcome", header = TRUE, snp_col = "rsid",
-                     effect_allele_col = "ALT", other_allele_col = "REF",
-                     eaf_col = "all_meta_AF", 
-                     beta_col = "all_inv_var_meta_beta",
-                     se_col = "all_inv_var_meta_sebeta",
-                     pval_col = "all_inv_var_meta_p", log_pval = FALSE,
-                     samplesize_col = "all_meta_sample_N")
-    outcome_dat <- clump_data(o)
-    d <- merge(e,o,by="SNP")
-    e <- subset(e,SNP%in%with(d,SNP))
-    o <- subset(o,SNP%in%with(d,SNP))
-    if (nrow(e)==0 | nrow(o)==0) next
-    dat <- harmonise_data(e, o, action = 1)
-    directionality <- directionality_test(dat)
-    result <- mr(dat)
-    heterogeneity <- mr_heterogeneity(dat)
-    pleiotropy <- mr_pleiotropy_test(dat)
-    single <- mr_singlesnp(dat)
-    loo <- mr_leaveoneout(dat)
-    prefix <- paste0("MR-",prot,"-",rsid)
-    invisible(lapply(c("directionality","result","heterogeneity","pleiotropy","single","loo"), function(x) {
-                    v <- lapply(x, function(x) tryCatch(get(x), error=function(e) NULL))[[1]]
-                    if (!is.null(v)) write.table(format(v,digits=3),file=file.path(INF,"work","HGI",paste0(prefix,"-",x,".txt")),
-                                                 quote=FALSE,row.names=FALSE,sep="\t")
-               }))
-  }
-}
-
-pqtlMR()
-MR()
+# pqtlMR()
--- rsid/HGI.sh
+++ rsid/HGI.sh
@@ -26,20 +26,19 @@ function _exposure()
     awk '{print $1,$2,toupper($3),toupper($4),$5,$6,$7,10^$8,$9}'
   ) > work/HGI/INF.ins
   grep -e chr3 -e chr9 work/INF1.merge-rsid | \
-  cut -f5,6,8,9 --output-delimiter=' ' | parallel -j5 -C' ' '
-  gunzip -c ${INF}/METAL/{1}-1.tbl.gz | \
-  cut -f1-6,10-12,18 | \
-  awk -vchr={3} -vpos={4} -vM=1e6 -vlogp=-5.45131 "
-        (\$1==chr && \$2>=pos-M && \$2 <= pos+M && \$9<=logp) || \$9<=logp
-      " > work/HGI/{1}-{2}.mri
-  (
-    echo -e "prot\trsid\tChromosome\tPosition\tAllele1\tAllele2\tFreq1\tEffect\tStdErr\tlogP\tN"
-    awk "{\$4=toupper(\$4);\$5=toupper(\$5);print}" work/HGI/{1}-{2}.mri | \
-    sort -k3,3 | \
-    join -23 ${INF}/work/INTERVAL.rsid - | \
-    awk -v prot={1} "{\$1=prot;print}" | \
-    tr " " "\t"
-  ) | gzip -f > work/HGI/{1}-{2}.mrx
+  cut -f5,6,8,9 --output-delimiter=' ' | \
+  parallel -j5 -C' ' '
+    gunzip -c ${INF}/METAL/{1}-1.tbl.gz | \
+    cut -f1-6,10-12,18 | \
+    awk -vchr={3} -vpos={4} -vM=1e6 -vlogp=-5.45131 "\$1==chr && \$2>=pos-M && \$2 <= pos+M && \$9<=logp" > work/HGI/{1}-{2}.mri
+    (
+      echo -e "prot\trsid\tChromosome\tPosition\tAllele1\tAllele2\tFreq1\tEffect\tStdErr\tlogP\tN"
+      awk "{\$4=toupper(\$4);\$5=toupper(\$5);print}" work/HGI/{1}-{2}.mri | \
+      sort -k3,3 | \
+      join -23 ${INF}/work/INTERVAL.rsid - | \
+      awk -v prot={1} "{\$1=prot;print}" | \
+      tr " " "\t"
+    ) | gzip -f > work/HGI/{1}-{2}.mrx
   '
 }
 _exposure
