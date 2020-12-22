668ae6a44498f6729150a67236a98d69185dda2b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-12-22 21:41:43 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/gwasvcf.sh
+++ rsid/gwasvcf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 export TMPDIR=${HPC_WORK}/work
+module load gcc/6
 for prot in $(cut -f1 work/inf1.tmp | grep -v BDNF | sort)
 do
   echo ${prot}
@@ -38,3 +39,36 @@ R --no-save <<END
   }
 END
 )
+
+function python()
+{
+  R --no-save <<\ \ END
+    library(jsonlite)
+    j <- list(chr_col = 0, pos_col = 1, snp_col = 2, ea_col = 3, oa_col = 4, eaf_col = 5,
+              beta_col = 6, se_col = 7, pval_col = 8, ncontrol_col = 9, delimiter = "\t",
+              header = TRUE, build = "GRCh37")
+    INF <- Sys.getenv("INF")
+    write(toJSON(j, auto_unbox=T), file = file.path(INF,"rsid","gwasvcf.json"))
+  END
+  module load jdk-8u141-b15-gcc-5.4.0-p4aaopt
+  module load gatk
+  module load python/3.7
+  cd ${HPC_WORK}/gwas2vcf
+  source env/bin/activate
+  for prot in $(sed '1d' ${INF}/work/INF1.merge | cut -f5 | grep -v BDNF | sort | uniq)
+  do
+    echo ${prot}
+    export prot=${prot}
+    (
+    gunzip -c ${INF}/METAL/${prot}-1.tbl | \
+    awk -vOFS="\t" 'BEGIN{print "chr","pos","snpid","a1","a2","af","b","se","logp","n"}'
+    awk -vOFS="\t" 'NR>1{print $1,$2,$3,toupper($4),toupper($5),$6, $10, $11, (-$12), $18}' | \
+    grep -v "<" | \
+    sort -k1,1n -k2,2n
+  ) | \
+  gzip -f > ${INF}/METAL/gwasvcf/${prot}.txt.gz
+  python main.py --out ${INF}/METAL/gwasvcf/${prot}.vcf.gz --data ${INF}/METAL/gwasvcf/${prot}.txt.gz
+                 --ref human_g1k_v37.fasta --id "${prot}" --json ${INF}/rsid/gwasvcf.json
+  done
+  cd -
+}
