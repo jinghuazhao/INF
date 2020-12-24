2685a21bc7cc70f70de6fd4928210ddbfac49d17
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-12-24 22:23:26 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/GTEx.sh
+++ rsid/GTEx.sh
@@ -21,69 +21,72 @@ R --no-save <<END
   class(cis38)
   cis_dat38 <- data.frame(cis38)[c("seqnames","start","end")]
   names(cis_dat38) <- c("chr38","start38","end38")
-  write.table(data.frame(cis_dat,cis_dat38),file="cis.dat",quote=FALSE,row.names=FALSE,sep="\t")
-  test <- function()
-  {
-    library(gwascat)
-    cur = makeCurrentGwascat()  # result varies by day
-    data(cur)
-    cur
-    library(rtracklayer)
-    path = system.file(package="liftOver", "extdata", "hg38ToHg19.over.chain")
-    ch = import.chain(path)
-    ch
-    seqlevelsStyle(cur) = "UCSC"  # necessary
-    cur19 = liftOver(cur, ch)
-    class(cur19)
-  }
+  write.table(data.frame(cis_dat,cis_dat38),file="cis-pQTL.dat",quote=FALSE,row.names=FALSE,sep="\t")
 END
 }
 
 function eQTL()
 {
-export GTEx_v8=~/rds/public_databases/GTEx/GTEx_Analysis_v8_eQTL_cis_associations
-export ext=.v8.EUR.signif_pairs.txt.gz
-export col_gene=1
-export col_variant=2
 export GTEx_v8=${HPC_WORK}/GTEx_Analysis_v8_eQTL
 export ext=.v8.signif_variant_gene_pairs.txt.gz
 export col_gene=2
 export col_variant=1
+export GTEx_v8=~/rds/public_databases/GTEx/GTEx_Analysis_v8_eQTL_cis_associations
+export ext=.v8.EUR.signif_pairs.txt.gz
+export col_gene=1
+export col_variant=2
 export M=1e6
 export nlines=60
 (
   parallel -C' ' --env GTEx_v8 --env ext --env col --env M '
-    read SNP hgnc ensGene pos chrpos < <(awk -v row={1} "NR==row{print \$4,\$7,\$8,\$13,\$11\"_\"\$13}" cis.dat)
+    read SNP hgnc ensGene pos chrpos < <(awk -v row={1} "NR==row{print \$4,\$7,\$8,\$13,\$11\"_\"\$13}" cis-pQTL.dat)
     zgrep ${ensGene} ${GTEx_v8}/{2}${ext} | \
-    awk -v col_gene=${col_gene} -v col_variant=${col_variant} -v M=${M} -v bp=${pos} -v OFS="\t" "{
+    awk -v col_gene=${col_gene} -v col_variant=${col_variant} -v ensGene=${ensGene} -v M=${M} -v bp=${pos} -v OFS="\t" "{
        split(\$col_variant,a,\"_\");
        chr=a[1];pos=a[2];a1=a[3];a2=a[4];
        if(a[3]<a[4]) {a1=a[3];a2=a[4]} else {a1=a[4];a2=a[3]}
        \$col_variant=chr\":\"pos\"_\"a1\"_\"a2;
-       if(bp>=a[2]-M && bp<a[2]+M && length(a1)==1 && length(a2)==1) print \$col_gene,\$col_variant,\$11
+       snpid=chr\":\"bp\"_\"a1\"_\"a2
+       if(index(\$col_gene,ensGene) && bp>=a[2]-M && bp<a[2]+M && length(a1)==1 && length(a2)==1) print \$col_gene,\$col_variant,\$7,chr,pos,a1,a2,snpid
      }" | \
     sort -k3,3g | \
     awk -vSNP=${SNP} -vensGene=${ensGene} -vtissue={2} -vOFS="\t" "NR==1 {print SNP,ensGene,tissue,\$0}"
   ' ::: $(seq 2 ${nlines}) ::: $(ls ${GTEx_v8} | grep -v egenes | xargs -l basename -s ${ext})
 ) | \
 sort -k1,1 -k2,2 > eQTL_GTEx.dat
+awk '$5==$11' eQTL_GTEx.dat | cut -f1 | uniq
+awk '$5==$11' eQTL_GTEx.dat
 }
 
-for SNP in $(cut -f1 eQTL_GTEx.dat | uniq)
+R --no-save <<END
+  options(width=200)
+  library(pQTLtools)
+  eQTL_GTEx <- read.table("eQTL_GTEx.dat",col.names=c("MarkerName","ensGene","tissue","gene","sentinel","p","chr","pos","a1","a2","eQTL"))
+  cis_dat <- within(eQTL_GTEx,{seqnames <- chr; start <- pos-1; end <- pos})
+  ord_cis <- with(cis_dat,order(chr,pos))
+  cis_dat <- cis_dat[ord_cis,c("seqnames","start","end","MarkerName","ensGene","tissue","gene","sentinel","eQTL","a1","a2")]
+  cis_gr <- with(cis_dat,GenomicRanges::GRanges(seqnames=seqnames,IRanges::IRanges(start,end)))
+  hpc_work <- Sys.getenv("HPC_WORK")
+  path = file.path(hpc_work, "bin", "hg38ToHg19.over.chain")
+  library(rtracklayer)
+  chain <- import.chain(path)
+  chain
+  seqlevelsStyle(cis_gr) <- "UCSC"
+  cis37 <- liftOver(cis_gr, chain)
+  class(cis37)
+  cis_dat37 <- data.frame(cis37)[c("seqnames","start","end")]
+  names(cis_dat37) <- c("chr37","start37","end37")
+  cisQTL <- within(cbind(cis_dat,cis_dat37),{snpid <- paste0("chr",chr37,":",end37,"_",a1,"_",a2)})
+  write.table(cisQTL,file="cis-eQTL.dat",quote=FALSE,row.names=FALSE,sep="\t")
+END
+for SNP in $(cut -f1 cis-eQTL.dat | uniq)
 do
   export chr=$(awk -vSNP=${SNP} 'BEGIN{gsub(/chr/,"",SNP);split(SNP,a,":");print a[1]}')
   echo ${SNP} - ${chr}
   (
     echo ${SNP}
-    awk -vSNP=${SNP} '$1==SNP' eQTL_GTEx.dat | cut -f5 | uniq
+    awk -vSNP=${SNP} '$1==SNP' eQTL_GTEx.dat | cut -f15 | uniq
   ) > ${SNP}.snps
   plink --bfile INTERVAL/cardio/INTERVAL --chr ${chr} --extract ${SNP}.snps --make-bed --out ${SNP}
   plink --bfile ${SNP} --no-sex --no-pheno --r2 inter-chr --out ${SNP}
-done
-
-for chr in {1..22}
-do
-  echo ${chr}
-  export chr1KG=~/rds/public_databases/1000G/ALL.chr${chr}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
-  qctool -g $chr1KG -og 1KG-${chr} -ofiletype binary_ped
 done
