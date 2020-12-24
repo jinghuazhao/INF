5b279872cb9e77a1e62a004aefe1a1f559b17bbb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-12-24 16:53:26 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/gwasvcf.sh
+++ rsid/gwasvcf.sh
@@ -50,26 +50,5 @@ function python()
     INF <- Sys.getenv("INF")
     write(toJSON(j, auto_unbox=T), file = file.path(INF,"rsid","gwasvcf.json"))
   END
-  module load jdk-8u141-b15-gcc-5.4.0-p4aaopt
-  module load gatk
-  module load python/3.7
-  cd ${HPC_WORK}/gwas2vcf
-  source env/bin/activate
-  for prot in $(sed '1d' ${INF}/work/INF1.merge | cut -f5 | grep -v BDNF | sort | uniq)
-  do
-    echo ${prot}
-    export prot=${prot}
-    (
-      awk -vOFS="\t" 'BEGIN{print "chr","pos","snpid","a1","a2","af","b","se","logp","n"}'
-      gunzip -c ${INF}/METAL/${prot}-1.tbl | \
-      awk -vOFS="\t" 'NR>1{print $1,$2,$3,toupper($4),toupper($5),$6, $10, $11, (-$12), $18}' | \
-      grep -v "<" | \
-      sort -k1,1n -k2,2n
-    ) | \
-    gzip -f > ${INF}/METAL/gwasvcf/${prot}.txt.gz
-    python main.py --out ${INF}/METAL/gwasvcf/${prot}.vcf.gz --data ${INF}/METAL/gwasvcf/${prot}.txt.gz \
-                   --ref human_g1k_v37.fasta --id "${prot}" --json ${INF}/rsid/gwasvcf.json
-    tabix -f ${INF}/METAL/gwasvcf/${prot}.vcf.gz
-  done
-  cd -
+  sbatch ${INF}/rsid/gwasvcf.sb
 }
