d683ca87df92483153a6e887657a953184dc5fb0
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-01-14 20:09:43 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/tables.R
+++ rsid/tables.R
@@ -1,31 +1,53 @@
-options(scipen=20, width=2000)
+#options(scipen=20, width=2000)
+
 require(openxlsx)
-
 url <- "https://jhz22.user.srcf.net/INF1.latest.xlsx"
-sheets <- c("summary","studies","inf1","pqtls","cojo","eqtls","eqtlstudies","transpqtls","knownpqtls","pqtlstudies","somalogic","pqtldisease")
-prefix <- "ST"
-titles <- c("summary","study information","panel information",
-            "pQTL summary","conditional analysis",
-            "eQTL overlap","eQTL studies","trans pQTL mapping","previous pQTLs","previous pQTL studies","SomaLogic replication",
-            "Disease GWAS overlap")
-sheetnames <- paste(prefix,titles,sep=" - ")
+read.sheet <- function(url=url,sheet,cols,rows)
+{
+  read.xlsx(url,sheet=sheet,colNames=TRUE,cols=cols,rows=rows,skipEmptyRows=TRUE)
+}
 
-summary <- data.frame(Sheet=sheets,Description=sheetnames)
-studies <- read.xlsx(url, sheet="Studies", colNames=TRUE, skipEmptyRows=TRUE, cols=1:3, rows=2:15)
-inf1 <- subset(read.xlsx(url, sheet="INF1", colNames=TRUE, skipEmptyRows=TRUE, cols=1:12, rows=1:93),uniprot!="P23560")
-pqtls <- read.xlsx(url, sheet="pQTLs", colNames=TRUE, skipEmptyRows=TRUE, cols=1:21, rows=1:181)
-cojo <- read.xlsx(url, sheet="cojo", colNames=TRUE, skipEmptyRows=TRUE, cols=1:22, rows=1:228)
-eqtls <- subset(within(read.xlsx(url, sheet="eQTLs", colNames=TRUE, skipEmptyRows=TRUE, cols=1:36, rows=1:214),{allele1 <- a1;allele2 <- a2}),
-                select=-c(a1,a2))
+summary <- read.sheet(sheet="Summary", cols=1:2, rows=2:36)
+inf1 <- subset(read.sheet(sheet="INF1", cols=1:12, rows=2:94),uniprot!="P23560")
+studies <- read.sheet(sheet="Studies", cols=1:3, rows=2:14)
+pqtls <- read.sheet(sheet="pQTLs", cols=1:21, rows=2:182)
+interval <- read.sheet(sheet="INTERVAL", cols=1:12, rows=2:29)
+otherknownloci <- read.sheet(sheet="OtherKnownLoci", cols=1:12, rows=2:102)
+cvd1 <- read.sheet(sheet="CVD1", cols=1:12, rows=2:53)
+aristotle <- read.sheet(sheet="ARISTOTLE", cols=1:14, rows=2:182)
+cojo <- read.sheet(sheet="cojo", cols=1:19, rows=2:229)
+h2pve <- read.sheet(sheet="h2pve", cols=1:10, rows=2:93)
+vep <- read.sheet(sheet="VEP", cols=1:69, rows=2:261)
+eqtls <- read.sheet(sheet="eQTLs", cols=1:24, rows=2:24)
+reactome <- read.sheet(sheet="reactome", cols=1:19, rows=2:589)
+great <- read.sheet(sheet="GREAT", cols=1:24, rows=2:30)
+garfield <- read.sheet(sheet="GARFIELD", cols=1:18, rows=2:7037)
+fusion <- read.sheet(sheet="FUSION", cols=1:26, rows=2:117)
+efo <- subset(read.sheet(sheet="EFO", cols=c(1:4), rows=c(2:79)),!is.na(MRBASEID))
+smr <- read.sheet(sheet="SMR", cols=1:27, rows=2:83)
+pqtlmr_immue <- read.sheet(sheet="pqtlMR-immune", cols=1:7, rows=2:67)
+pqtlmr_misc <- read.sheet(sheet="pqtlMR-misc", cols=1:7, rows=2:39)
+ivw <- read.sheet(sheet="IVW", cols=1:7, rows=2:12)
+gsmr <- read.sheet(sheet="GSMR", cols=1:12, rows=2:55)
+crp <- read.sheet(sheet="CRP", cols=1:15, rows=2:30)
+mrc2 <- read.sheet(sheet="MRC2", cols=1:7, rows=2:8)
+mvmr <- read.sheet(sheet="MVMR", cols=1:9, rows=2:6)
+genedrugbank <- read.sheet(sheet="geneDrugbank", cols=1:7, rows=2:72)
 eqtlstudies <- data.frame(Studies="Studies",pmid="pmid")
 transpqtls <- data.frame(Studies="Studies",pmid="pmid")
 knownpqtls <- data.frame(Studies="Studies",pmid="pmid")
 pqtlstudies <- data.frame(Studies="Studies",pmid="pmid")
 pqtldisease <- data.frame(Studies="Studies",pmid="pmid")
 somalogic <- data.frame(Studies="Studies",pmid="pmid")
-efo <- subset(read.xlsx(url, sheet="EFO", colNames=TRUE, skipEmptyRows=TRUE, cols=c(1:4), rows=c(1:78)),!is.na(MRBASEID))
 
 xlsx <- paste0("work/SCALLOP-INF.xlsx")
+outsheets <- c("summary","studies","inf1","pqtls","cojo","eqtls","eqtlstudies","knownpqtls","pqtlstudies","somalogic","pqtldisease")
+prefix <- "ST"
+titles <- c("summary","study information","panel information",
+            "pQTL summary","conditional analysis",
+            "eQTL overlap","eQTL studies","trans pQTL mapping","previous pQTLs","previous pQTL studies","SomaLogic replication",
+            "Disease GWAS overlap")
+sheetnames <- paste(prefix,titles,sep=" - ")
 wb <- createWorkbook(xlsx)
 for (i in 1:length(sheets))
 {
