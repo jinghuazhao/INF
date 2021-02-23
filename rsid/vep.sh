77d3c60b5de1d3dc0b370700fd66b4e0f315b9ce
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-02-23 17:21:22 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/vep.sh
+++ rsid/vep.sh
@@ -37,3 +37,29 @@ vep -i ${s}.vcf -o ${s}.vepoutput --per_gene --check_existing --force_overwrite 
 grep -v '#' ${s}.vepoutput | \
 grep -e missense_variant -e stop_lost -e splice_acceptor_variant | cut -f1 | grep -f - INF1.merge.cis
 cd ${dir}
+
+### clumping
+
+cat work/INF1.merge.alleles | \
+tr '\t' ' ' | \
+parallel -C' ' --env INF '
+  echo {2}
+  plink --bfile ${INF}/INTERVAL/cardio/INTERVAL \
+        --snp {2} --window 2000 \
+        --clump METAL/gwas2vcf/{1}.tsv.gz \
+        --clump-snp-field snpid \
+        --clump-p1 1 --clump-p2 1 --clump-r2 0.8 --clump-field p \
+        --out ${INF}/annotate/{1}-{2}
+  awk "NR>1{print \$3}" {1}-{2}.clumped > ${INF}/annotate/{1}-{2}.snpid
+  export exist=$(grep {2} ${INF}/annotate/{1}-{2}.snpid)
+  if [ ${exist} =="" ]; then echo {1} >> ${INF}/annotate/{1}-{2}.snpid; fi
+  (
+    echo "##fileformat=VCFv4.0"
+    echo "#CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO" | \
+    tr "," "\t"
+    awk "{print \$3}" {1}-{2}.snpid | \
+    awk "{snpid=\$1;gsub(/chr|:|_/,\" \",snpid);split(snpid,a,\" \");print a[1],a[2],\$1,a[3],a[4],\".\",\".\",\".\"}" | \
+    sort -k1,1n -k2,2n | \
+    tr " " "\t"
+  ) > ${INF}/annotate/{1}-{2}.vcf
+'
