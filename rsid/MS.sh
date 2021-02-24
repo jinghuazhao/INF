263d628e5844f2be8e1e3adb034dfcb2ae81659d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-02-24 21:18:26 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/MS.sh
+++ rsid/MS.sh
@@ -424,3 +424,9 @@ R --no-save -q <<END
 END
 
 # https://www.gtexportal.org/home/gene/RP1-102E24.8
+# no sample size (N)!!!
+export ichip=~/rds/results/public/gwas/multiple_sclerosis/ImmunoChip_Results/Immunochip_FinalResults_LimitedDiscovery.txt
+awk -vchr=${chr} -vstart=${start} -vend=${end} '$1==chr&&$3>=start&&$3<=end {print $4,$5,$6,$7,log($10),$11,$9}' ${ichip}
+# CHR BPHG18 BPHG19 ImmunochipID Risk_Allele Ref_Allele Risk_Allele_Freq N P OR SE Q I Region
+# 1 1108138 1118275 vh_1_1108138 G A 0.9586 11 7.62E-01 1.012 0.0404 0.085 39.51 none
+
--- rsid/vep.sh
+++ rsid/vep.sh
@@ -1,65 +1,58 @@
 #!/usr/bin/bash
 
+function prune()
+{
+  sed 's/\t/ /g' ${INF}/work/INF1.merge.alleles | parallel -C' ' --env INF '
+    echo {1}-{2}
+    export rt=${INF}/annotate/{1}-{2}
+    plink --bfile ${INF}/INTERVAL/cardio/INTERVAL --snp {2} --window 1000 \
+          --geno 0.1 --mind 0.1 --maf 0.005 --indep-pairwise 1000kb 1 0.8 --out ${rt}
+    if [ $(grep -w {2} ${rt}.prune.in | wc -l) -eq 0 ]; then
+       export i=$(grep -w -f ${rt}.prune.in ${INF}/INTERVAL/cardio/INTERVAL.bim | \
+                  awk -vpos={4} "function abs(x) {if (x<0) return -x; else return x;} {d=abs(\$4-pos);print \$1, \$2, \$4, d}" | \
+                  sort -r -k4,4n | \
+                  awk "NR==1 {print \$2}" \
+                )
+       sed -i 's/'"$i"'/'"{2}"'/g' ${rt}.prune.in
+    fi
+    (
+      echo "##fileformat=VCFv4.0"
+      echo "#CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO" | \
+      tr "," "\t"
+      sed "s/chr//" ${rt}.prune.in | \
+      awk -vOFS="\t" "{snpid=\$1;gsub(/:|_/,\" \",snpid);split(snpid,a,\" \");print a[1],a[2],\$1,a[3],a[4],\".\",\".\",\".\"}"
+      sort -k1,1n -k2,2n
+    ) > ${rt}.vcf
+    rm ${rt}.prune.* ${rt}.log ${rt}.nosex
+  '
+}
+
 export LOFTEE=${HPC_WORK}/loftee
 export VEP=${HPC_WORK}/ensembl-vep
 export TMPDIR=/rds/user/jhz22/hpc-work/work
-export dir=${INF}
 
-for f in INF1.proxy INF1.proxy.trans
+cd ${LOFTEE}
+for f in $(ls ${INF}/annotate/*vcf)
 do
 # snpid --> rsid
-  cd ${INF}/work
-  cp ${f}.vepinput ${f}-rsid.vepinput
+  cp ${f}.vcf ${f}-rsid.vcf
   (
-  sed '1,2d' ${f}.vepinput | cut -f3 | sort | join - INTERVAL.rsid | \
-  parallel --dry-run -C' ' "
-    export s={1};
-    export r={2};
-    sed -i 's/'\"\${s}\"'/'\"\${r}\"'/g' ${f}-rsid.vepinput
-  "
+    sed '1,2d' ${f}.vcf | cut -f3 | sort | join - ${INF}/work/INTERVAL.rsid | \
+    parallel --dry-run -C' ' "
+      export s={1};
+      export r={2};
+      sed -i 's/'\"\${s}\"'/'\"\${r}\"'/g' ${f}-rsid.vcf
+    "
   ) | bash
 # VEP annotation
-  cd ${LOFTEE}
-  vep --input_file ${INF}/work/${f}-rsid.vepinput --output_file ${INF}/work/${f}-rsid.tab --force_overwrite \
+  vep --input_file ${INF}/work/${f}-rsid.vepinput --output_file ${INF}/annotate/${f}-rsid.tab --force_overwrite \
       --cache --dir_cache ${HPC_WORK}/ensembl-vep/.vep --dir_plugins ${HPC_WORK}/loftee --offline \
       --species homo_sapiens --assembly GRCh37 --pick --nearest symbol --symbol --plugin TSSDistance \
       --plugin LoF,loftee_path:.,human_ancestor_fa:human_ancestor.fa.gz,conservation_file:phylocsf_gerp.sql.gz \
       --tab
-done
 # per_gene
-sed 's/chr//'  INF1.merge.cis | \
-grep -f - INF1.merge-rsid.vepinput > INF1.merge.cis.vcf
-export s=INF1.merge.cis
-vep -i ${s}.vcf -o ${s}.vepoutput --per_gene --check_existing --force_overwrite --offline \
-    --species homo_sapiens --everything --assembly GRCh37 --nearest symbol \
-    --symbol --pubmed --uniprot --protein --sift b --polyphen b --tab
-## cross-check
-grep -v '#' ${s}.vepoutput | \
-grep -e missense_variant -e stop_lost -e splice_acceptor_variant | cut -f1 | grep -f - INF1.merge.cis
-cd ${dir}
-
-### clumping
-
-cat work/INF1.merge.alleles | \
-tr '\t' ' ' | \
-parallel -C' ' --env INF '
-  echo {2}
-  plink --bfile ${INF}/INTERVAL/cardio/INTERVAL \
-        --snp {2} --window 2000 \
-        --clump METAL/gwas2vcf/{1}.tsv.gz \
-        --clump-snp-field snpid \
-        --clump-p1 1 --clump-p2 1 --clump-r2 0.8 --clump-field p \
-        --out ${INF}/annotate/{1}-{2}
-  awk "NR>1{print \$3}" {1}-{2}.clumped > ${INF}/annotate/{1}-{2}.snpid
-  export exist=$(grep {2} ${INF}/annotate/{1}-{2}.snpid)
-  if [ ${exist} =="" ]; then echo {1} >> ${INF}/annotate/{1}-{2}.snpid; fi
-  (
-    echo "##fileformat=VCFv4.0"
-    echo "#CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO" | \
-    tr "," "\t"
-    awk "{print \$3}" {1}-{2}.snpid | \
-    awk "{snpid=\$1;gsub(/chr|:|_/,\" \",snpid);split(snpid,a,\" \");print a[1],a[2],\$1,a[3],a[4],\".\",\".\",\".\"}" | \
-    sort -k1,1n -k2,2n | \
-    tr " " "\t"
-  ) > ${INF}/annotate/{1}-{2}.vcf
-'
+  vep -i ${f}.vcf -o ${f}.pergene --per_gene --check_existing --force_overwrite --offline \
+      --species homo_sapiens --everything --assembly GRCh37 --nearest symbol \
+      --symbol --pubmed --uniprot --protein --sift b --polyphen b --tab
+done
+cd -
