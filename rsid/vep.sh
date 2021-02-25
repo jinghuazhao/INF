7eaaf8ae9ad1d0e850e4eb043b5a8903fa115c38
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-02-25 09:05:41 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/vep.sh
+++ rsid/vep.sh
@@ -2,50 +2,49 @@
 
 function prune()
 {
+  export sq="'"
   sed 's/\t/ /g' ${INF}/work/INF1.merge.alleles | parallel -C' ' --env INF '
     echo {1}-{2}
     export rt=${INF}/annotate/{1}-{2}
     plink --bfile ${INF}/INTERVAL/cardio/INTERVAL --snp {2} --window 1000 \
           --geno 0.1 --mind 0.1 --maf 0.005 --indep-pairwise 1000kb 1 0.8 --out ${rt}
-    if [ $(grep -w {2} ${rt}.prune.in | wc -l) -eq 0 ]; then
+    export incl=$(grep -w {2} ${rt}.prune.in)
+    if [ "${incl}" == "" ]; then
        export i=$(grep -w -f ${rt}.prune.in ${INF}/INTERVAL/cardio/INTERVAL.bim | \
                   awk -vpos={4} "function abs(x) {if (x<0) return -x; else return x;} {d=abs(\$4-pos);print \$1, \$2, \$4, d}" | \
                   sort -r -k4,4n | \
                   awk "NR==1 {print \$2}" \
                 )
-       sed -i 's/'"$i"'/'"{2}"'/g' ${rt}.prune.in
+       sed -i "s/${i}/{2}/;s/${sq}//g" ${rt}.prune.in
     fi
     (
       echo "##fileformat=VCFv4.0"
       echo "#CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO" | \
       tr "," "\t"
       sed "s/chr//" ${rt}.prune.in | \
-      awk -vOFS="\t" "{snpid=\$1;gsub(/:|_/,\" \",snpid);split(snpid,a,\" \");print a[1],a[2],\$1,a[3],a[4],\".\",\".\",\".\"}"
+      awk -vOFS="\t" "{snpid=\$1;gsub(/:|_/,\" \",snpid);split(snpid,a,\" \");print a[1],a[2],\"chr\"\$1,a[3],a[4],\".\",\".\",\".\"}"
       sort -k1,1n -k2,2n
     ) > ${rt}.vcf
     rm ${rt}.prune.* ${rt}.log ${rt}.nosex
   '
 }
 
-export LOFTEE=${HPC_WORK}/loftee
-export VEP=${HPC_WORK}/ensembl-vep
-export TMPDIR=/rds/user/jhz22/hpc-work/work
-
-cd ${LOFTEE}
+cd ${HPC_WORK}/loftee
 for f in $(ls ${INF}/annotate/*vcf | sed 's/.vcf//g')
 do
+  echo ${f}
 # snpid --> rsid
-  cp ${f}.vcf ${f}-rsid.vcf
+  cp ${f}.vcf ${f}.vepinput
   (
     sed '1,2d' ${f}.vcf | cut -f3 | sort | join - ${INF}/work/INTERVAL.rsid | \
     parallel --dry-run -C' ' "
       export s={1};
       export r={2};
-      sed -i 's/'\"\${s}\"'/'\"\${r}\"'/g' ${f}-rsid.vcf
+      sed -i 's/'\"\${s}\"'/'\"\${r}\"'/g' ${f}.vepinput
     "
   ) | bash
 # VEP annotation
-  vep --input_file ${f}-rsid.vcf --output_file ${f}-rsid.tab --force_overwrite \
+  vep --input_file ${f}.vepinput --output_file ${f}.tab --force_overwrite \
       --cache --dir_cache ${HPC_WORK}/ensembl-vep/.vep --dir_plugins ${HPC_WORK}/loftee --offline \
       --species homo_sapiens --assembly GRCh37 --pick --nearest symbol --symbol --plugin TSSDistance \
       --plugin LoF,loftee_path:.,human_ancestor_fa:human_ancestor.fa.gz,conservation_file:phylocsf_gerp.sql.gz \
