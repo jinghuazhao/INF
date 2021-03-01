ff010d735f7c66c629c5235b5b30f305a8d43149
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-03-01 21:55:11 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/SomaLogic-Olink.R
+++ rsid/SomaLogic-Olink.R
@@ -6,15 +6,16 @@ library(dplyr)
 HOME <- Sys.getenv("HOME")
 INF <- Sys.getenv("INF")
 
-source.vs.INF1 <- function(source,group)
-  venn.diagram(list(SomaLogic=unique(source[["UniProt"]]), Olink=INF1[["uniprot"]]),paste0(group,"-INF1.tiff"),
-               force.unique=FALSE,height=8,width=8,units="in")
 # panels
 SomaLogic <- setdiff(with(SomaLogic160410,UniProt),"P23560")
 Olink <- setdiff(with(inf1,uniprot),"P23560")
-venn.diagram(list(SomaLogic=SomaLogic, Olnk=Olink),"SomaLogic-Olink.tiff",force.unique=FALSE,height=8,width=8,units="in")
-overlap <- intersect(SomaLogic,Olink)
+venn.diagram(list(SomaLogic=SomaLogic, Olnk=Olink),"SomaLogic-Olink-panel.tiff",force.unique=FALSE,height=8,width=8,units="in")
+
 # pQTLs
+source.vs.INF1 <- function(source,group)
+  venn.diagram(list(SomaLogic=source[["UniProt"]], Olink=INF1[["uniprot"]]),paste0(group,"-INF1-pQTLs.tiff"),
+               force.unique=FALSE,height=8,width=8,units="in")
+overlap <- intersect(SomaLogic,Olink)
 INF1_metal <- read.delim(file.path(INF,"work","INF1.METAL"))
 INF1 <- INF1_metal %>% filter(uniprot %in% overlap)
 box <- read.delim(file.path(HOME,"SomaLogic","doc","INTERVAL-box.tsv")) %>% filter(UniProt %in% overlap)
@@ -26,28 +27,53 @@ ST4 <- st4 %>% select(Locus.ID,SOMAmer.ID,Target,UniProt,"Sentinel.variant*",Chr
        rename(cis_trans="cis/.trans") %>% mutate(UniProt_group=paste0(UniProt,"_",cis_trans))
 source.vs.INF1(ST4,"ST4")
 
-INF1 <- INF1_metal %>% filter(uniprot %in% overlap) %>% mutate(uniprot_group=paste0(uniprot,"_",cis.trans))
-venn.diagram(list(Olink=with(INF1,uniprot),SomaLogic=with(ST4,UniProt)),
-             file="ab.tiff",force.unique=FALSE,height=8,width=8,units="in")
-venn.diagram(list(Olink=INF1[grepl("cis",INF1$cis.trans),"uniprot"],SomaLogic=ST4[grepl("cis",ST4$cis_trans),"UniProt"]),
-             file="cis-cis.tiff",force.unique=FALSE,height=8,width=8,units="in")
-venn.diagram(list(Olink=INF1[grepl("trans",INF1$cis.trans),"uniprot"],SomaLogic=ST4[grepl("trans",ST4$cis_trans),"UniProt"]),
-             file="trans-trans.tiff",force.unique=FALSE,height=8,width=8,units="in")
-venn.diagram(list(Olink=INF1[grepl("cis",INF1$cis.trans),"uniprot"],SomaLogic=ST4[grepl("trans",ST4$cis_trans),"UniProt"]),
-             file="cis-trans.tiff",force.unique=FALSE,height=8,width=8,units="in")
-venn.diagram(list(Olink=INF1[grepl("trans",INF1$cis.trans),"uniprot"],SomaLogic=ST4[grepl("cis",ST4$cis_trans),"UniProt"]),
-             file="trans-cis.tiff",force.unique=FALSE,height=8,width=8,units="in")
-# Name	SL	common	Olink
-# Panel	3523	77	14
-# ---
-# Box	15	54	102
-# PS	4	23	133
-# agree
-# cis	5	16	33
-# trans	17	11	96
-# ST4	22	27	129
-# disagree
-# c-t	18	10	39
-# t-c	8	13	94
+vd <- function(d,f) venn.diagram(as.list(d),category=c("SomaLogic","Olink"),force.unique=FALSE,height=8,width=8,units="in",filename=f)
+join <- function()
+{
+  INF1 <- INF1_metal %>% filter(uniprot %in% overlap) %>% mutate(uniprot_group=paste0(uniprot,"_",cis.trans))
+  ST4_INF1 <-merge(ST4,INF1,by.x="UniProt",by.y="uniprot")
+  with(ST4_INF1,table(cis_trans,cis.trans))
 
+  AA <- ST4_INF1[c("uniprot_group","UniProt_group")]
+  CC <- subset(ST4_INF1,grepl("cis",cis.trans) & grepl("cis",cis_trans), select=c(cis_trans,cis.trans))
+  CT <- subset(ST4_INF1,grepl("cis",cis.trans) & grepl("trans",cis_trans), select=c(cis_trans,cis.trans))
+  TC <- subset(ST4_INF1,grepl("trans",cis.trans) & grepl("cis",cis_trans), select=c(cis_trans,cis.trans))
+  TT <- subset(ST4_INF1,grepl("trans",cis.trans) & grepl("trans",cis_trans), select=c(cis_trans,cis.trans))
+
+  vd(AA,"ST4-INF1.tiff")
+  vd(CC,"cis-cis.tiff")
+  vd(CT,"cis-trans.tiff")
+  vd(TC,"trans-cis.tiff")
+  vd(TT,"trans-trans.tiff")
+}
+
+UniProts <- unique(ST4$UniProt)
+uniprots <- unique(INF1$uniprot)
+both <- intersect(ST4$UniProt,INF1$uniprot)
+venn.diagram(list(SomaLogic=ST4[["UniProt"]],Olink=INF1[["uniprot"]]),filename="ST4-INF1-prot.tiff")
+
+CC <- list(subset(ST4,UniProt %in% both & cis_trans=="cis")[["UniProt"]],subset(INF1,uniprot %in% both & cis.trans=="cis")[["uniprot"]])
+CT <- list(subset(ST4,UniProt %in% both & cis_trans=="cis")[["UniProt"]],subset(INF1,uniprot %in% both & cis.trans=="trans")[["uniprot"]])
+TC <- list(subset(ST4,UniProt %in% both & cis_trans=="trans")[["UniProt"]],subset(INF1,uniprot %in% both & cis.trans=="cis")[["uniprot"]])
+TT <- list(subset(ST4,UniProt %in% both & cis_trans=="trans")[["UniProt"]],subset(INF1,uniprot %in% both & cis.trans=="trans")[["uniprot"]])
+vd(CC,"cis-cis.tiff")
+vd(CT,"cis-trans.tiff")
+vd(TC,"trans-cis.tiff")
+vd(TT,"trans-trans.tiff")
+
+#
+# Name	SL	both	Olink
+# proteins
+# Panel	3523	77	14
+# ST4	5	27	32
+# pQTLs
+# Box	48	54	102
+# PS	312	23	133
+# ST4	22	27	129
+# ST4/INF1
+# CC	5	16	7
+# TT	11	11	41
+# CT	8	13	39
+# TC	12	10	13
+#
 # coloc???
