e78a6d8b2edf317f336b3d4d046b7c320d8db71c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-03-05 12:33:19 +0000

    Final work on CSD3 with rsids in the downstream analysis

--- rsid/SomaLogic-Olink.R
+++ rsid/SomaLogic-Olink.R
@@ -9,12 +9,22 @@ INF <- Sys.getenv("INF")
 # panels
 SomaLogic <- setdiff(with(SomaLogic160410,UniProt),"P23560")
 Olink <- setdiff(with(inf1,uniprot),"P23560")
-venn.diagram(list(SomaLogic=SomaLogic, Olnk=Olink),"SomaLogic-Olink-panel.tiff",force.unique=FALSE,height=8,width=8,units="in")
+pdf("SomaLogic-Olink.pdf")
+panel <- venn.diagram(list(SomaLogic=SomaLogic, Olnk=Olink),filename=NULL,force.unique=FALSE,height=8,width=8,units="in")
+grid.newpage()
+grid.draw(panel)
 
 # pQTLs
 source.vs.INF1 <- function(source,group)
-  venn.diagram(list(SomaLogic=source[["UniProt"]], Olink=INF1[["uniprot"]]),paste0(group,"-INF1-pQTLs.tiff"),
-               force.unique=FALSE,height=8,width=8,units="in")
+{
+  dlist <- list(SomaLogic=source[["UniProt"]], Olink=INF1[["uniprot"]])
+  calc.overlap <- calculate.overlap(dlist)
+  a1_a2_a3 <- unlist(lapply(calc.overlap,length))
+  cat(c(a1_a2_a3[1]-a1_a2_a3[3],a1_a2_a3[3],a1_a2_a3[2]-a1_a2_a3[3]),sep="\t","\n")
+  pqtl <- venn.diagram(dlist,filename=NULL,force.unique=FALSE,height=8,width=8,units="in")
+  grid.newpage()
+  grid.draw(pqtl)
+}
 overlap <- intersect(SomaLogic,Olink)
 INF1_metal <- read.delim(file.path(INF,"work","INF1.METAL"))
 INF1 <- INF1_metal %>% filter(uniprot %in% overlap)
@@ -28,46 +38,52 @@ ST4 <- st4 %>% select(Locus.ID,SOMAmer.ID,Target,UniProt,"Sentinel.variant*",Chr
        rename(cis_trans="cis/.trans")
 source.vs.INF1(ST4,"ST4")
 
-INF1 <- INF1_metal %>% filter(uniprot %in% overlap)
 UniProts <- unique(ST4$UniProt)
 uniprots <- unique(INF1$uniprot)
 both <- intersect(ST4$UniProt,INF1$uniprot)
 UniProt_uniprot <- list(SomaLogic=ST4[["UniProt"]],Olink=INF1[["uniprot"]])
-venn.diagram(UniProt_uniprot,filename="ST4-INF1-prot.tiff",fill=c("blue","red"),height=6,width=6,units="in")
+ST4_INF1_prot <- venn.diagram(UniProt_uniprot,filename=NULL,fill=c("blue","red"),height=6,width=6,units="in")
+grid.newpage()
+grid.draw(ST4_INF1_prot)
 calc.overlap <- calculate.overlap(UniProt_uniprot)
 cat(unlist(lapply(calc.overlap,length)),sep="\t","\n")
 
-C1 <- subset(ST4,UniProt %in% both & cis_trans=="cis")[["UniProt"]]
-C2 <- subset(INF1,uniprot %in% both & cis.trans=="cis")[["uniprot"]]
-T1 <- subset(ST4,UniProt %in% both & cis_trans=="trans")[["UniProt"]]
-T2 <- subset(INF1,uniprot %in% both & cis.trans=="trans")[["uniprot"]]
-C1C2 <- intersect(C1,C2)
-T1T2 <- intersect(T1,T2)
-
-CC <- list(subset(ST4,UniProt %in% both & cis_trans=="cis")[["UniProt"]],subset(INF1,uniprot %in% both & cis.trans=="cis")[["uniprot"]])
-TT <- list(subset(ST4,UniProt %in% both & cis_trans=="trans")[["UniProt"]],subset(INF1,uniprot %in% both & cis.trans=="trans")[["uniprot"]])
-CT <- list(setdiff(C1,C1C2),setdiff(T2,T1T2))
-TC <- list(setdiff(T1,T1T2),setdiff(C2,C1C2))
-
-vd <- function(d,f,diagram=FALSE)
-{
-  dlist <- as.list(d)
-  if (diagram) venn.diagram(dlist,category=c("SomaLogic","Olink"),force.unique=FALSE,height=8,width=8,units="in",filename=f)
-  calc.overlap <- calculate.overlap(dlist)
-  a1_a2_a3 <- unlist(lapply(calc.overlap,length))
-  cat(c(a1_a2_a3[1]-a1_a2_a3[3],a1_a2_a3[3],a1_a2_a3[2]-a1_a2_a3[3]),sep="\t","\n")
-}
-
-vd(CC,"cis-cis.tiff", diagram=TRUE)
-vd(TT,"trans-trans.tiff", diagram=TRUE)
-vd(CT,"cis-trans.tiff")
-vd(TC,"trans-cis.tiff")
 OnlyUniProts <- setdiff(with(ST4,UniProt),both)
 Onlyuniprots <- setdiff(with(INF1,uniprot),both)
 table(subset(ST4,UniProt %in% OnlyUniProts)["cis_trans"])
 table(subset(ST4,UniProt %in% both)["cis_trans"])
-table(subset(INF1,uniprot %in% Onlyuniprots)["cis.trans"])
 table(subset(INF1,uniprot %in% both)["cis.trans"])
+table(subset(INF1,uniprot %in% Onlyuniprots)["cis.trans"])
+
+C1 <- subset(ST4,UniProt %in% both & cis_trans=="cis")[["UniProt"]]
+T1 <- subset(ST4,UniProt %in% both & cis_trans=="trans")[["UniProt"]]
+C2 <- subset(INF1,uniprot %in% both & cis.trans=="cis")[["uniprot"]]
+T2 <- subset(INF1,uniprot %in% both & cis.trans=="trans")[["uniprot"]]
+C1C2 <- intersect(C1,C2)
+T1T2 <- intersect(T1,T2)
+
+CC <- list(C1,C2)
+TT <- list(T1,T2)
+CT <- list(setdiff(C1,C1C2),setdiff(T2,T1T2))
+TC <- list(setdiff(T1,T1T2),setdiff(C2,C1C2))
+
+vd <- function(dlist,diagram=FALSE)
+{
+  calc.overlap <- calculate.overlap(dlist)
+  a1_a2_a3 <- unlist(lapply(calc.overlap,length))
+  cat(c(a1_a2_a3[1]-a1_a2_a3[3],a1_a2_a3[3],a1_a2_a3[2]-a1_a2_a3[3]),sep="\t","\n")
+  if (diagram)
+  {
+    vd <- venn.diagram(dlist,category=c("SomaLogic","Olink"),force.unique=FALSE,height=8,width=8,units="in",filename=NULL)
+    grid.newpage()
+    grid.draw(vd)
+  }
+}
+
+vd(CC, diagram=TRUE)
+vd(TT, diagram=TRUE)
+vd(CT)
+vd(TC)
 
 #
 # Name	SL	both	Olink
@@ -86,66 +102,60 @@ table(subset(INF1,uniprot %in% both)["cis.trans"])
 #
 # coloc???
 
-SomaLogic_Olink <- function()
-{
-  library(VennDiagram)
-  INF1_prot <- read.table("work/INF1.merge.prot",col.names=c("prot","uniprot"))
-  library(pQTLtools)
-  SomaLogic_prot <- unique(replace(st4$UniProt,st4$UniProt=="P29460,Q9NPF7","P29460"))
-  plist <- list(INF1_prot$uniprot,SomaLogic_prot)
-  ov <- VennDiagram::calculate.overlap(plist)
-  ov$a3
-  venn.plot <- draw.pairwise.venn(1469, 70, 28,
-               category = c("SomaLogic", "Olink"),
-               fill = c("blue", "red"),
-               lty = "blank",
-               cex = 2,
-               cat.cex = 2,
-               cat.pos = c(200, 50),
-               cat.dist = 0.09,
-               cat.just = list(c(-1, -1), c(1, 1)),
-               ext.pos = 30,
-               ext.dist = -0.05,
-               ext.length = 0.85,
-               ext.line.lwd = 2,
-               ext.line.lty = "dashed"
-             );
-  grid.draw(venn.plot);
-  png('SomaLogic-Olink-proteins.png', height=20, width=20, units="cm", res=300)
-  grid.draw(venn.plot);
-  dev.off();
-  grid.newpage()
-  venn.plot <- draw.pairwise.venn(45, 83, 10,
-               category = c("SomaLogic", "Olink"),
-               fill = c("blue", "red"),
-               lty = "blank",
-               cex = 2,
-               cat.cex = 2,
-               cat.pos = c(30, 10),
-               cat.dist = 0.09,
-               cat.just = list(c(-1, -1), c(1, 1)),
-               ext.pos = 30,
-               ext.dist = -0.05,
-               ext.length = 0.85,
-               ext.line.lwd = 2,
-               ext.line.lty = "dashed"
-             );
-  grid.draw(venn.plot);
-  png('SomaLogic-Olink-sentinels.png', height=20, width=20, units="cm", res=300)
-  grid.draw(venn.plot);
-  dev.off();
-  grid.newpage()
-  st6[c(39,53),"UniProt"] <- "P29460"
-  st6ov <- subset(st6,UniProt%in%ov$a3)
+INF1_prot <- read.table(file.path(INF,"work","INF1.merge.prot"),col.names=c("prot","uniprot"))
+SomaLogic_prot <- unique(replace(st4$UniProt,st4$UniProt=="P29460,Q9NPF7","P29460"))
+plist <- list(SomaLogic_prot,INF1_prot$uniprot)
+ov <- VennDiagram::calculate.overlap(plist)
+calc.overlap <- calculate.overlap(plist)
+a1_a2_a3 <- unlist(lapply(calc.overlap,length))
+a1_a2_a3
+cat(c(a1_a2_a3[1]-a1_a2_a3[3],a1_a2_a3[3],a1_a2_a3[2]-a1_a2_a3[3]),sep="\t","\n")
+
+grid.newpage()
+venn.plot <- draw.pairwise.venn(a1_a2_a3[1],a1_a2_a3[2],a1_a2_a3[3],
+             category = c("SomaLogic", "Olink"),
+             fill = c("blue", "red"),
+             lty = "blank",
+             cex = 2,
+             cat.cex = 2,
+             cat.pos = c(200, 50),
+             cat.dist = 0.09,
+             cat.just = list(c(-1, -1), c(1, 1)),
+             ext.pos = 30,
+             ext.dist = -0.05,
+             ext.length = 0.85,
+             ext.line.lwd = 2,
+             ext.line.lty = "dashed"
+           );
+grid.draw(venn.plot);
+grid.newpage()
+venn.plot <- draw.pairwise.venn(45, 83, 10,
+             category = c("SomaLogic", "Olink"),
+             fill = c("blue", "red"),
+             lty = "blank",
+             cex = 2,
+             cat.cex = 2,
+             cat.pos = c(30, 10),
+             cat.dist = 0.09,
+             cat.just = list(c(-1, -1), c(1, 1)),
+             ext.pos = 30,
+             ext.dist = -0.05,
+             ext.length = 0.85,
+             ext.line.lwd = 2,
+             ext.line.lty = "dashed"
+           );
+grid.draw(venn.plot);
+st6[c(39,53),"UniProt"] <- "P29460"
+st6ov <- subset(st6,UniProt%in%ov$a3)
 # significant on INTERVAL (27)
-  dim(subset(st6ov,as.numeric(p.1)<1e-11))
-  z <- with(st6ov,{
-    chr <- st6ov[["Chr"]]
-    pos <- st6ov[["Pos"]]
-    a1 <- st6ov[["Effect.Allele.(EA)"]]
-    a2 <- st6ov[["Other.Allele.(OA)"]]
-    cbind(UniProt,snpid=gap::chr_pos_a1_a2(chr,pos,a1,a2))
-  })
-  write.table(merge(inf1,z,by.x="uniprot",by.y="UniProt")[c("snpid","prot","uniprot")],
-              file="SomaLogic.id3",col.names=FALSE,row.names=FALSE,quote=FALSE)
-}
+dim(subset(st6ov,as.numeric(p.1)<1e-11))
+z <- with(st6ov,{
+  chr <- st6ov[["Chr"]]
+  pos <- st6ov[["Pos"]]
+  a1 <- st6ov[["Effect.Allele.(EA)"]]
+  a2 <- st6ov[["Other.Allele.(OA)"]]
+  cbind(UniProt,snpid=gap::chr_pos_a1_a2(chr,pos,a1,a2))
+})
+write.table(merge(inf1,z,by.x="uniprot",by.y="UniProt")[c("snpid","prot","uniprot")],
+            file="SomaLogic.id3",col.names=FALSE,row.names=FALSE,quote=FALSE)
+dev.off()
