76377ff34bad3c6ef56918f1f16804230f5c35e5
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-04-12 21:52:39 +0100

    Work on CSD3

--- csd3/merge.sh
+++ csd3/merge.sh
@@ -157,25 +157,62 @@ awk -v OFS="\t" '{
 
 R --no-save -q <<END
   library(gap)
-# library(Rmpfr)
   gz <- gzfile("METAL/IL.17C-1.tbl.gz")
-  IL.17C <- within(read.delim(gz,as.is=TRUE), {
-   Z <- Effect/StdErr;
-#  P <- as.numeric(2*pnorm(mpfr(abs(Z),100),lower.tail=FALSE))
-   P <- 2*pnorm(abs(Z),lower.tail=FALSE)
-  })
-  subset(IL.17C, P==0)
+  IL.17C <- within(read.delim(gz,as.is=TRUE), {Z <- Effect/StdErr})
   png("IL.17C.png", res=300, units="in", width=9, height=6)
   par(oma=c(0,0,0,0), mar=c(5,6.5,1,1))
-  mhtplot.trunc(IL.17C, chr="Chromosome", bp="Position", p="P", snp="MarkerName", z = "Z",
-                suggestiveline=FALSE, genomewideline=-log10(5e-10), logp = TRUE,
+  mhtplot.trunc(IL.17C, chr="Chromosome", bp="Position", z="Z", snp="MarkerName",
+                suggestiveline=FALSE, genomewideline=-log10(5e-10),
                 cex.mtext=0.6, cex.text=0.7,
-                mtext.line=4, y.brk1=300, y.brk2=500, cex.axis=0.6, cex.y=0.6, cex=0.5,
+                mtext.line=4, y.brk1=10, y.brk2=12, cex.axis=0.6, cex.y=0.6, cex=0.5,
                 y.ax.space=20,
                 col = c("blue4", "skyblue")
   )
   dev.off()
 END
+
+R --no-save -q <<END
+  library(gap)
+  INF <- Sys.getenv("INF")
+  gz <- gzfile(file.path(INF,"METAL","IL.12B-1.tbl.gz"))
+  IL.12B <- within(read.delim(gz,as.is=TRUE), {Z <- Effect/StdErr; P <- pvalue(Z); log10P <- -log10p(Z)})
+  genes <- data.frame(chr=c("chr3","chr3","chr5","chr6","chr12","chr13","chr14","chr14"),
+                      snpid=c("chr3:5026008_A_G","chr3:188115682_A_C","chr5:158792819_C_G","chr6:31154493_A_G","chr12:111884608_C_T",
+                              "chr13:28604007_C_T","chr14:68760141_C_T","chr14:103230758_C_G"),
+                      snp=c("rs11130215","rs9815073","rs10076557","rs3130510","rs3184504","rs76428106","rs12588969","rs1950897"),
+                      gene=c("BHLHE40","LPP","IL12B","MHC","SH2B3","FLT3","RAD51B","TARF3")
+           )
+  library(dplyr)
+  IL.12B <- left_join(IL.12B,genes,by=c("MarkerName"="snpid"),keep=TRUE) %>%
+             mutate(MarkerName=ifelse(!is.na(snpid),gene,MarkerName)) %>%
+             select(-c(chr,snpid,snp))
+  subset(IL.12B,!is.na(gene))
+  png("IL.12B-mhtplot.trunc.png", res=300, units="in", width=9, height=6)
+  par(oma=c(0,0,0,0), mar=c(5,6.5,1,1))
+  mhtplot.trunc(IL.12B, chr="Chromosome", bp="Position", z="Z", snp="MarkerName",
+                suggestiveline=FALSE, genomewideline=-log10(5e-10),
+                cex.mtext=1.2, cex.text=1.2,
+                annotatelog10P=-log10(5e-10), annotateTop = FALSE, highlight=with(genes,gene),
+                mtext.line=3, y.brk1=115, y.brk2=200, cex.axis=1.2, cex.y=1.2, cex=0.5,
+                y.ax.space=20,
+                col = c("blue4", "skyblue")
+  )
+  dev.off()
+END
+# IL.12B[which(IL.12B$MarkerName%in%genes$snpid),"MarkerName"] <- genes[match(interaction(IL.12B[which(IL.12B$MarkerName%in%genes$snpid),c("MarkerName")]), 
+#                                              interaction(genes[,c("snpid")])), "gene"]
+# library(dplyr)
+# left_join(IL.12B,genes[,c(1:3)],by=c("MarkerName"="snpid"))%>%
+# mutate(MarkerName=ifelse(snpid!="",gene,MarkerName))%>%
+# select(-snp)
+# chr3  chr3:5026008_A_G    rs11130215 BHLHE40
+# chr3  chr3:188115682_A_C  rs9815073  LPP
+# chr5  chr5:158792819_C_G  rs10076557 IL12B
+# chr6  chr6:31154493_A_G   rs3130510  MHC
+# chr12 chr12:111884608_C_T rs3184504  SH2B3
+# chr13 chr13:28604007_C_T  rs76428106 FLT3
+# chr14 chr14:68760141_C_T  rs12588969 RAD51B
+# chr14 chr14:103230758_C_G rs1950897  TARF3
 
 R --no-save <<END
   library(gap)
