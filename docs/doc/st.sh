395102fbcf8b4321be4c96d309435ec69501c3fc
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-06-24 21:45:52 +0100

    docs

--- docs/doc/st.sh
+++ docs/doc/st.sh
@@ -2,12 +2,6 @@
 
 function nonweb ()
 {
-git add .gitignore
-git commit -m ".gitignore"
-git add README.md
-git commit -m "README"
-git add index.html
-git commit -m "index.html"
 git add doc
 git commit -m "Documents and auxiliary files"
 git add cardio
@@ -21,6 +15,12 @@ git commit -m "Notes and programs on TRYGGVE"
 git push
 }
 
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add index.html
+git commit -m "index.html"
 git add docs
 git commit -m "docs"
 git add mkdocs.yml
