ef9f39846df48d523f5873b9923bf71ab6d6a7a5
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-06-24 20:59:10 +0000

    Deployed f2297f3 with MkDocs version: 1.1.2

--- doc/st.sh
+++ doc/st.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/bash
 
-git add .gitignore
-git commit -m ".gitignore"
-git add README.md
-git commit -m "README"
-git add index.html
-git commit -m "index.html"
+function nonweb ()
+{
 git add doc
 git commit -m "Documents and auxiliary files"
 git add cardio
@@ -16,4 +12,20 @@ git add rsid
 git commit -m "Final work on CSD3 with rsids in the downstream analysis"
 git add tryggve
 git commit -m "Notes and programs on TRYGGVE"
+git push
+}
+
+mkdocs build
+mkdocs gh-deploy
+
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add index.html
+git commit -m "index.html"
+git add docs
+git commit -m "docs"
+git add mkdocs.yml
+git commit -m "mkdocs.yml"
 git push
