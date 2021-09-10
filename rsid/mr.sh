1b2905cb1073899c56445e5c2740b5861caf1e99
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-09-10 12:36:34 +0000

    Deployed 2bd6923d with MkDocs version: 1.1.2

--- rsid/mr.sh
+++ rsid/mr.sh
@@ -253,8 +253,7 @@ function mrx()
         awk "{\$4=toupper(\$4);\$5=toupper(\$5);print}" ${INF}/mr/${suffix}/{2}-${suffix}.mri | \
         sort -k3,3 | \
         join -23 ${INF}/work/INTERVAL.rsid - | \
-        awk -v prot={2} "{\$1=prot;print}" | \
-        tr " " "\t"
+        awk -v prot={2} "{\$1=prot;print}" | \        tr " " "\t"
       ) | gzip -f > ${INF}/mr/gsmr/mrx/{2}-${suffix}.mrx
     '
 }
@@ -295,7 +294,8 @@ function ref_prot_outcome_gsmr()
         echo -e "SNP A1 A2 freq b se p N"
         bcftools query -f "%CHROM %POS %ID %ALT %REF [%AF] [%ES] [%SE] [%LP] [$N] \n" -r {3} ${INF}/OpenGWAS/${efo}.vcf.gz | \
         awk "{
-          if(\$4<\$5) snpid=\"chr\"\$1\":\"\$2\"_\"\$4\"_\"\$5; else snpid=\"chr\"\$1\":\"\$2"_"\$4"_"\$5
+          if(\$4<\$5) snpid=\"chr\"\$1\":\"\$2\"_\"\$4\"_\"\$5;
+                 else snpid=\"chr\"\$1\":\"\$2\"_\"\$5\"_\"\$4
           \$7=10^-\$7
           print snpid, \$4, \$5, \$6, \$7, \$8, \$9, \$10
         }"
@@ -440,53 +440,77 @@ function exposure()
 
 --- specific runs ---
 
-if [ ! -d ${INF}/mr/RA ]; then mkdir ${INF}/mr/RA; fi
+for dir in out trait; do if [ ! -d ${INF}/mr/GWAS/${dir} ]; then mkdir -p ${INF}/mr/GWAS/${dir}; fi; done
 export M=1e6
-cut -f3 ${INF}/work/INF1.METAL | sed '1d' | sort | uniq | grep -w -f - ${INF}/work/INF1.merge.genes | \
-parallel -C' ' --env INF --env M '
-(
-  echo SNP A1 A2 freq b se p N
-  gunzip -c ${INF}/OpenGWAS/Ha_et_al_2020_RA_Trans.txt.gz | \
-  awk -v chr={3} -v start={4} -v end={5} -v M=${M} "
-  {
-    if (NR>1)
+function RA()
+{
+  cut -f3 ${INF}/work/INF1.METAL | sed '1d' | sort | uniq | grep -w -f - ${INF}/work/INF1.merge.genes | \
+  parallel -C' ' --env INF --env M '
+  (
+    echo SNP A1 A2 freq b se p N
+    gunzip -c ${INF}/OpenGWAS/Ha_et_al_2020_RA_Trans.txt.gz | \
+    awk -v chr={3} -v start={4} -v end={5} -v M=${M} "
     {
-      split(toupper(\$1),a,\":\");
-      a1=toupper(\$2)
-      a2=toupper(\$3)
-      if(a1<a2) snpid=\"chr\" chr \":\" a[2] \"_\" a1 \"_\" a2;
-      else snpid=\"chr\" chr \":\" a[2] \"_\" a2 \"_\" a1;
-      if(a[1]==chr && a[2] >= start-M && a[2] < end+M) print a[1], a[2], snpid, a1, a2, \$4, \$5, \$6, \$7, \$8
-    }
-  }" | sort -k1,1n -k2,2n | cut -d" " -f1-2 --complement
-) | gzip -f > ${INF}/mr/RA/{2}.gz
+      if (NR>1)
+      {
+        split(toupper(\$1),a,\":\");
+        a1=toupper(\$2)
+        a2=toupper(\$3)
+        if(a1<a2) snpid=\"chr\" chr \":\" a[2] \"_\" a1 \"_\" a2;
+        else snpid=\"chr\" chr \":\" a[2] \"_\" a2 \"_\" a1;
+        if(a[1]==chr && a[2] >= start-M && a[2] < end+M) print a[1], a[2], snpid, a1, a2, \$4, \$5, \$6, \$7, \$8
+      }
+    }" | sort -k1,1n -k2,2n | cut -d" " -f1-2 --complement
+  ) | gzip -f > ${INF}/mr/GWAS/trait/${trait}-{2}.gz
 '
+}
+export trait=RA
+RA
+function ukb_b_9125()
+{
+    awk '$21=="cis" {print $3}' ${INF}/work/INF1.METAL | sort | uniq | grep -w -f - ${INF}/work/INF1.merge.genes | \
+    awk -vM=1e6 "{print \$1, \$2, \$3\":\"\$4-M\"-\"\$5+M}" | \
+    parallel -C' ' -j15 --env INF --env efo --env N --env suffix '
+      (
+        echo -e "SNP A1 A2 freq b se p N"
+        bcftools query -f "%CHROM %POS %ID %ALT %REF [%AF] [%ES] [%SE] [%LP] [$N] \n" -r {3} ${INF}/OpenGWAS/${trait}.vcf.gz | \
+        awk "{
+          if(\$4<\$5) snpid=\"chr\"\$1\":\"\$2\"_\"\$4\"_\"\$5;
+                 else snpid=\"chr\"\$1\":\"\$2\"_\"\$5\"_\"\$4
+          \$7=10^-\$7
+          print snpid, \$4, \$5, \$6, \$7, \$8, \$9, \$10
+        }"
+      ) | \
+      gzip -f> ${INF}/mr/GWAS/trait/${trait}-{2}.gz
+    '
+}
+export trait=ukb-b-9125
+export N=462933
+ukb_b_9125
 # Q9P0M4 IL.17C 16 88704999 88706881
 # MarkerName      Allele1 Allele2 Freq1   Effect  StdErr  P-value TotalSampleSize
 # 5:29439275      t       c       0.4862  -0.0085 0.0145  0.5596  291180
-if [ ! -d ${INF}/mr/RA/out ]; then mkdir ${INF}/mr/RA/out; fi
 for i in {1..70}; do
-    export trait=RA
     export i=${i}
     awk '$21=="cis" {print $3}' ${INF}/work/INF1.METAL | sort | uniq | grep -w -f - ${INF}/work/INF1.merge.genes | \
     awk 'NR==ENVIRON["i"]{print $2}' | \
     while read prot; do
       export prot=${prot}
-      echo ${trait} ${INF}/mr/RA/${prot}.gz > ${INF}/mr/RA/gsmr_${prot}
+      echo ${trait} ${INF}/mr/GWAS/trait/${trait}-${prot}.gz > ${INF}/mr/GWAS/trait/gsmr_${trait}_${prot}
       gcta-1.9 --mbfile ${INF}/mr/gsmr/ref/gsmr_${prot} \
-               --gsmr-file ${INF}/mr/gsmr/prot/gsmr_${prot} ${INF}/mr/RA/gsmr_${prot} \
+               --gsmr-file ${INF}/mr/gsmr/prot/gsmr_${prot} ${INF}/mr/GWAS/trait/gsmr_${trait}_${prot} \
                --gsmr-direction 0 \
-               --clump-r2 0.1 --gwas-thresh 5e-8 --diff-freq 0.4 --heidi-thresh 0.05 --gsmr-snp-min 15 --effect-plot \
-               --out ${INF}/mr/RA/out/${prot}
+               --clump-r2 0.1 --gwas-thresh 5e-8 --diff-freq 0.4 --heidi-thresh 0.05 --gsmr-snp-min 10 --effect-plot \
+               --out ${INF}/mr/GWAS/out/${trait}-${prot}
 
       R --no-save -q <<\ \ \ \ \ \ END
         INF <- Sys.getenv("INF")
         trait <- Sys.getenv("trait")
         p <- Sys.getenv("prot")
         source(file.path(INF,"csd3","gsmr_plot.r"))
-        gsmr_data <- read_gsmr_data(paste0(INF,"/mr/RA/out/",p,".eff_plot.gz"))
+        gsmr_data <- read_gsmr_data(paste0(INF,"/mr/GWAS/out/",trait,"-",p,".eff_plot.gz"))
         gsmr_summary(gsmr_data)
-        pdf(paste0(INF,"/mr/RA/out/",p,".eff_plot.pdf"))
+        pdf(paste0(INF,"/mr/GWAS/out/",trait,"-",p,".eff_plot.pdf"))
         plot_gsmr_effect(gsmr_data, p, trait, colors()[75])
         dev.off()
       END
