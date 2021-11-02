3bfe9682889a4ec46c786eefa34da8adc6431484
Jing Hua Zhao <jinghuazhao@hotmail.com>
2021-11-02 20:54:19 +0000

    docs

--- docs/rsid/LTBR.R
+++ docs/rsid/LTBR.R
@@ -7,14 +7,16 @@ LTBR <- function(xy,out="p2d.png")
   points(data[[3]]~x,col="orange",pch=17)
   points(data[[4]]~x,col="red",pch=19)
   title("MS-LTBR-TNFB")
-  legend("topleft", legend = c("MS","LTBR","TNFB"), col = c("blue","orange","red"),
-  pch = c(15,17,19),
-  bty = "n",
-  pt.cex = 2,
-  cex = 1.2,
-  text.col = "black",
-  horiz = FALSE,
-  inset = c(0.1, 0.1))
+  legend("topleft",
+         legend = c("MS","LTBR","TNFB"),
+         col = c("blue","orange","red"),
+         pch = c(15,17,19),
+         bty = "n",
+         pt.cex = 2,
+         cex = 1.2,
+         text.col = "black",
+         horiz = FALSE,
+         inset = c(0.1, 0.1))
   axis(1,at=x,tick=TRUE)
   axis(2)
   dev.off()
