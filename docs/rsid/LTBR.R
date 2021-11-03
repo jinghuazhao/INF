7cfe974f59bec0bf180805b0c9b29b114c287766
Jing Hua Zhao <jinghuazhao@hotmail.com>
2021-11-03 10:43:08 +0000

    docs

--- docs/rsid/LTBR.R
+++ docs/rsid/LTBR.R
@@ -1,10 +1,10 @@
-LTBR <- function(xy,out="p2d.png")
+LTBR <- function(b,se,out="p2d.png")
 {
-  data <- data.frame(xy)
-  x <- data[[1]]
   png(out,res=300,width=18,height=10,units="in")
   par(mfrow=c(2,1))
-  plot(data[[2]]~x,ylim=c(-0.5,0.5),col="blue",xlab="Position",ylab="effect size",pch=15,axes=FALSE)
+  data <- data.frame(b)
+  x <- data[[1]]
+  plot(data[[2]]~x,ylim=c(-0.5,0.5),col="blue",xlab="Position",ylab="Effect size",pch=15,axes=FALSE)
   points(data[[3]]~x,col="orange",pch=17)
   points(data[[4]]~x,col="red",pch=19)
   title("MS-LTBR-TNFB")
@@ -19,9 +19,11 @@ LTBR <- function(xy,out="p2d.png")
          horiz = FALSE,
          inset = c(0.1, 0.1))
   axis(2)
-  plot(data[[2]]~x,ylim=c(-0.5,0.5),col="blue",type="b",xlab="Position",ylab="effect size",pch=15,axes=FALSE)
-  lines(data[[3]]~x,col="orange",pch=17)
-  lines(data[[4]]~x,col="red",pch=19)
+  data <- data.frame(se)
+  x <- data[[1]]
+  plot(data[[2]]~x,ylim=c(-0.5,0.5),col="blue",xlab="Position",ylab="Standard error",pch=15,axes=FALSE)
+  points(data[[3]]~x,col="orange",pch=17)
+  points(data[[4]]~x,col="red",pch=19)
   axis(1,at=x,tick=TRUE)
   axis(2)
   dev.off()
