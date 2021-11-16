dd6ee30bbc4ff90927b57620fd10d805d9c6c627
Jing Hua Zhao <jinghuazhao@hotmail.com>
2021-11-16 22:06:53 +0000

    Deployed 49cbb78a with MkDocs version: 1.1.2

--- csd3/hotspot.sh
+++ csd3/hotspot.sh
@@ -1,57 +1,40 @@
-# 18-10-2019 JHZ
+# 16-11-2021 JHZ
 
 export HOTSPOT=$1
-export cvt=work/INF1.merge.cis.vs.trans
+export cvt=${INF}/work/INF1.merge.cis.vs.trans
 
-(
-  grep -w ${HOTSPOT} ${cvt} | \
-  awk -v OFS="\t" '{print "chr" $3,$4-1, $4}'
-) > a1
+bedtools intersect -a <(grep -w ${HOTSPOT} ${cvt} | awk -v FS="," -v OFS="\t" '{print "chr" $3,$4-1, $4}') \
+                   -b <(sort -k1,1n -k2,2n ${INF}/csd3/glist-hg19 | grep -v -w -e X -e Y -e XY | awk '{$1="chr" $1;print}' | sed 's/ /\t/g') \
+                   -wa -wb -loj | \
+cut -f1-3,7 > hotspot.txt
 
-(
-  sort -k1,1n -k2,2n csd3/glist-hg19 | \
-  grep -v X | \
-  grep -v Y | \
-  awk '{$1="chr" $1;print}' | \
-  sed 's/ /\t/g'
-) > a2
-
-bedtools intersect -a a1 -b a2 -wa -wb -loj | \
-cut  -f1-3,7 > a
-
-R --no-save -q <<END
+Rscript -e '
+  library(dplyr)
+  INF <- Sys.getenv("INF")
   HOTSPOT <- Sys.getenv("HOTSPOT")
-  cvt <- Sys.getenv("cvt")
-  cvt <- subset(read.table(cvt,as.is=TRUE,header=TRUE), SNP==HOTSPOT)
-  cvt <- within(cvt,p.chr <- paste0("chr",p.chr))
-  b <- cvt[c("p.chr","p.start","p.end","p.gene","cis.trans")]
-  names(b)=c("chr","start","end", "gene", "cistrans")
-  cols <- rep(10,nrow(b))
-  d <- read.table("a",as.is=TRUE,col.names=c("chr","start","end", "gene"))
-  a <- aggregate(d,by=list(with(d,chr),with(d,start),with(d,end)),FUN="paste")[-c(4:6)]
-# chr12:11058117_C_T
-# chr17:34326215_A_C
-# if (class(a[,4]) != "matrix") {
-#   a <- d
-#   cols[b["cistrans"]=="cis"] <- 12
-# } else {
-#   a[,4] <- apply(a[,4],1,"paste",collapse=",")
-#   names(a) <- c("chr","start","end","gene")
-# }
-# chr19:49206145_C_G
-  a <- d
-  labels <- rbind(b,data.frame(unique(a),cistrans="."))
-  library(circlize)
-  pdf(paste0(HOTSPOT,".pdf"))
+  b <- read.csv(Sys.getenv("cvt"),as.is=TRUE) %>% filter(SNP==HOTSPOT) %>%
+       mutate(p.chr=paste0("chr",p.chr)) %>% rename(chr=p.chr,start=p.start,end=p.end,gene=p.gene,cistrans=cis.trans)
+  cols <- rep(12,nrow(b))
+  cols[b[["cis"]]] <- 10
+  d <- read.table("hotspot.txt",as.is=TRUE,col.names=c("chr","start","end", "gene"))
+  u <- unique(d)
+  a <- aggregate(u,by=list(with(u,chr),with(u,start),with(u,end)),FUN="paste",collapse=" ") %>% select(-c(chr,start,end))
+  names(a) <- c("chr","start","end", "gene")
+  labels <- bind_rows(select(b,chr,start,end,gene),a)
+  suppressMessages(library(circlize))
+  pdf(file.path(INF,"hotspots",paste0("hotspot-",HOTSPOT,".pdf")))
   circos.par(start.degree = 90, track.height = 0.1, cell.padding = c(0, 0, 0, 0))
   circos.initializeWithIdeogram(species="hg19", track.height = 0.05, ideogram.height = 0.06)
   circos.genomicLabels(labels,labels.column = 4, side="inside")
-  circos.genomicLink(a, b, col = cols, directional=1, border = 10, lwd = 2)
+  circos.genomicLink(d, b[c("chr","start","end")], col = cols, directional=1, border = 10, lwd = 2)
   circos.clear()
   dev.off()
-END
+  unlink("hotspot.txt")
+'
 
-pdftopng -r 300 ${HOTSPOT}.pdf ${HOTSPOT}
-mv ${HOTSPOT}-000001.png ${HOTSPOT}.png
+pdftopng -r 300 ${INF}/hotspots/hotspot-${HOTSPOT}.pdf ${HOTSPOT}
+mv ${HOTSPOT}-000001.png ${INF}/hotspots/hotspot-${HOTSPOT}.png
 
-rm a1 a2 a
+# chr12:11058117_C_T
+# chr17:34326215_A_C
+# chr19:49206145_C_G
