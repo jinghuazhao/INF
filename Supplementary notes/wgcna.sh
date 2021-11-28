f4bd7acf429fb919d8c9bb06029b7e00c130f8be
Jing Hua Zhao <jinghuazhao@hotmail.com>
2021-11-28 18:07:49 +0000

    Deployed 0f85fa0e with MkDocs version: 1.1.2

--- Supplementary notes/wgcna.sh
+++ Supplementary notes/wgcna.sh
@@ -14,19 +14,19 @@ Rscript -e '
   prot <- select(prot,gsub("4E","X4E",unique(pqtl[["prot"]])))
   require(WGCNA)
   enableWGCNAThreads()
-# Ajacency matrix using soft thresholding with beta=6
+# Adjacency matrix using soft thresholding with beta=6
   ADJ <- abs(cor(prot, method="pearson"))^6
 # genes < 5,000
   k <- as.vector(apply(ADJ,2,sum,na.rm=TRUE))
 # genes > 5,000
-# k <- softConnectivity(datE=out,power=6)
+# k <- softConnectivity(datE=prot,power=6)
+# network analysis on 70 most connected genes
+# prot[,rank(-k,ties.method="first") <= 70]
 # histogram of k and a scale free topology plot
   sizeGrWindow(10,5)
   par(mfrow=c(1,2))
   hist(k)
   scaleFreePlot(k, main="Check scale free topology\n")
-# network analysis on 70 most connected genes
-  datExpr=prot[,rank(-k,ties.method="first") <= 70]
 # dissimilarity Topological Overlap Matrix
   dissADJ <- 1 - ADJ
   dissTOM <- TOMdist(ADJ)
@@ -43,33 +43,32 @@ Rscript -e '
   table(pam5$clustering,pamTOM5$clustering)
   table(pam5$clustering,pamTOM6$clustering)
 # average linkage hierachical clusterin
+# ADJ
   hierADJ <- hclust(as.dist(dissADJ),method="average")
-# dendrogram
+  colorStaticADJ <- as.character(cutreeStaticColor(hierADJ,cutHeight=.99,minSize=5))
+  colorDynamicADJ <- labels2colors(cutreeDynamic(hierADJ,method="tree",minClusterSize=5))
+  colorDynamicHybridADJ <- labels2colors(cutreeDynamic(hierADJ,distM=dissADJ,cutHeight=0.998,
+                                         deepSplit=2,pamRespectsDendro=FALSE))
+  colorADJ <- data.frame(pam5$clustering,colorStaticADJ,colorDynamicADJ,colorDynamicHybridADJ)
   sizeGrWindow(10,5)
-  plotDendroAndColors(hierADJ,colors=pam4$clustering,dendroLabels=FALSE,hang=0.03)
-  title("Gene hierarchical clustering dendrogram")
-  colorStaticADJ <- as.character(cutreeStaticColor(hierADJ,cutHeight=.99,minSize=10))
-# dendrogram with module colors
-  sizeGrWindow(10,5)
-  plotDendroAndColors(hierADJ,colors=data.frame(pam4$clustering,colorStaticADJ),dendroLabels=FALSE,abHeight=0.99,
+  plotDendroAndColors(dendro=hierADJ,colors=colorADJ,
+                      dendroLabels=FALSE,
+                      marAll=c(0.2,8,2.7,0.2),
                       main="Gene dendrogram and module colors")
-# module definition via dynamic branch cutting methods
-  branch.number <- cutreeDynamic(hierADJ,method="tree")
-# transforms the branch numbers into colors
-  colorDynamicADJ <- labels2colors(branch.number)
-  colorDynamicHybridADJ <- labels2colors(cutreeDynamic(hierADJ,distM=dissADJ,cutHeight=0.998,deepSplit=2,pamRespectsDendro=FALSE))
-# all module detection methods together
-  sizeGrWindow(10,5)
-  plotDendroAndColors(dendro=hierADJ,colors=data.frame(pam4$clustering,colorStaticADJ,colorDynamicADJ,colorDynamicHybridADJ),
-                      dendroLabels=FALSE,marAll=c(0.2,8,2.7,0.2),main="Gene dendrogram and module colors")
-# dendrogram
+# TOM
   hierTOM <- hclust(as.dist(dissTOM),method="average");
-  colorStaticTOM <- as.character(cutreeStaticColor(hierTOM,cutHeight=.99,minSize=20))
-  colorDynamicTOM <- labels2colors(cutreeDynamic(hierTOM,method="tree"))
+  colorStaticTOM <- as.character(cutreeStaticColor(hierTOM,cutHeight=.99,minSize=5))
+  colorDynamicTOM <- labels2colors(cutreeDynamic(hierTOM,method="tree",minClusterSize=5))
   colorDynamicHybridTOM <- labels2colors(cutreeDynamic(hierTOM,distM=dissTOM,cutHeight=0.998,
-                                         deepSplit=2,pamRespectsDendro = FALSE))
+                                         deepSplit=2,pamRespectsDendro=FALSE))
+  colorTOM <- data.frame(pamTOM5$clustering,colorStaticTOM,colorDynamicTOM,colorDynamicHybridTOM)
   sizeGrWindow(10,5)
-  plotDendroAndColors(hierTOM,colors=data.frame(hierTOM$labels,colorStaticTOM,colorDynamicTOM,colorDynamicHybridTOM),
-                      dendroLabels=FALSE, marAll=c(1,8,3,1),
+  plotDendroAndColors(hierTOM,colors=colorTOM,
+                      dendroLabels=FALSE,
+                      marAll=c(1,8,3,1),
                       main="Gene dendrogram and module colors, TOM dissimilarity")
+  options(width=200)
+  colorADJTOM <- cbind(colorADJ,colorTOM)
+  table(colorADJTOM$colorStaticADJ)
+  for(col in c("blue","brown","grey","turquoise")) print(subset(colorADJTOM,colorStaticADJ==col))
 '
