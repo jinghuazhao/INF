3c7a27b2d000c9cde99ef5123e3e0e4fbe23752f
Jing Hua Zhao <jinghuazhao@hotmail.com>
2021-12-09 11:26:49 +0000

    Supplementary notes

--- rsid/deCODE.sh
+++ rsid/deCODE.sh
@@ -17,40 +17,60 @@ END
 }
 
 function replication()
-(
-  gunzip -c ${proteomics_results}/2155/10000_28_CRYBB2_CRBB2.txt.gz | head -1 | awk -vOFS="\t" '{print "prot",$0}'
-  join -13 -22 <(cut -f1,4,7 --output-delimiter=' ' ${INF}/deCODE/SomaLogicv4.tsv | sort -k3,3) \
-               <(cut -f2,4,5,20 --output-delimiter=' ' ${INF}/work/INF1.METAL | awk '{print $2":"$3,$4,$1}' | sort -k2,2) | \
-  parallel -C' ' -j20 '
-    tabix ${INF}/deCODE/{2}*.gz {4} | grep -w {5} | awk -vOFS="\t" -vprot={2} "{print prot,\$0}"
+{
+  (
+    gunzip -c ${proteomics_results}/2155/10000_28_CRYBB2_CRBB2.txt.gz | head -1 | awk -vOFS="\t" '{print "prot",$0}'
+    join -13 -22 <(cut -f1,4,7 --output-delimiter=' ' ${INF}/deCODE/SomaLogicv4.tsv | sort -k3,3) \
+                 <(cut -f2,4,5,20 --output-delimiter=' ' ${INF}/work/INF1.METAL | awk '{print $2":"$3,$4,$1}' | sort -k2,2) | \
+    parallel -C' ' -j20 '
+      tabix ${INF}/deCODE/{2}*.gz {4} | grep -w {5} | awk -vOFS="\t" -vprot={2} "{print prot,\$0}"
+    '
+  ) > ${INF}/deCODE/replication38.tsv
+  Rscript -e '
+    INF <- Sys.getenv("INF")
+    suppressMessages(library(dplyr))
+    rep38 <- read.delim(file.path(INF,"deCODE","replication38.tsv")) %>%
+             mutate(seqname=Chrom,start=as.integer(Pos-1),end=Pos) %>%
+                    arrange(Chrom,Pos)
+    gr <- with(rep38,GenomicRanges::GRanges(seqnames=seqname,IRanges::IRanges(start,end,names=Name))) %>% unique()
+    suppressMessages(library(rtracklayer))
+    path <- system.file(package="liftOver", "extdata", "hg38ToHg19.over.chain")
+    ch <- import.chain(path)
+    seqlevelsStyle(gr) <- "UCSC"
+    gr19 <- liftOver(gr,ch)
+    df <- mutate(data.frame(gr),group_name=names(gr)) %>%
+          left_join(data.frame(gr19),by=c("group_name")) %>%
+          mutate(chr=seqnames.x,pos37=end.y,Name=group_name) %>%
+          select(Name,chr,pos37,group) %>%
+          full_join(rep38) %>%
+          mutate(snpid=if_else(effectAllele>otherAllele,paste0(chr,":",pos37,"_",otherAllele,"_",effectAllele),
+                                                        paste0(chr,":",pos37,"_",effectAllele,"_",otherAllele)),
+                 pval=gap::pvalue(Beta/SE)) %>%
+          select(snpid,prot,pval)
+    write.table(df,file=file.path(INF,"deCODE","replication.tsv"),row.names=FALSE,col.names=FALSE,quote=FALSE,sep="\t")
   '
-) > ${INF}/deCODE/replication.tsv
+}
 
 function deCODE()
 (
-echo -e "Protein\tSentinels\tUniProt\tSNPid\tcis/trans\tProxies\tr2\tp\tTarget Full Name\tSource\tPMID\tComment"
-join -12 -21 <(awk 'NR>1 && $9<=5e-8 {
-                    split($4,a,":")
-                    if(a[3]<a[4]) snpid=a[1]":"a[2]"_"a[3]"_"a[4];
-                    else snpid==a[1]":"a[2]"_"a[4]"_"a[3]
-                    print snpid, $1, $9
-                 }' ${INF}/deCODE/replication.tsv | sort -k2,2) \
-             <(cut -f1,7 --output-delimiter=' ' ${INF}/deCODE/${v4} | sort -k1,1) | \
-awk '{print $4"-"$2,$0}' | \
-sort -k1,1 | \
-join - <(awk 'NR>1 {print $20"-"$1,$3,$2,$21}' ${INF}/work/INF1.METAL | sort -k1,1) | \
-awk 'a[$1]++==0' | \
-cut -d' ' -f1,2 --complement | \
-sort -k4,4 | \
-tr ' ' '\t'| \
-join -24 <(Rscript -e 'write.table(pQTLtools::inf1[c("prot","target.short")],col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t")' | \
-           sort -t$'\t' -k1,1) - | \
-awk -vOFS='\t' '{print $2,$6,$5,$3,$7,"as sentinels",1,$4}' | \
-sort -t$'\t' -k1,1 | \
-join -t$'\t' - \
-             <(Rscript -e 'write.table(data.frame(pQTLtools::inf1[c("target.short","target")],Source="Ferkingstad et al. (2021)",PMID="",Comment=""),
-                                       col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t")' | \
-               sort -t$'\t' -k1,1)
+  echo -e "Protein\tSentinels\tUniProt\tSNPid\tcis/trans\tProxies\tr2\tp\tTarget Full Name\tSource\tPMID\tComment"
+  join -12 -21 <(awk '$3<5e-8' ${INF}/deCODE/replication.tsv | sort -k2,2) \
+               <(cut -f1,7 --output-delimiter=' ' ${INF}/deCODE/${v4} | sort -k1,1) | \
+  awk '{print $4"-"$2,$0}' | \
+  sort -k1,1 | \
+  join - <(awk 'NR>1 {print $20"-"$1,$3,$2,$21}' ${INF}/work/INF1.METAL | sort -k1,1) | \
+  awk 'a[$1]++==0' | \
+  cut -d' ' -f1,2 --complement | \
+  sort -k4,4 | \
+  tr ' ' '\t'| \
+  join -24 <(Rscript -e 'write.table(pQTLtools::inf1[c("prot","target.short")],col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t")' | \
+             sort -t$'\t' -k1,1) - | \
+  awk -vOFS='\t' '{print $2,$6,$5,$3,$7,"as sentinels",1,$4}' | \
+  sort -t$'\t' -k1,1 | \
+  join -t$'\t' - \
+               <(Rscript -e 'write.table(data.frame(pQTLtools::inf1[c("target.short","target")],Source="Ferkingstad et al. (2021)",PMID="",Comment=""),
+                                         col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t")' | \
+                 sort -t$'\t' -k1,1)
 )
 
 deCODE > ${INF}/deCODE/deCODE.tsv
