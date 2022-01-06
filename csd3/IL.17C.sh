04a05530c47ba384985b9525cfa10214e0472e90
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-01-06 20:14:31 +0000

    Deployed 87e8ef5f with MkDocs version: 1.1.2

--- csd3/IL.17C.sh
+++ csd3/IL.17C.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/bash
 
+#SBATCH --job-name=IL.17C
+#SBATCH --account CARDIO-SL0-CPU
+#SBATCH --partition cardio
+#SBATCH --qos=cardio
+#SBATCH --time=5-00:00:00
+#SBATCH --output=/rds/user/jhz22/hpc-work/work/%A_%a.out
+#SBATCH --error=/rds/user/jhz22/hpc-work/work/%A_%a.err
+#SBATCH --export ALL
+
+function setup()
 # IL.17C
+{
 tabix sumstats/INTERVAL/INTERVAL.TRAIL.gz 16 | sort -k11,11gr | awk '$11!="NA"' | tail
 # chr16:85703851_C_G rs530452211 GSE1
 # chr16:54261627_A_G rs12933399 RP11-324D17.1
@@ -15,17 +26,19 @@ vep --id "16 61477410 61477410 G/GTA" --species homo_sapiens --assembly GRCh37 -
                                       --nearest symbol --pick
 vep --id "16 88684495 88684495 G/T" --species homo_sapiens --assembly GRCh37 -o rs17700884 --cache --offline --force_overwrite --tab \
                                     --nearest symbol --pick
+}
 
 Rscript -e '
   suppressMessages(library(dplyr))
   suppressMessages(library(gap))
   INF <- Sys.getenv("INF")
-  gz <- gzfile(file.path(INF,"sumstats","INTERVAL","INTERVAL.IL.17C.gz"))
-  IL.17C <- within(read.delim(gz,as.is=TRUE), {Z <- BETA/SE; P <- pvalue(Z); log10P <- -log10p(Z)}) %>%
-            rename(Chromosome=CHR,Position=POS,MarkerName=SNPID) %>%
-            select(Chromosome,Position,MarkerName,Z,P,log10P)
   genes <- data.frame(chr=c("chr16"), snpid=c("chr16:88684495_G_T"), snp=c("rs17700884"), gene=c("IL17C"))
-  IL.17C <- left_join(IL.17C,genes,by=c("MarkerName"="snpid"),keep=TRUE) %>%
+  gz <- gzfile(file.path(INF,"sumstats","INTERVAL","INTERVAL.IL.17C.gz"))
+  IL.17C <- read.delim(gz,as.is=TRUE) %>%
+            mutate(Z=BETA/SE,P=pvalue(Z),log10P=-log10p(Z)) %>%
+            rename(Chromosome=CHR,Position=POS,MarkerName=SNPID) %>%
+            select(Chromosome,Position,MarkerName,Z,P,log10P) %>%
+            left_join(genes,by=c("MarkerName"="snpid"),keep=TRUE) %>%
             mutate(MarkerName=ifelse(!is.na(snpid),gene,MarkerName)) %>%
             select(-c(chr,snpid,snp))
   save(IL.17C, genes, file=file.path(INF,"work","IL.17C.rda"))
