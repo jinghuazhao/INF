e9863b5367cf2723ae407f79a6a2a93d56f8046d
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-01-08 14:17:37 +0000

    Deployed 98d4d36b with MkDocs version: 1.1.2

--- csd3/IL.17C.sh
+++ csd3/IL.17C.sh
@@ -12,27 +12,28 @@
 function setup()
 # IL.17C
 {
-tabix sumstats/INTERVAL/INTERVAL.TRAIL.gz 16 | sort -k11,11gr | awk '$11!="NA"' | tail
-# chr16:85703851_C_G rs530452211 GSE1
-# chr16:54261627_A_G rs12933399 RP11-324D17.1
-# chr16:61477410_G_GTA rs567557033 CDH8
-# chr16:88684495_G_T rs17700884 
-
-vep --id "16 85703851 85703851 C/G" --species homo_sapiens --assembly GRCh37 -o rs530452211 --cache --offline --force_overwrite --tab \
-                                    --nearest symbol --pick
-vep --id "16 54261627 54261627 A/G" --species homo_sapiens --assembly GRCh37 -o rs12933399 --cache --offline --force_overwrite --tab \
-                                    --nearest symbol --pick
-vep --id "16 61477410 61477410 G/GTA" --species homo_sapiens --assembly GRCh37 -o rs567557033 --cache --offline --force_overwrite --tab \
-                                      --nearest symbol --pick
-vep --id "16 88684495 88684495 G/T" --species homo_sapiens --assembly GRCh37 -o rs17700884 --cache --offline --force_overwrite --tab \
-                                    --nearest symbol --pick
+tabix sumstats/INTERVAL/INTERVAL.IL.17C.gz 16 | sort -k11,11gr | awk '$11!="NA"' | tail
+(
+cat <<END
+chr16:88684495_G_T rs17700884
+chr16:88695338_C_G rs4782388
+chr16:88692206_A_ACGC 16:88692206_A_ACGC
+chr16:88692204_T_TGAC rs71391399
+chr16:88692203_T_TTGACGA rs559941365
+END
+) | \
+sed 's/chr//;s/:/ /;s/_/ /g' | \
+awk '{print $1,$2,$2,$3"/"$4,$5}' | \
+parallel -C' ' '
+vep --id "{1} {2} {3} {4}" -o {5} --species homo_sapiens --assembly GRCh37 --cache --offline --force_overwrite --tab --nearest symbol --pick
+'
 gunzip -c ${INF}/sumstats/INTERVAL/INTERVAL.IL.17C.gz | cut -f1-3,9-10 | gzip -f > ${INF}/work/IL.17C.gz
 
 Rscript -e '
   suppressMessages(library(dplyr))
   suppressMessages(library(gap))
   INF <- Sys.getenv("INF")
-  genes <- data.frame(MarkerName=c("chr16:88684495_G_T"),gene=c("IL17C"),color=c("red"))
+  genes <- data.frame(MarkerName=c("chr16:88692203_T_TTGACGA"),gene=c("IL17C"),color=c("red"))
   gz <- gzfile(file.path(INF,"work","IL.17C.gz"))
   IL.17C <- read.delim(gz,as.is=TRUE) %>%
             mutate(Z=BETA/SE,P=pvalue(Z),log10P=-log10p(Z)) %>%
@@ -40,43 +41,6 @@ Rscript -e '
             select(Chromosome,Position,MarkerName,Z,P,log10P) %>%
             left_join(genes)
   save(IL.17C, genes, file=file.path(INF,"work","IL.17C.rda"))
-'
-}
-
-Rscript -e '
-  suppressMessages(library(dplyr))
-  suppressMessages(library(gap))
-  INF <- Sys.getenv("INF")
-  load(file.path(INF,"work","IL.17C.rda"))
-  subset(IL.17C,!is.na(gene))
-  png("IL.17C-mhtplot.trunc.png", res=300, units="in", width=9, height=6)
-  par(oma=c(0,0,0,0), mar=c(5,6.5,1,1))
-  mhtplot.trunc(subset(IL.17C,!is.na(Z),select=-color), chr="Chromosome", bp="Position", z="Z", snp="MarkerName",
-                suggestiveline=-log10(1e-7), genomewideline=-log10(5e-10),
-                cex.mtext=1.2, cex.text=0.7,
-                annotatelog10P=-log10(1.1e-6), annotateTop = FALSE, highlight=with(genes,gene),
-                mtext.line=3, y.brk1=0.1, y.brk2=0.5, trunc.yaxis=FALSE, delta=0.01, cex.axis=1.2,
-                cex=0.5, font=2, font.axis=1,
-                y.ax.space=20,
-                col = c("blue4", "skyblue")
-  )
-  dev.off()
-  mhtdata <- filter(IL.17C,!is.na(Z)) %>%
-             select(-MarkerName,-Z,-log10P) %>%
-             mutate(P=as.numeric(P))
-  cis <- with(mhtdata,Chromosome==16 & Position>=88704999-1e6 & Position<=88706881+1e6)
-  mhtdata[cis,"color"] <- "red"
-  subset(mhtdata,!is.na(gene))
-  png("IL.17C-mhtplot2.png", res=300, units="in", width=9, height=6)
-  opar <- par()
-  par(cex=0.5)
-  ops <- mht.control(colors=rep(c("blue4","skyblue"),11),srt=0,yline=2.5,xline=2)
-  hops <- hmht.control(data=subset(mhtdata,!is.na(gene)))
-  mhtplot2(mhtdata,ops,hops,xlab="",ylab="",srt=0, cex.axis=1.2)
-  axis(2,at=1:8)
-  title("")
-  par(opar)
-  dev.off()
 '
 
 R --no-save -q <<END
@@ -96,9 +60,44 @@ R --no-save -q <<END
   mhtplot.trunc(IL.17C, chr="Chromosome", bp="Position", p="P", snp="MarkerName", z = "Z",
                 suggestiveline=FALSE, genomewideline=-log10(5e-10), logp = TRUE,
                 cex.mtext=0.6, cex.text=0.7,
-                mtext.line=4, y.brk1=300, y.brk2=500, cex.axis=0.6, cex=0.5,
-                y.ax.space=20,
+                mtext.line=4, y.brk1=30, y.brk2=50, cex.axis=1.2, cex=0.5, y.ax.space=10,
                 col = c("blue4", "skyblue")
   )
   dev.off()
 END
+}
+
+Rscript -e '
+  suppressMessages(library(dplyr))
+  suppressMessages(library(gap))
+  INF <- Sys.getenv("INF")
+  load(file.path(INF,"work","IL.17C.rda"))
+  subset(IL.17C,!is.na(gene))
+  png("IL.17C-mhtplot.trunc.png", res=300, units="in", width=9, height=6)
+  par(oma=c(0,0,0,0), mar=c(5,6.5,1,1))
+  mhtplot.trunc(subset(IL.17C,!is.na(Z),select=-color), chr="Chromosome", bp="Position", z="Z", snp="MarkerName",
+                suggestiveline=-log10(1e-7), genomewideline=-log10(5e-10),
+                cex.mtext=1.2, cex.text=0.7,
+                annotatelog10P=-log10(6.35e-9), annotateTop = FALSE, highlight=with(genes,gene),
+                mtext.line=3, y.brk1=2, y.brk2=3, trunc.yaxis=FALSE, delta=0.1, cex.axis=1.2,
+                cex=0.5, font=2, font.axis=1, y.ax.space=1,
+                col = c("blue4", "skyblue")
+  )
+  dev.off()
+  mhtdata <- filter(IL.17C,!is.na(Z)) %>%
+             select(-MarkerName,-Z,-log10P) %>%
+             mutate(P=as.numeric(P))
+  cis <- with(mhtdata,Chromosome==16 & Position>=88704999-1e6 & Position<=88706881+1e6)
+  mhtdata[cis,"color"] <- seq(length(colors()))[colors()=="red"]
+  subset(mhtdata,!is.na(gene))
+  png("IL.17C-mhtplot.png", res=300, units="in", width=9, height=6)
+  opar <- par()
+  par(cex=0.5)
+  ops <- mht.control(colors=rep(c("blue4","skyblue"),11),srt=0,yline=2.5,xline=2)
+  hops <- hmht.control(data=subset(mhtdata,!is.na(gene)))
+  mhtplot2(mhtdata,ops,hops,xlab="",ylab="",srt=0, cex.axis=1.2)
+  axis(2,at=1:10)
+  title("")
+  par(opar)
+  dev.off()
+'
