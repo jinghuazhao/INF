d5dca439ef5950b1ad13e432e69a43169ecf4d96
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-08-31 15:49:14 +0100

    Supplementary notes

--- rsid/mr.sh
+++ rsid/mr.sh
@@ -381,8 +381,8 @@ R --no-save -q <<END
    attach(tnfb)
    png(file.path(INF,"mr","gsmr","out","TNFB.png"),height=8,width=15,units="in",res=300)
    requireNamespace("meta")
-   mg <- meta::metagen(Effect,StdErr,sprintf("%s",outcome),sm="OR",title="TNFB")
-   meta::forest(mg,colgap.forest.left = "0.5cm",fontsize=24,leftcols="studlab",leftlabs="Outcome",rightlabs="ci",plotwidth="5inch",
+   mg <- meta::metagen(Effect,StdErr,sprintf("%s",gsub("IGA","IgA",outcome)),sm="OR",title="TNFB")
+   meta::forest(mg,colgap.forest.left = "0.5cm",fontsize=24,leftcols="studlab",leftlabs="Outcome",rightlabs="ci",plotwidth="5inch",sortvar=Effect,
                 common=FALSE, random=FALSE, print.I2=FALSE, print.pval.Q=FALSE, print.tau2=FALSE,addrow=TRUE,backtransf=TRUE,spacing=1.6)
    with(mg,cat("prot =", p, "MarkerName =", m, "Q =", Q, "df =", df.Q, "p =", pval.Q,
                "I2 =", I2, "lower.I2 =", lower.I2, "upper.I2 =", upper.I2, "\n"))
