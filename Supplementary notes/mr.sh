1b3c47fbc2e9afc7f9f90953ccaa274b8d7e4e0f
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-09-23 13:45:35 +0000

    Deployed 3d2ef42d with MkDocs version: 1.1.2

--- Supplementary notes/mr.sh
+++ Supplementary notes/mr.sh
@@ -453,22 +453,27 @@ function mr_recollect()
     rt <- Sys.getenv("rt")
     IVW <- read.delim(file.path(rt,"mr-efo.mr")) %>%
            mutate(fdr=p.adjust(pval,method="fdr")) %>%
-           select(prot,id.outcome,b,se,pval,fdr,nsnp,disease)
+           left_join(select(pQTLtools::inf1,prot,gene)) %>%
+           select(gene,id.outcome,b,se,pval,fdr,nsnp,disease)
     cat(nrow(filter(IVW,fdr<=0.05)),"\n")
     write.table(IVW,file.path(rt,"mr-efo-mr.tsv"),quote=FALSE,row.names=FALSE,sep="\t")
     Heterogeneity <- read.delim(file.path(rt,"mr-efo.het")) %>%
                      mutate(fdr=p.adjust(Q_pval,method="fdr")) %>%
-                     select(prot,id.outcome,method,Q,Q_df,Q_pval,fdr,disease)
+                     left_join(select(pQTLtools::inf1,prot,gene)) %>%
+                     select(gene,id.outcome,method,Q,Q_df,Q_pval,fdr,disease)
     cat(nrow(filter(Heterogeneity,fdr<=0.05)),"\n")
     write.table(Heterogeneity,file.path(rt,"mr-efo-het.tsv"),quote=FALSE,row.names=FALSE,sep="\t")
-    GSMR <- read.delim(file.path(rt,"out","gsmr-efo.txt"))
+    GSMR <- read.delim(file.path(rt,"out","gsmr-efo.txt")) %>%
+            rename(target.short=protein) %>%
+            left_join(select(pQTLtools::inf1,target.short,gene)) %>%
+            select(gene,MRBASEID,trait,bxy,se,p,nsnp,fdr,Ncases,Ncontrols,id,uri,Zhengetal)
     xlsx <- file.path(rt,"gsmr-mr.xlsx")
     wb <- createWorkbook(xlsx)
     hs <- createStyle(textDecoration="BOLD", fontColour="#FFFFFF", fontSize=12, fontName="Arial Narrow", fgFill="#4F80BD")
     for (sheet in c("GSMR","IVW","Heterogeneity"))
     {
       addWorksheet(wb,sheet,zoom=150)
-      writeData(wb,sheet,"gsmr",xy=c(1,1),headerStyle=createStyle(textDecoration="BOLD",
+      writeData(wb,sheet,sheet,xy=c(1,1),headerStyle=createStyle(textDecoration="BOLD",
                 fontColour="#FFFFFF", fontSize=14, fontName="Arial Narrow", fgFill="#4F80BD"))
       body <- get(sheet)
       writeDataTable(wb, sheet, body, xy=c(1,2), headerStyle=hs, firstColumn=TRUE, tableStyle="TableStyleMedium2")
