c52cd042af2def00fd70e5e225e7c5f69d687d3e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-12-19 21:54:56 +0000

    Supplementary notes

--- rsid/rs12075-circos.R
+++ rsid/rs12075-circos.R
@@ -19,13 +19,7 @@ fetch_region <- function()
     print(subset(t,snpid=="chr1:159175354_A_G"))
     print(dim(t))
   }
-  f <- file.path(INF,"work","mono.tsv.gz")
-  mono <- seqminer::tabix.read.table(tabixFile = f, tabixRange = r, stringsAsFactors = FALSE)
-  names(mono) <- c("VARIANT","ID_dbSNP49","CHR","BP","REF","ALT","EFFECT_INT","SE_INT","MLOG10P_INT","ALT_FREQ_INT","INFO_INT")
-  mono <- rename(mono, snpid=VARIANT,rsid=ID_dbSNP49,chr=CHR,pos=BP,a1=ALT,a2=REF,b=EFFECT_INT,se=SE_INT,log10p=MLOG10P_INT,af=ALT_FREQ_INT) %>%
-          select(snpid, rsid, chr, pos, a2, a1, b, se, af, log10p) %>%
-          mutate(snpid=if_else(a1<a2,paste0("chr",chr,":",pos,"_",a1,"_",a2),paste0("chr",chr,":",pos,"_",a2,"_",a1)),log10p=-log10p)
-  wide <- right_join(d,mono)[grepl("chr|pos|snpid|rsid|a1|a2|af|b|se|log10p",names(d))] %>%
+  wide <- d[grepl("chr|pos|snpid|rsid|a1|a2|af|b|se|log10p",names(d))] %>%
           arrange(chr,pos)
   for(i in 1:6)
   {
@@ -36,10 +30,7 @@ fetch_region <- function()
     if(i==1) d <- t
     else d <- bind_rows(d,t)
   }
-  mono <- mutate(mono,track="Monocytes") %>%
-          select(-snpid,-rsid)
-  long <- bind_rows(d,mono[c("chr","pos","a1","a2","af","b","se","log10p","track")])
-  list(wide=wide,long=long)
+  list(wide=wide,long=d)
 }
 
 plot_region <- function(dat)
@@ -74,29 +65,9 @@ p <- c("MCP.1", "MCP.3", "MCP.2", "CCL11", "MCP.4", "CXCL6")
 dat <- fetch_region()
 wide <- with(dat,wide) %>% mutate(pos=pos/1e6)
 long <- with(dat,long) %>%
-        mutate(lcl=b-1.96*se,ucl=b+1.96*se,track=factor(track,levels=c(g,"Monocytes")),pos=pos/1e6)
+        mutate(lcl=b-1.96*se,ucl=b+1.96*se,track=factor(track,levels=g),pos=pos/1e6)
 region_coords <- c(first(wide$pos),last(wide$pos))
 l <- plot_region(long)
-ggsave("rs12075.png",l,height=20,width=10)
-
-rs12075 <- filter(long,pos==159.175354)
-f <- ggplot(data=rs12075, aes(y=1:7, x=b))+
-     geom_point(size=2)+
-     geom_errorbarh(aes(xmax = ucl, xmin = lcl, height=0.001))+
-     scale_x_continuous(name="Effect size")+
-     scale_y_continuous(breaks=1:7,label=with(rs12075,track),name="",trans="reverse")+
-     geom_vline(xintercept=0, color="black", linetype="dashed", alpha=.5)+
-     theme_minimal()+
-     theme(text=element_text(size=16, color="black"),
-           axis.title = element_text(size=18),
-           axis.text.x = element_text(size=16),
-           panel.grid=element_blank(),
-           panel.spacing = unit(1, "lines"))
-  
-# arrangement of plots
-require(cowplot)
-fl <- plot_grid(f, l, nrow = 1, labels = "AUTO", label_size = 16)
-ggsave("rs12075-forest-assoc.png",height=20,width=10)
 
 circos_plot <- function()
 # circos plot
@@ -127,25 +98,3 @@ circos_plot()
 # gunzip -c ~/rds/results/public/gwas/blood_cell_traits/astle_2016/raw_results/blood_cell_traits/gzipped_interval/mono.tsv.gz | \
 # bgzip -f > ${INF}/work/mono.tsv.gz
 # tabix -S1 -s3 -b4 -e4 ${INF}/work/mono.tsv.gz
-
-deprecated <- function()
-{
-m <- with(rs12075,weighted.mean(b,1/se^2))
-format_p <- function(p) paste("p =", substring(prettyNum(p, digits=2, scientific=TRUE), 2))
-ymin <- -10
-f <- ggplot(data = rs12075,
-     aes(x = 7:1, y = fold*b, label=paste0(track,": ",format_p(10^log10p)))) +
-     geom_pointrange(aes(ymin=lcl, ymax=ucl), size=2) +
-     geom_text(y=ymin, hjust=0) +
-     geom_abline(intercept = m, slope = 0, lty = 2) +
-     ylim(c(ymin,5)) +
-     coord_flip() +
-     theme_bw() + theme(
-                         axis.text.y = element_blank(),
-                         panel.grid = element_blank(),
-                         panel.border = element_blank(),
-                         text = element_text(size=17)
-                       ) +
-     xlab("Phenotypes") +
-     ylab("Effect size")
-}
