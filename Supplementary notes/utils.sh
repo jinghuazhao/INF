c22ff8834c2221dff479e9bd5dc4c17f5809fef4
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-01-14 11:41:31 +0000

    Deployed 70b9a12a with MkDocs version: 1.1.2

--- Supplementary notes/utils.sh
+++ Supplementary notes/utils.sh
@@ -599,3 +599,33 @@ R --no-save -q <<END
   dev.off()
 END
 }
+
+function IL17C()
+# gunzip -c ${INF}/METAL/IL.17C-1.tbl.gz | cut -f1-3,10-11 | gzip -f > ${INF}/work/INF-IL.17C.gz
+{
+Rscript -e '
+  suppressMessages(library(dplyr))
+  suppressMessages(library(gap))
+  INF <- Sys.getenv("INF")
+  genes <- data.frame(MarkerName=c("chr16:88684495_G_T"),gene=c("IL17C"),color=c("red"))
+  IL.17C <- read.delim(file.path(INF,"work","INF-IL.17C.gz"),as.is=TRUE) %>%
+            mutate(Z=Effect/StdErr,P=as.numeric(pvalue(Z)),gene=NA,color=NA) %>%
+            filter(!is.na(Z)) %>%
+            select(Chromosome,Position,MarkerName,Z,P,color) %>%
+            arrange(Chromosome,Position)
+  loc <- with(IL.17C,MarkerName=="chr16:88684495_G_T")
+  IL.17C[loc,"gene"] <- "IL17C"
+  mhtdata <- select(IL.17C,Chromosome,Position,P,color,gene)
+  cis <- with(mhtdata,Chromosome==16 & Position>=88704999-1e6 & Position<=88706881+1e6)
+  mhtdata[cis,"color"] <- "IL17C"
+  subset(mhtdata,!is.na(gene))
+  png(file.path(INF,"work","INF-IL.17C-mhtplot.png"), res=300, units="in", width=12, height=8)
+    par(cex=0.8, mar=c(6,6,3,1),xpd=TRUE)
+    ops <- mht.control(colors=rep(c("blue4","skyblue"),11),srt=0,yline=2.5,xline=2)
+    hops <- hmht.control(data=filter(mhtdata,!is.na(gene)))
+    mhtplot2(mhtdata,ops,hops,xlab="",ylab="",srt=0, cex.axis=2)
+    axis(2,at=0:15)
+    abline(h=-log10(5e-10),col="red")
+  dev.off()
+'
+}
