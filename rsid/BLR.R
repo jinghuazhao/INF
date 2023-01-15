5f312dc63dff3ebad7d5426a7c898d0d3a644600
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-01-15 20:44:55 +0000

    Supplementary notes

--- rsid/BLR.R
+++ rsid/BLR.R
@@ -1,77 +1,78 @@
+meyer_test <- function()
 # Meyer data
-function meyer_test()
 {
-set.seed(1234567)
-meyer <- within(meyer,
-        {
-          yNa <- y
-           g1 <- ifelse(generation == 1, 1, 0)
-           g2 <- ifelse(generation == 2, 1, 0)
-           id <- animal
-       animal <- ifelse(!is.na(animal), animal, 0)
-          dam <- ifelse(!is.na(dam), dam, 0)
-         sire <- ifelse(!is.na(sire), sire, 0)
-        })
-G <- kin.morgan(meyer)$kin.matrix * 2
-library("regress")
-r <- regress(y ~ -1 + g1 + g2, ~ G, data = meyer)
-r
-attach(meyer)
-X <- as.matrix(meyer[c("g1", "g2")])
-m <- BLR(yNa, XF = X, GF = list(ID = 1:nrow(G), A = G),
-         prior = list(varE = list(df = 1, S = 0.25),
-         varU = list(df = 1, S = 0.63)),
-         nIter = 5000, burnIn = 500, thin = 1, saveAt = "meyer.BLR")
-with(r, h2G(sigma, sigma.cov))
+  set.seed(1234567)
+  meyer <- within(meyer,
+           {
+             yNa <- y
+              g1 <- ifelse(generation == 1, 1, 0)
+              g2 <- ifelse(generation == 2, 1, 0)
+              id <- animal
+          animal <- ifelse(!is.na(animal), animal, 0)
+             dam <- ifelse(!is.na(dam), dam, 0)
+            sire <- ifelse(!is.na(sire), sire, 0)
+           })
+  G <- kin.morgan(meyer)$kin.matrix * 2
+  r <- regress(y ~ -1 + g1 + g2, ~ G, data = meyer)
+  r
+  attach(meyer)
+  X <- as.matrix(meyer[c("g1", "g2")])
+  m <- BLR(yNa, XF = X, GF = list(ID = 1:nrow(G), A = G),
+           prior = list(varE = list(df = 1, S = 0.25),
+           varU = list(df = 1, S = 0.63)),
+           nIter = 5000, burnIn = 500, thin = 1, saveAt = "meyer.BLR_")
+  with(r, h2G(sigma, sigma.cov))
 }
 
+scallop <- function(protein,eps=0.02)
+# INTERVAL data
+# sed '2d' ~/INF/INTERVAL/o5000-inf1-outlier_in-r2.sample > ~/INF/h2/s.sample"
+{
+  m <- as.formula(paste(protein,"~",paste(covar,collapse="+"),"+",paste(qcovar,collapse="+"),"+","GRM"))
+  i <- regress(m, data = s)
+  i
+  attach(s)
+  X <- as.matrix(s[c(covar, qcovar)])
+  keep <- !is.na(apply(X,1,sum))
+  y <- s[[protein]][keep]
+  XF <- X[keep,]
+  A <- with(G,GRM)[keep,keep] + eps*diag(length(keep[keep]))
+  m <- BLR(y, XF = XF, GF = list(ID = 1:nrow(A), A = A),
+           prior = list(varE = list(df = 1, S = 0.25),
+           varU = list(df = 1, S = 0.63)),
+           nIter = 500000, burnIn = 15000, thin = 1, saveAt = paste0(protein,".BLR_"))
+  with(m, h2G(sigma, sigma.cov))
+  detach(s)
+  attach(m)
+  varU
+  varE
+  varU / ((1 + eps) * varU + varE)
+  U <- as.mcmc(scan(paste0(protein,".BLR_varU.dat"))[-(1:150000)])
+  E <- as.mcmc(scan(paste0(protein,".BLR_varE.dat"))[-(1:150000)])
+  e <- as.mcmc(cbind(U, E, h2 = U / ((1 + eps) * U + E)))
+  summary(e)$statistics
+  HPDinterval(e)
+  pdf(paste0(protein,".BLR.pdf"))
+  plot(e)
+  dev.off()
+}
+
+library("regress")
 library("BLR")
+library("coda")
 library(gap)
 
-# INTERVAL data
-ReadGRMBin <- function(prefix, AllN=FALSE, size=4)
-{
-  BinFileName <- paste(prefix,".grm.bin",sep="")
-  NFileName <- paste(prefix,".grm.N.bin",sep="")
-  IDFileName <- paste(prefix,".grm.id",sep="")
-  id <- read.table(IDFileName)
-  n <- dim(id)[1]
-  BinFile <- file(BinFileName, "rb");
-  grm <- readBin(BinFile, n=n*(n+1)/2, what=numeric(0), size=size)
-  close(BinFile)
-  NFile <- file(NFileName, "rb");
-  if(AllN) N <- readBin(NFile, n=n*(n+1)/2, what=numeric(0), size=size)
-  else N <- readBin(NFile, n=1, what=numeric(0), size=size)
-  close(NFile)
-  i <- sapply(1:n, function(i) i*(i+1)/2)
-  GRM <- matrix(NA,n,n)
-  GRM[upper.tri(GRM,diag=TRUE)] <- grm
-  GRM[lower.tri(GRM)] <- t(GRM)[lower.tri(GRM)]
-  invisible(list(grm=grm, id=id, N=N, GRM=GRM))
-}
+meyer_test()
+
 INF <- Sys.getenv("INF")
-system("sed '2d' /home/jhz22/INF/INTERVAL/o5000-inf1-outlier_in-r2.sample > s.sample")
-f <- file.path(INF,"INTERVAL","o5000-inf1-outlier_in-r2.sample")
+setwd(file.path(INF,"h2"))
 s <- read.table("s.sample",header=TRUE)
 covar <- c("sexPulse","season","plate")
 qcovar <- c("age","bleed_to_process_time",paste0("PC",1:20))
-big3 <- c("CCL25","IL.18R1","TNFB")
-header3 <- c("CCL25___O15444","IL.18R1___Q13478","TNFB___P01374")
 prefix <- file.path(INF,"h2","INTERVAL")
 G <- ReadGRMBin(prefix)
 GRM <- with(G,GRM)
-m <- as.formula(paste("CCL25___O15444","~",paste(covar,collapse="+"),"+",paste(qcovar,collapse="+"),"+","GRM"))
-i <- regress(m, data = s)
-i
-attach(s)
-X <- as.matrix(s[c(covar, qcovar)])
-keep <- !is.na(apply(X,1,sum))
-y <- CCL25___O15444[keep]
-XF <- X[keep,]
-A <- with(G,GRM)[keep,keep]+0.02*diag(length(keep[keep]))
-m <- BLR(y, XF = XF, GF = list(ID = 1:nrow(A), A = A),
-         prior = list(varE = list(df = 1, S = 0.25),
-         varU = list(df = 1, S = 0.63)),
-         nIter = 5000, burnIn = 500, thin = 1, saveAt = "CCL25___O15444.BLR")
-with(m, h2G(sigma, sigma.cov))
-detach(s)
+
+scallop("CCL25___O15444")
+scallop("IL.18R1___Q13478")
+scallop("TNFB___P01374")
