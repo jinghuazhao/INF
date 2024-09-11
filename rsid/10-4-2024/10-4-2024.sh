95b763977af83a5da6cd256d2095d274d2fc9e44
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-09-11 15:48:30 +0100

    Deployed 9691335e with MkDocs version: 1.5.3

--- rsid/10-4-2024/10-4-2024.sh
+++ rsid/10-4-2024/10-4-2024.sh
@@ -56,13 +56,14 @@ function forestplot()
           filter(grepl(traits,trait)) %>%
           filter(proxy==0 & r2==1 & !is.na(beta)) %>%
           select(-c(hg19_coordinates,ref_rsid,ref_hg19_coordinates,ref_a1,ref_a2,
-                    ref_hg38_coordinates,hg38_coordinates,proxy,r2,dprime))
+                    ref_hg38_coordinates,hg38_coordinates,proxy,r2,dprime)) %>%
+          rename(SNP=snp)
   swap <- gwas[["direction"]]=="-"
   gwas[swap,"a1"] <- "C"
   gwas[swap,"a2"] <- "A"
   write.table(gwas,file="ps.txt",sep="\t",quote=FALSE)
   select(gwas,study,efo,trait,beta,se,p,n)
-  write.table(gwas[c(4,14,16),c("study","pmid","trait","efo","snp","a1","a2","beta","se","p")],
+  write.table(gwas[c(4,14,16),c("study","pmid","trait","efo","SNP","a1","a2","beta","se","p")],
               file="Ins.txt",sep="\t",row.names=FALSE,quote=FALSE)
   #########################################
   #MR pQTL analysis script -- Jie Zheng #
@@ -111,11 +112,11 @@ function forestplot()
   #outcomes = ids)
 
   data <- read.delim("Ins.txt")
-  outcome_data <- format_data(data,
-                              type = "outcome", header = TRUE,
-                              phenotype_col = "trait", snp_col = "snp", beta_col = "beta",
-                              se_col = "se", eaf_col = "eaf", effect_allele_col = "a1",
-                              other_allele_col = "a2", pval_col = "p")
+  outcome_dat <- format_data(data,
+                             type = "outcome", header = TRUE,
+                             phenotype_col = "trait", snp_col = "SNP", beta_col = "beta",
+                             se_col = "se", eaf_col = "eaf", effect_allele_col = "a1",
+                             other_allele_col = "a2", pval_col = "p")
 
   ##harmonise the exposure and outcome data
   dat <- NULL
@@ -163,7 +164,7 @@ function forestplot()
   ins <- select(jma,prot,SNP,a1,a2,freq,b,se,p) %>%
          setNames(c("Phenotype","SNP","effect_allele","other_allele","eaf","beta","se","pval"))
   write.csv(ins,file="~/Ins.csv",quote=FALSE,row.names=FALSE)
-  write.csv(rename(data,Phenotype=trait,effect_allele=a2,other_allele=a1,pval=p),
+  write.csv(rename(data,Phenotype=trait,effect_allele=a1,other_allele=a2,pval=p),
             file="~/Out.csv",quote=FALSE,row.names=FALSE)
   END
 }
